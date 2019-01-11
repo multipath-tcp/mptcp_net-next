b9027de8834af08d1498ba39083a164e1082f101
Matthieu Baerts <matthieu.baerts@tessares.net>
2019-01-11 10:00:53 +0100

    git: init scripts branch
    This branch will be used to host all our scripts linked to the
    mptcp_net-next.
    
    Change-Id: Ibd7d1e9a0c1d5308685a93b786510d3da0d94fa4
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

---
+++ ci/update-tg-tree.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+#
+# The goal is to regularly sync 'net-next' branch on this repo with Davem's one.
+# Then our topgit tree can be updated and the modifications can be pushed only
+# after a successful build and tests. In case of problem, a notification will be
+# sent to Matthieu Baerts.
