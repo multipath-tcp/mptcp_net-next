2c68a2ee5e60391c0d059196e32fe60b3da7d380
Matthieu Baerts <matthieu.baerts@tessares.net>
2019-01-11 14:12:58 +0100

    ci:upd-tg: sync net-next with upstream
    Upstream is here Davem's net-next.
    
    Here in this script, we are going to only update the TopGit tree --
    currently "empty" -- and not the "master" branch which originally
    contains MPTCP implementation from multipath-tcp/mptcp.
    
    For this first step, we only sync net-next branch with Davem's one. No
    risk of error as we are not supposed to modify this branch, the base of
    our tree.
    
    Note that we also need to have a periodical sync to allow devs to
    propose a change base on latest net-next: using the top of the tree --
    t/upstream -- as target for inclusion in our tree or the base --
    net-next -- for direct inclusion upstream like Florian did. We don't
    want devs to update the tree manually.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Change-Id: I60de106b77de4d8f06524edf051c2f83f936d578

--- ci/update-tg-tree.sh
+++ ci/update-tg-tree.sh
@@ -4,3 +4,43 @@
 # Then our topgit tree can be updated and the modifications can be pushed only
 # after a successful build and tests. In case of problem, a notification will be
 # sent to Matthieu Baerts.
+
+# We should manage all errors in this script
+set -e
+
+# Gerrithub remote
+GIT_REMOTE_GERRITHUB_NAME="origin"
+
+# Davem remote
+GIT_REMOTE_NET_NEXT_URL="git://git.kernel.org/pub/scm/linux/kernel/git/davem/net-next.git"
+GIT_REMOTE_NET_NEXT_BRANCH="master"
+
+# Local repo
+GIT_BRANCH_NET_NEXT="net-next"
+
+# $@: message to display before quiting
+exit_err() {
+	echo "ERROR: ${*}"
+	exit 1
+}
+
+# $1: branch ;  [ $2: remote, default: origin ]
+git_checkout_clean() { local branch remote
+	branch="${1}"
+	remote="${2:-${GIT_REMOTE_GERRITHUB_NAME}}"
+
+	git checkout -f "${branch}" || git checkout -b "${branch}" "${remote}/${branch}"
+
+	# no need to remove .gitignored files, should be handle by git and we might
+	# need these files (.config, scripts, etc.)
+	git clean -fd
+}
+
+tg_update_base() {
+	git_checkout_clean "${GIT_BRANCH_NET_NEXT}"
+
+	git pull "${GIT_REMOTE_NET_NEXT_URL}" "${GIT_REMOTE_NET_NEXT_BRANCH}"
+	git push "${GIT_REMOTE_GERRITHUB_NAME}" "${GIT_BRANCH_NET_NEXT}"
+}
+
+tg_update_base || exit_err "Unable to update the base"
