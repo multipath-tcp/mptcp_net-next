11851addc175bb054003c416a8448b977d63235c
Matthieu Baerts <matthieu.baerts@tessares.net>
2019-02-20 10:07:56 +0100

    ci:upd-tg: ease integration with a docker
    In this docker, we will have all dependences needed to run this script,
    topgit included. That reduces the size of the script and ease the
    integration of the script.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Change-Id: I69f47c74868a6d24f003d45bb6288db46e23e63e

---
+++ ci/Dockerfile.update-tg-tree.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+#
+# Generate a Dockerfile, build it and run update-tg-tree.sh script
+
+SCRIPT="${1:-./update-tg-tree.sh}"
+if ! test -f "${SCRIPT}"; then
+    echo "Unable to execute ${SCRIPT} from ${PWD}"
+    exit 1
+fi
+shift
+SCRIPT_OPTS=("${@}")
+
+DOCKER_NAME="update-tg-tree"
+DOCKER_DIR=$(dirname "${0}")
+
+TG_SETUP_URL="https://github.com/mackyle/topgit/releases/download/topgit-0.19.12/topgit-0.19.12.tar.gz"
+TG_SETUP_TARBALL="topgit.tar.gz"
+
+DOCKERFILE=$(mktemp --tmpdir="${DOCKER_DIR}")
+trap 'rm -f "${DOCKERFILE}"' EXIT
+
+cat <<EOF > "${DOCKERFILE}"
+FROM ubuntu:latest
+
+# Use the same rights as the launcher
+RUN useradd -ms /bin/bash -u ${UID} -U ${USER} -d ${HOME}
+
+# dependencies for the script
+RUN apt-get update && \
+    apt-get install -y build-essential libncurses5-dev gcc libssl-dev bc bison \
+                       libelf-dev flex git curl tar hashalot && \
+    apt-get clean
+
+# TopGit
+RUN curl -L "${TG_SETUP_URL}" -o "${TG_SETUP_TARBALL}" && \
+    tar xzf "${TG_SETUP_TARBALL}" && \
+    cd "topgit-"* && \
+    make prefix="/usr" install && \
+    cd .. && \
+    rm -rf "${TG_SETUP_TARBALL}" "topgit-"*
+
+# switch to the current user and current dir
+USER ${USER}
+VOLUME ${PWD}
+WORKDIR ${PWD}
+EOF
+
+docker build -t "${DOCKER_NAME}" -f "${DOCKERFILE}" "${DOCKER_DIR}"
+# ssh and gitconfig is needed to create commits and pull/push
+docker run \
+    -v "${PWD}:${PWD}" \
+    -v "${HOME}/.ssh:${HOME}/.ssh" \
+    -v "${HOME}/.gitconfig:${HOME}/.gitconfig:ro" \
+    --rm "${DOCKER_NAME}" \
+    bash "-${-}" "${SCRIPT}" "${SCRIPT_OPTS[@]}"
--- ci/update-tg-tree.sh
+++ ci/update-tg-tree.sh
@@ -15,11 +15,6 @@ GIT_REMOTE_GERRITHUB_NAME="origin"
 GIT_REMOTE_NET_NEXT_URL="git://git.kernel.org/pub/scm/linux/kernel/git/davem/net-next.git"
 GIT_REMOTE_NET_NEXT_BRANCH="master"
 
-# topgit utilitary
-TG_SETUP_URL="https://github.com/mackyle/topgit/releases/download/topgit-0.19.12/topgit-0.19.12.tar.gz"
-TG_SETUP_SHA256="8b6b89c55108cc75d007f63818e43aa91b69424b5b8384c06ba2aa3122f5e440"
-TG_SETUP_PREFIX="build"
-
 # Local repo
 TG_TOPIC_BASE="net-next"
 TG_TOPIC_TOP="t/upstream"
@@ -48,48 +43,6 @@ git_clean() {
 	# no need to remove .gitignored files, should be handle by git and we might
 	# need these files (.config, scripts, etc.)
 	git clean -fd
-}
-
-
-##############
-## TG Setup ##
-##############
-
-# $1: output file
-tg_setup_download_if_needed() { local sha_check
-	sha_check="topgit.check"
-
-	# if already downloaded
-	printf "%s  %s\\n" "${TG_SETUP_SHA256}" "${output}" > "${sha_check}"
-	[ -f "topgit.tar.gz" ] && sha256 -c --quiet "${sha_check}" && return 0
-
-	curl -L "${TG_SETUP_URL}" -o "${output}"
-}
-
-tg_setup_install() { local output
-	output="topgit.tar.gz"
-
-	tg_setup_download_if_needed "${output}"
-
-	rm -rf "topgit-"* "./${TG_SETUP_PREFIX}" 2>/dev/null
-	mkdir -p "./${TG_SETUP_PREFIX}"
-
-	tar xzf "${output}"
-	cd "topgit-"*
-	# it seems a full path is needed for the prefix
-	make prefix="$(realpath "../${TG_SETUP_PREFIX}")" install
-	cd ..
-
-	PATH="${PWD}/${TG_SETUP_PREFIX}/bin:${PATH}"
-}
-
-tg_setup() {
-	# use 'patches' as a build dir as it is ignored by git and not used by make
-	mkdir -p patches/topgit && cd "${_}"
-
-	tg_setup_install
-
-	cd ../..
 }
 
 
@@ -197,7 +150,6 @@ tg_export() {
 ##########
 
 git_clean || exit_err "Unable to clean the environment"
-tg_setup || exit_err "Unable to setup topgit"
 tg_update_base || exit_err "Unable to update the topgit base"
 trap 'tg_trap_reset "${?}"' EXIT
 tg_update_tree || exit_err "Unable to update the topgit tree"
