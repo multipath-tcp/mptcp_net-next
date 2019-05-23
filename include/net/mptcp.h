e12122e35bdd4477eba0cbd7b9d208d6eb325123
Peter Krystad <peter.krystad@linux.intel.com>
2019-05-23 01:06:20 +0000

    mptcp: Handle MPTCP TCP options
    Currently only MPTCP v0 is supported so ignore v1 MP_CAPABLE option.
    
    Signed-off-by: Peter Krystad <peter.krystad@linux.intel.com>

--- include/net/mptcp.h
+++ include/net/mptcp.h
@@ -8,4 +8,29 @@
 #ifndef __NET_MPTCP_H
 #define __NET_MPTCP_H
 
+/* MPTCP option subtypes */
+#define OPTION_MPTCP_MPC_SYN	BIT(0)
+#define OPTION_MPTCP_MPC_SYNACK	BIT(1)
+#define OPTION_MPTCP_MPC_ACK	BIT(2)
+
+struct mptcp_out_options {
+	u16 suboptions;
+	u64 sndr_key;
+	u64 rcvr_key;
+};
+
+#ifdef CONFIG_MPTCP
+
+void mptcp_parse_option(const unsigned char *ptr, int opsize,
+			struct tcp_options_received *opt_rx);
+void mptcp_write_options(__be32 *ptr, struct mptcp_out_options *opts);
+
+#else
+
+static inline void mptcp_parse_option(const unsigned char *ptr, int opsize,
+				      struct tcp_options_received *opt_rx)
+{
+}
+
+#endif /* CONFIG_MPTCP */
 #endif /* __NET_MPTCP_H */
