55bd6370abe94806149416fcc7394ba2bf8dab9b
Matthieu Baerts <matthieu.baerts@tessares.net>
2019-06-03 17:48:42 +0200

    ci:virtme: refactoring to split in functions
    That helps to understand the script by using functions with clear names.
    
    But it is mainly to prepare the following commit that will run the tests
    twice with different kernel configs.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Change-Id: I44f69261471b9ee28cdbab509399756513da8690

--- ci/virtme.sh
+++ ci/virtme.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 #
 # The goal is to launch MPTCP kernel selftests
+#
+# Arguments:
+#   - "manual": to have a console in the VM. Additional args are for the kconfig
+#   - args we pass to kernel's "scripts/config" script.
 
 # We should manage all errors in this script
 set -e
@@ -18,21 +22,46 @@ VIRTME_RUN_EXPECT="${VIRTME_SCRIPT_DIR}/virtme.expect"
 
 SELFTESTS_DIR="tools/testing/selftests/net/mptcp"
 
-"${VIRTME_CONFIGKERNEL}" --arch=x86_64 --defconfig
+KCONFIG_EXTRA_CHECKS=(-e KASAN -e KASAN_OUTLINE -d TEST_KASAN
+                      -e PROVE_LOCKING -d DEBUG_LOCKDEP)
 
-# Extra options are needed for MPTCP kselftests and debug
-echo | scripts/config -e MPTCP -e VETH -e NET_SCH_NETEM \
-                      -e KASAN -e KASAN_OUTLINE -d TEST_KASAN \
-                      -e PROVE_LOCKING -d DEBUG_LOCKDEP
+# tmp files
+OUTPUT_SCRIPT=
+OUTPUT_VIRTME=
 
-make -j"$(nproc)" -l"$(nproc)"
-make -j"$(nproc)" -l"$(nproc)" headers_install
+# $@: extra kconfig
+gen_kconfig() { local kconfig
+        # Extra options are needed for MPTCP kselftests
+        kconfig=(-e MPTCP -e VETH -e NET_SCH_NETEM)
+        if [ -n "${1}" ]; then
+                kconfig+=("${@}")
+        fi
 
-OUTPUT_SCRIPT=$(mktemp --tmpdir="${PWD}")
-OUTPUT_VIRTME=$(mktemp --tmpdir="${PWD}")
+        "${VIRTME_CONFIGKERNEL}" --arch=x86_64 --defconfig
 
-mkdir -p "${VIRTME_SCRIPT_DIR}"
-cat <<EOF > "${VIRTME_SCRIPT}"
+        echo | ./scripts/config "${kconfig[@]}"
+}
+
+build() {
+        make -j"$(nproc)" -l"$(nproc)"
+        make -j"$(nproc)" -l"$(nproc)" headers_install
+}
+
+# $1 previous file
+get_tmp_file_rm_previous() {
+        if [ -f "${1}" ]; then
+                rm -f "${1}"
+        fi
+
+        mktemp --tmpdir="${PWD}"
+}
+
+prepare() {
+        OUTPUT_SCRIPT=$(get_tmp_file_rm_previous "${OUTPUT_SCRIPT}")
+        OUTPUT_VIRTME=$(get_tmp_file_rm_previous "${OUTPUT_VIRTME}")
+
+        mkdir -p "${VIRTME_SCRIPT_DIR}"
+        cat <<EOF > "${VIRTME_SCRIPT}"
 #! /bin/bash -x
 make -C tools/testing/selftests TARGETS=net/mptcp run_tests | \
         tee "${OUTPUT_SCRIPT}"
@@ -40,23 +69,23 @@ make -C tools/testing/selftests TARGETS=net/mptcp run_tests | \
 find . -user root -exec rm -rf "{}" \;
 echo "${VIRTME_SCRIPT_END}"
 EOF
-chmod +x "${VIRTME_SCRIPT}"
+        chmod +x "${VIRTME_SCRIPT}"
 
-# allow to launch anything else
-if [ "${1}" = "manual" ]; then
+        trap 'rm -f "${OUTPUT_SCRIPT}" "${OUTPUT_VIRTME}"' EXIT
+}
+
+run() {
         sudo "${VIRTME_RUN}" "${VIRTME_RUN_OPTS[@]}"
-        exit
-fi
+}
 
-trap 'rm -f "${OUTPUT_SCRIPT}" "${OUTPUT_VIRTME}"' EXIT
-
-cat <<EOF > "${VIRTME_RUN_SCRIPT}"
+run_expect() {
+        cat <<EOF > "${VIRTME_RUN_SCRIPT}"
 #! /bin/bash -x
 sudo "${VIRTME_RUN}" ${VIRTME_RUN_OPTS[@]} 2>&1
 EOF
-chmod +x "${VIRTME_RUN_SCRIPT}"
+        chmod +x "${VIRTME_RUN_SCRIPT}"
 
-cat <<EOF > "${VIRTME_RUN_EXPECT}"
+        cat <<EOF > "${VIRTME_RUN_EXPECT}"
 #!/usr/bin/expect -f
 
 set timeout 900
@@ -71,20 +100,49 @@ send -- "/usr/lib/klibc/bin/poweroff\r"
 
 expect eof
 EOF
-chmod +x "${VIRTME_RUN_EXPECT}"
+        chmod +x "${VIRTME_RUN_EXPECT}"
 
-# for an unknown reason, we cannot use "--script-sh", qemu is not started, no
-# debug. As a workaround, we use expect
-"${VIRTME_RUN_EXPECT}" | tee "${OUTPUT_VIRTME}"
+        # for an unknown reason, we cannot use "--script-sh", qemu is not
+        # started, no debug. As a workaround, we use expect.
+        "${VIRTME_RUN_EXPECT}" | tee "${OUTPUT_VIRTME}"
+}
 
-if grep -C 30 "Call Trace:" "${OUTPUT_VIRTME}"; then
-        echo "Call Trace found"
-        exit 2
-elif grep -q "^ok [0-9]\+ selftests: mptcp: mptcp_connect\.sh$" "${OUTPUT_SCRIPT}"; then
-        echo "Selftests OK"
-        exit 0
+# $@: args for kconfig
+analyse() {
+        echo "Kconfig: ${*}"
+        if grep -C 30 "Call Trace:" "${OUTPUT_VIRTME}"; then
+                echo "Call Trace found"
+                exit 2
+        elif grep -q "^ok [0-9]\+ selftests: mptcp: mptcp_connect\.sh$" "${OUTPUT_SCRIPT}"; then
+                echo "Selftests OK"
+        else
+                echo "Error when launching selftests"
+                grep -A 9999 "^# selftests: mptcp: mptcp_connect\.sh$" "${OUTPUT_SCRIPT}"
+                exit 1
+        fi
+}
+
+# $@: args for kconfig
+go_manual() {
+        gen_kconfig "${@}"
+        build
+        run
+}
+
+# $@: args for kconfig
+go_expect() {
+        gen_kconfig "${@}"
+        build
+        prepare
+        run_expect
+        analyse "${@}"
+}
+
+
+# allow to launch anything else
+if [ "${1}" = "manual" ]; then
+        shift
+        go_manual "${@}"
 else
-        echo "Error when launching selftests"
-        grep -A 9999 "^# selftests: mptcp: mptcp_connect\.sh$" "${OUTPUT_SCRIPT}"
-        exit 1
+        go_expect "${KCONFIG_EXTRA_CHECKS[@]}" "${@}"
 fi
