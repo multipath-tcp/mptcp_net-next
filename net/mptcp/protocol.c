multipath-tcp/mptcp_net-next

commit c99bd38973823c8d58c825ba24301106b56eba9b

mptcp:init: panic in case of error - part 1
The idea is to mimic what is done in tcp_init() and not ignore errors
from the init phase. It also simplifies the code as we don't have to
manage exit parts anymore.

Can be squashed in:
- "mptcp: Add MPTCP socket stubs" (mptcp_init())
- "mptcp: Associate MPTCP context with TCP socket" (subflow_init())
- "mptcp: Create SUBFLOW socket for incoming connections" (subflow_ops)

Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/protocol.c
@@ -110,13 +110,10 @@ static struct inet_protosw mptcp_protosw = {
 	.ops		= &inet_stream_ops,
 };
 
-void mptcp_init(void)
+void __init mptcp_init(void)
 {
-	int err;
-
-	err = proto_register(&mptcp_prot, 1);
-	if (err)
-		return;
+	if (proto_register(&mptcp_prot, 1) != 0)
+		panic("Failed to register MPTCP proto.\n");
 
 	inet_register_protosw(&mptcp_protosw);
 

commit fe95c134e25acb84975107f7c57784f9348c8c35

mptcp:protocol: remove useless 'return'
Fixes: c99bd3897382 (mptcp:init: panic in case of error - part 1)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/protocol.c
@@ -116,6 +116,4 @@ void __init mptcp_init(void)
 		panic("Failed to register MPTCP proto.\n");
 
 	inet_register_protosw(&mptcp_protosw);
-
-	return;
 }