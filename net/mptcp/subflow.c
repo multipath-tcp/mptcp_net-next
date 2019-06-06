multipath-tcp/mptcp_net-next

commit 7cf2e6c94084fd21d16b9f674869a51b2985f218

mptcp:init: panic in case of error - part 2
The idea is to mimic what is done in tcp_init() and not ignore errors
from the init phase. It also simplifies the code as we don't have to
manage exit parts anymore.

Can be squashed in:
- "mptcp: Add MPTCP socket stubs" (mptcp_init())
- "mptcp: Associate MPTCP context with TCP socket" (subflow_init())
- "mptcp: Create SUBFLOW socket for incoming connections" (subflow_ops)

Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/subflow.c
@@ -72,7 +72,8 @@ static struct tcp_ulp_ops subflow_ulp_ops __read_mostly = {
 
 int subflow_init(void)
 {
-	return tcp_register_ulp(&subflow_ulp_ops);
+	if (tcp_register_ulp(&subflow_ulp_ops) != 0)
+		panic("MPTCP: failed to register subflows to ULP");
 }
 
 void subflow_exit(void)

net/mptcp/protocol.c
@@ -171,26 +171,17 @@ static struct inet_protosw mptcp_protosw = {
 
 void __init mptcp_init(void)
 {
-	int err;
-
+	mptcp_prot.h.hashinfo = tcp_prot.h.hashinfo;
 	mptcp_stream_ops = inet_stream_ops;
 	mptcp_stream_ops.bind = mptcp_bind;
 	mptcp_stream_ops.connect = mptcp_stream_connect;
 
-	err = subflow_init();
-	if (err)
-		goto subflow_failed;
+	subflow_init();
 
 	if (proto_register(&mptcp_prot, 1) != 0)
 		panic("Failed to register MPTCP proto.\n");
 
 	inet_register_protosw(&mptcp_protosw);
 
-	return;
-
-proto_failed:
-	subflow_exit();
-
-subflow_failed:
 	return;
 }

net/mptcp/protocol.h
@@ -57,7 +57,6 @@ static inline struct socket *mptcp_subflow_tcp_socket(const struct subflow_conte
 	return subflow->tcp_sock;
 }
 
-int subflow_init(void);
-void subflow_exit(void);
+void subflow_init(void);
 
 #endif /* __MPTCP_PROTOCOL_H */

commit f51c08d3ba7d264e8a146e5ab5e617fbf3e92f13

mptcp:sf: fix typo conflict resolution
Fixes: 7cf2e6c94084 (mptcp:init: panic in case of error - part 2)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/subflow.c
@@ -76,7 +76,7 @@ int subflow_init(void)
 		panic("MPTCP: failed to register subflows to ULP");
 }
 
-void subflow_exit(void)
+int subflow_exit(void)
 {
 	tcp_unregister_ulp(&subflow_ulp_ops);
 }