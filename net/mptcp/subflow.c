multipath-tcp/mptcp_net-next

commit 239f3baa2dc04841d304b4e3dc8c7031e8f64dff

mptcp:init: panic in case of error - part 3
The idea is to mimic what is done in tcp_init() and not ignore errors
from the init phase. It also simplifies the code as we don't have to
manage exit parts anymore.

Can be squashed in:
- "mptcp: Add MPTCP socket stubs" (mptcp_init())
- "mptcp: Associate MPTCP context with TCP socket" (subflow_init())
- "mptcp: Create SUBFLOW socket for incoming connections" (subflow_ops)

Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/subflow.c
@@ -222,19 +222,11 @@ static int subflow_ops_init(struct request_sock_ops *subflow_ops)
 	return 0;
 }
 
-static void subflow_ops_exit(void)
-{
-	kmem_cache_destroy(subflow_request_sock_ops.slab);
-}
-
 int subflow_init(void)
 {
-	int ret;
-
 	subflow_request_sock_ops = tcp_request_sock_ops;
-	ret = subflow_ops_init(&subflow_request_sock_ops);
-	if (ret != 0)
-		return ret;
+	if (subflow_ops_init(&subflow_request_sock_ops) != 0)
+		panic("MPTCP: failed to init subflow request sock ops");
 
 	subflow_request_sock_ipv4_ops = tcp_request_sock_ipv4_ops;
 	subflow_request_sock_ipv4_ops.init_req = subflow_v4_init_req;
@@ -246,12 +238,4 @@ int subflow_init(void)
 
 	if (tcp_register_ulp(&subflow_ulp_ops) != 0)
 		panic("MPTCP: failed to register subflows to ULP");
-
-	return ret;
-}
-
-void subflow_exit(void)
-{
-	tcp_unregister_ulp(&subflow_ulp_ops);
-	subflow_ops_exit();
 }

commit 483f621c71510e7326dc12922d3f447edfbb4d6b

mptcp: add missing '\n' with panic()
Fixes: 239f3baa2dc0 (mptcp:init: panic in case of error - part 3)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/subflow.c
@@ -226,7 +226,7 @@ void subflow_init(void)
 {
 	subflow_request_sock_ops = tcp_request_sock_ops;
 	if (subflow_ops_init(&subflow_request_sock_ops) != 0)
-		panic("MPTCP: failed to init subflow request sock ops");
+		panic("MPTCP: failed to init subflow request sock ops\n");
 
 	subflow_request_sock_ipv4_ops = tcp_request_sock_ipv4_ops;
 	subflow_request_sock_ipv4_ops.init_req = subflow_v4_init_req;