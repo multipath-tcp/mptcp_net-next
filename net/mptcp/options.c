multipath-tcp/mptcp_net-next

commit 233967ed765327f245ba62a508c7c2ec348da299

mptcp: fix checkpatch issues
What has been fixed:

 - "foo* bar" should be "foo *bar" (2)
 - line over 80 characters

What has not been fixed:

 - /

Fixes: 4916eb951361 (mptcp: Write MPTCP DSS headers to outgoing data packets)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/options.c
@@ -241,7 +241,9 @@ static bool mptcp_established_options_dss(struct sock *sk, struct sk_buff *skb,
 	if (mpext && mpext->use_ack) {
 		ack_size = TCPOLEN_MPTCP_DSS_ACK64;
 
-		/* Add kind/length/subtype/flag overhead if mapping not populated */
+		/* Add kind/lenght/subtype/flag overhead if mapping is not
+		 * populated
+		 */
 		if (dss_size == 0)
 			ack_size += TCPOLEN_MPTCP_DSS_BASE;
 

include/net/mptcp.h
@@ -120,7 +120,7 @@ static inline bool mptcp_established_options(struct sock *sk,
 					     struct sk_buff *skb,
 					     unsigned int *size,
 					     unsigned int remaining,
-					     struct mptcp_out_options* opts)
+					     struct mptcp_out_options *opts)
 {
 	return false;
 }

commit 52083ddbf0bcb88def470b52707f5ccb9bee44a7

mptcp: fix remaining checkpatch issue
A typo (with 'length') was introduced when fixing another issue.

Fixes: 233967ed7653 (mptcp: fix checkpatch issues)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/options.c
@@ -241,7 +241,7 @@ static bool mptcp_established_options_dss(struct sock *sk, struct sk_buff *skb,
 	if (mpext && mpext->use_ack) {
 		ack_size = TCPOLEN_MPTCP_DSS_ACK64;
 
-		/* Add kind/lenght/subtype/flag overhead if mapping is not
+		/* Add kind/length/subtype/flag overhead if mapping is not
 		 * populated
 		 */
 		if (dss_size == 0)