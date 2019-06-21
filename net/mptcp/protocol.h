multipath-tcp/mptcp_net-next

commit 6f0546582f943f3fbab557d234d467a215665deb

mptcp: move MPTCP option bits to internal header
OPTION_MPTCP_ADD/RM_* are only used in files from net/mptcp.

Fixes: abb537b13f1a (mptcp: Add ADD_ADDR handling)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/protocol.h
@@ -14,6 +14,10 @@
 #define OPTION_MPTCP_MPC_SYN	BIT(0)
 #define OPTION_MPTCP_MPC_SYNACK	BIT(1)
 #define OPTION_MPTCP_MPC_ACK	BIT(2)
+#define OPTION_MPTCP_ADD_ADDR	BIT(6)
+#define OPTION_MPTCP_ADD_ADDR6	BIT(7)
+#define OPTION_MPTCP_RM_ADDR	BIT(8)
+
 
 /* MPTCP option subtypes */
 #define MPTCPOPT_MP_CAPABLE	0

include/net/mptcp.h
@@ -24,10 +24,6 @@ struct mptcp_ext {
 			__unused:2;
 };
 
-#define OPTION_MPTCP_ADD_ADDR		BIT(6)
-#define OPTION_MPTCP_ADD_ADDR6		BIT(7)
-#define OPTION_MPTCP_RM_ADDR		BIT(8)
-
 struct mptcp_out_options {
 #if IS_ENABLED(CONFIG_MPTCP)
 	u16 suboptions;

commit d589ed1f7d358f953c7e79ec929812c56ab75e72

mptcp: fix double blank lines
Fixes: 6f0546582f94 (mptcp: move MPTCP option bits to internal header)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/protocol.h
@@ -18,7 +18,6 @@
 #define OPTION_MPTCP_ADD_ADDR6	BIT(7)
 #define OPTION_MPTCP_RM_ADDR	BIT(8)
 
-
 /* MPTCP option subtypes */
 #define MPTCPOPT_MP_CAPABLE	0
 #define MPTCPOPT_MP_JOIN	1