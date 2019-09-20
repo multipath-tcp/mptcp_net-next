ba0eb7460eb71614ccf10e4a3ab355f54fd59492
Mat Martineau <mathew.j.martineau@linux.intel.com>
2019-09-20 16:08:52 +0000

    mptcp: Make MPTCP socket block/wakeup ignore sk_receive_queue
    The MPTCP-level socket doesn't use sk_receive_queue, so it was possible
    for mptcp_recvmsg() to remain blocked when there was data ready for it
    to read. When the MPTCP socket is waiting for additional data and it
    releases the subflow socket lock, the subflow may have incoming packets
    ready to process and it sometimes called subflow_data_ready() before the
    MPTCP socket called sk_wait_data().
    
    This change adds a new function for the MPTCP socket to use when waiting
    for a data ready signal. Atomic bitops with memory barriers are used to
    set, test, and clear a MPTCP socket flag that indicates waiting subflow
    data. This flag replaces the sk_receive_queue checks used by other
    socket types.
    
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -377,6 +377,31 @@ del_out:
 	return ret;
 }
 
+static void mptcp_wait_data(struct sock *sk, long *timeo)
+{
+	DEFINE_WAIT_FUNC(wait, woken_wake_function);
+	struct mptcp_sock *msk = mptcp_sk(sk);
+	int data_ready;
+
+	add_wait_queue(sk_sleep(sk), &wait);
+	sk_set_bit(SOCKWQ_ASYNC_WAITDATA, sk);
+
+	release_sock(sk);
+
+	smp_mb__before_atomic();
+	data_ready = test_and_clear_bit(MPTCP_DATA_READY, &msk->flags);
+	smp_mb__after_atomic();
+
+	if (!data_ready)
+		*timeo = wait_woken(&wait, TASK_INTERRUPTIBLE, *timeo);
+
+	sched_annotate_sleep();
+	lock_sock(sk);
+
+	sk_clear_bit(SOCKWQ_ASYNC_WAITDATA, sk);
+	remove_wait_queue(sk_sleep(sk), &wait);
+}
+
 static int mptcp_recvmsg(struct sock *sk, struct msghdr *msg, size_t len,
 			 int nonblock, int flags, int *addr_len)
 {
@@ -426,6 +451,10 @@ static int mptcp_recvmsg(struct sock *sk, struct msghdr *msg, size_t len,
 		u64 ack_seq;
 		u64 old_ack;
 		u32 ssn;
+
+		smp_mb__before_atomic();
+		clear_bit(MPTCP_DATA_READY, &msk->flags);
+		smp_mb__after_atomic();
 
 		status = mptcp_get_mapping(ssk);
 
@@ -560,7 +589,7 @@ wait_for_data:
 
 		pr_debug("block");
 		release_sock(ssk);
-		sk_wait_data(sk, &timeo, NULL);
+		mptcp_wait_data(sk, &timeo);
 		lock_sock(ssk);
 	}
 
