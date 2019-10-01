multipath-tcp/mptcp_net-next

commit a03d15e6a899aff54dee73431f48a02f30de39f5

mptcp: Remove all traces of checksum support
Since checksum support is not planned remove any references
that look it may be supported.

Also re-factor DSS option size calculation and make
DSS option size check for exactly correct lengths.

Signed-off-by: Peter Krystad <peter.krystad@linux.intel.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/subflow.c
@@ -39,8 +39,7 @@ int mptcp_subflow_create_socket(struct sock *sk, struct socket **new_sock)
 	*new_sock = sf;
 	subflow->conn = sk;
 	subflow->request_mptcp = 1; // @@ if MPTCP enabled
-	subflow->request_cksum = 1; // @@ if checksum enabled
-	subflow->request_version = 0;
+	subflow->request_version = 0; /* currently only v0 supported */
 
 	return 0;
 }

net/mptcp/protocol.h
@@ -49,7 +49,6 @@ static inline struct mptcp_sock *mptcp_sk(const struct sock *sk)
 /* MPTCP subflow context */
 struct mptcp_subflow_context {
 	u32	request_mptcp : 1,  /* send MP_CAPABLE */
-		request_cksum : 1,
 		request_version : 4;
 	struct  socket *tcp_sock;  /* underlying tcp_sock */
 	struct  sock *conn;        /* parent mptcp_sock */

commit fa25b3cfa037066b6926b8b065e6a9b3dde75949

mptcp: build-fix
Fixes: a03d15e6a899 (mptcp: Remove all traces of checksum support)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/subflow.c
@@ -37,9 +37,6 @@ static void subflow_v4_init_req(struct request_sock *req,
 			subflow_req->version = listener->request_version;
 		else
 			subflow_req->version = rx_opt.mptcp.version;
-		if ((rx_opt.mptcp.flags & MPTCP_CAP_CHECKSUM_REQD) ||
-		    listener->request_cksum)
-			subflow_req->checksum = 1;
 		subflow_req->remote_key = rx_opt.mptcp.sndr_key;
 	} else {
 		subflow_req->mp_capable = 0;