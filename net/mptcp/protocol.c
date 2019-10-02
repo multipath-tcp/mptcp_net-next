e3d960027d40d963f05c22bd22dc9436027a484f
Paolo Abeni <pabeni@redhat.com>
2019-10-02 19:35:50 +0000

    mptcp: update per unacked sequence on pkt reception
    So that we keep per unacked sequence number consistent; since
    we update per msk data, use an atomic64 cmpxcgh() to protect
    against concurrent updates from multiple subflows.
    
    Initialize the snd_una at connect()/accept() time.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -686,6 +686,7 @@ static struct sock *mptcp_accept(struct sock *sk, int flags, int *err,
 
 		mptcp_crypto_key_sha1(msk->remote_key, NULL, &ack_seq);
 		msk->write_seq = subflow->idsn + 1;
+		atomic64_set(&msk->snd_una, msk->write_seq);
 		ack_seq++;
 		msk->ack_seq = ack_seq;
 		subflow->map_seq = ack_seq;
@@ -820,6 +821,7 @@ void mptcp_finish_connect(struct sock *sk, int mp_capable)
 
 		mptcp_crypto_key_sha1(msk->remote_key, NULL, &ack_seq);
 		msk->write_seq = subflow->idsn + 1;
+		atomic64_set(&msk->snd_una, msk->write_seq);
 		ack_seq++;
 		msk->ack_seq = ack_seq;
 		subflow->map_seq = ack_seq;
