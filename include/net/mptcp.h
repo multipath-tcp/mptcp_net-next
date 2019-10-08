6191b43a4f56a22c5424aefe06bd595c2b8796a2
Peter Krystad <peter.krystad@linux.intel.com>
2019-10-08 11:45:42 +0000

    mptcp: Handle MPTCP TCP options
    Currently only MPTCP v0 is supported so ignore v1 MP_CAPABLE option.
    
    Signed-off-by: Peter Krystad <peter.krystad@linux.intel.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Florian Westphal <fw@strlen.de>

--- include/net/mptcp.h
+++ include/net/mptcp.h
@@ -22,9 +22,21 @@ struct mptcp_ext {
 			__unused:2;
 };
 
+struct mptcp_out_options {
+#if IS_ENABLED(CONFIG_MPTCP)
+	u16 suboptions;
+	u64 sndr_key;
+	u64 rcvr_key;
+#endif
+};
+
 #ifdef CONFIG_MPTCP
 
 void mptcp_init(void);
+
+void mptcp_parse_option(const unsigned char *ptr, int opsize,
+			struct tcp_options_received *opt_rx);
+void mptcp_write_options(__be32 *ptr, struct mptcp_out_options *opts);
 
 static inline bool mptcp_skb_ext_exist(const struct sk_buff *skb)
 {
@@ -34,6 +46,11 @@ static inline bool mptcp_skb_ext_exist(const struct sk_buff *skb)
 #else
 
 static inline void mptcp_init(void)
+{
+}
+
+static inline void mptcp_parse_option(const unsigned char *ptr, int opsize,
+				      struct tcp_options_received *opt_rx)
 {
 }
 
