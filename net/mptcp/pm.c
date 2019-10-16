multipath-tcp/mptcp_net-next

commit b0cdad25c90a3f1ecac67e763017c8c5c7fa7ff2

mptcp: Update path manager interface commands for IPv6
squashto: Implement path manager interface commands

Signed-off-by: Peter Krystad <peter.krystad@linux.intel.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/pm.c
@@ -23,11 +23,11 @@ int mptcp_pm_announce_addr(u32 token, u8 local_id, struct in_addr *addr)
 		goto announce_put;
 	}
 
-	pr_debug("msk=%p, local_id=%d, addr=%x", msk, local_id, addr->s_addr);
+	pr_debug("msk=%p, local_id=%d", msk, local_id);
 	msk->pm.local_valid = 1;
 	msk->pm.local_id = local_id;
-	msk->pm.local_family = family;
-	msk->pm.local_addr.s_addr = addr->s_addr;
+	msk->pm.local_family = AF_INET;
+	msk->pm.local_addr = *addr;
 	msk->addr_signal = 1;
 
 announce_put:
@@ -38,7 +38,27 @@ announce_put:
 #if IS_ENABLED(CONFIG_IPV6)
 int mptcp_pm_announce_addr6(u32 token, u8 local_id, struct in6_addr *addr)
 {
-	return -ENOTSUPP;
+	struct mptcp_sock *msk = mptcp_token_get_sock(token);
+	int err = 0;
+
+	if (!msk)
+		return -EINVAL;
+
+	if (msk->pm.local_valid) {
+		err = -EBADR;
+		goto announce_put;
+	}
+
+	pr_debug("msk=%p, local_id=%d", msk, local_id);
+	msk->pm.local_valid = 1;
+	msk->pm.local_id = local_id;
+	msk->pm.local_family = AF_INET6;
+	msk->pm.local_addr6 = *addr;
+	msk->addr_signal = 1;
+
+announce_put:
+	sock_put((struct sock *)msk);
+	return err;
 }
 #endif
 
@@ -75,17 +95,18 @@ int mptcp_pm_create_subflow(u32 token, u8 remote_id, struct in_addr *addr)
 
 	local.sin_family = AF_INET;
 	local.sin_port = 0;
-	if (family == AF_INET)
-		local.sin_addr.s_addr = addr->s_addr;
+	if (!addr)
+		local.sin_addr = *addr;
 	else
 		local.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	remote.sin_family = msk->pm.remote_family;
 	remote.sin_port = htons(msk->dport);
-	remote.sin_addr.s_addr = msk->pm.remote_addr.s_addr;
+	remote.sin_addr = msk->pm.remote_addr;
 
-	err = mptcp_subflow_connect((struct sock *)msk, &local, &remote,
-				    remote_id);
+	err = mptcp_subflow_connect((struct sock *)msk,
+				    (struct sockaddr *)&local,
+				    (struct sockaddr *)&remote, remote_id);
 
 create_put:
 	sock_put((struct sock *)msk);
@@ -95,7 +116,39 @@ create_put:
 #if IS_ENABLED(CONFIG_IPV6)
 int mptcp_pm_create_subflow6(u32 token, u8 remote_id, struct in6_addr *addr)
 {
-	return -ENOTSUPP;
+	struct mptcp_sock *msk = mptcp_token_get_sock(token);
+	struct sockaddr_in6 remote;
+	struct sockaddr_in6 local;
+	int err;
+
+	if (!msk)
+		return -EINVAL;
+
+	pr_debug("msk=%p", msk);
+
+	if (!msk->pm.remote_valid || remote_id != msk->pm.remote_id) {
+		err = -EBADR;
+		goto create_put;
+	}
+
+	local.sin6_family = AF_INET6;
+	local.sin6_port = 0;
+	if (!addr)
+		local.sin6_addr = *addr;
+	else
+		local.sin6_addr = in6addr_any;
+
+	remote.sin6_family = msk->pm.remote_family;
+	remote.sin6_port = htons(msk->dport);
+	remote.sin6_addr = msk->pm.remote_addr6;
+
+	err = mptcp_subflow_connect((struct sock *)msk,
+				    (struct sockaddr *)&local,
+				    (struct sockaddr *)&remote, remote_id);
+
+create_put:
+	sock_put((struct sock *)msk);
+	return err;
 }
 #endif
 
@@ -147,7 +200,7 @@ void mptcp_pm_add_addr(struct mptcp_sock *msk, const struct in_addr *addr,
 
 	pr_debug("msk=%p, addr=%x, remote_id=%d", msk, addr->s_addr, id);
 
-	pm->remote_addr.s_addr = addr->s_addr;
+	pm->remote_addr = *addr;
 	pm->remote_id = id;
 	pm->remote_family = AF_INET;
 	pm->remote_valid = 1;

commit e3c234a7910578100a2fb1ca97f9582211fc7f83

mptcp: move code from t/mptcp-Implement-path-manager-interface-commands
to 'mptcp: Add path manager interface'.

Fixes: b0cdad25c90a (mptcp: Update path manager interface commands for IPv6)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/pm.c
@@ -200,7 +200,7 @@ void mptcp_pm_add_addr(struct mptcp_sock *msk, const struct in_addr *addr,
 
 	pr_debug("msk=%p, addr=%x, remote_id=%d", msk, addr->s_addr, id);
 
-	pm->remote_addr = *addr;
+	pm->remote_addr.s_addr = addr->s_addr;
 	pm->remote_id = id;
 	pm->remote_family = AF_INET;
 	pm->remote_valid = 1;