multipath-tcp/mptcp_net-next

commit 9dfbbd1eedec5fc0fd56d921906f947ce0d5e236

mptcp: Add missing ADD_ADDR handling
Add missing piece that copies IPv6 address into TCP option

squashto: Add ADD_ADDR handling

Signed-off-by: Peter Krystad <peter.krystad@linux.intel.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/options.c
@@ -405,14 +405,22 @@ static bool mptcp_established_options_addr(struct sock *sk,
 	if (mptcp_pm_addr_signal(msk, &id, &saddr))
 		return false;
 
-	if (saddr.ss_family == AF_INET && remaining < TCPOLEN_MPTCP_ADD_ADDR)
-		return false;
-
-	opts->suboptions |= OPTION_MPTCP_ADD_ADDR;
-	opts->addr_id = id;
-	opts->addr.s_addr = ((struct sockaddr_in *)&saddr)->sin_addr.s_addr;
-	*size = TCPOLEN_MPTCP_ADD_ADDR;
-
+	if (saddr.ss_family == AF_INET) {
+		if (remaining < TCPOLEN_MPTCP_ADD_ADDR)
+			return false;
+		opts->suboptions |= OPTION_MPTCP_ADD_ADDR;
+		opts->addr_id = id;
+		opts->addr = ((struct sockaddr_in *)&saddr)->sin_addr;
+		*size = TCPOLEN_MPTCP_ADD_ADDR;
+	}
+	if (saddr.ss_family == AF_INET6) {
+		if (remaining < TCPOLEN_MPTCP_ADD_ADDR6)
+			return false;
+		opts->suboptions |= OPTION_MPTCP_ADD_ADDR6;
+		opts->addr_id = id;
+		opts->addr6 = ((struct sockaddr_in6 *)&saddr)->sin6_addr;
+		*size = TCPOLEN_MPTCP_ADD_ADDR6;
+	}
 	msk->addr_signal = 0;
 
 	return true;

commit 0b515fa5e27a972693ba323081124222b411c50a

mptcp: avoid IPv6 check if already IPv4
As reported on the ML.

Fixes: 9dfbbd1eedec (mptcp: Add missing ADD_ADDR handling)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/options.c
@@ -412,8 +412,7 @@ static bool mptcp_established_options_addr(struct sock *sk,
 		opts->addr_id = id;
 		opts->addr = ((struct sockaddr_in *)&saddr)->sin_addr;
 		*size = TCPOLEN_MPTCP_ADD_ADDR;
-	}
-	if (saddr.ss_family == AF_INET6) {
+	} else if (saddr.ss_family == AF_INET6) {
 		if (remaining < TCPOLEN_MPTCP_ADD_ADDR6)
 			return false;
 		opts->suboptions |= OPTION_MPTCP_ADD_ADDR6;
@@ -421,6 +420,7 @@ static bool mptcp_established_options_addr(struct sock *sk,
 		opts->addr6 = ((struct sockaddr_in6 *)&saddr)->sin6_addr;
 		*size = TCPOLEN_MPTCP_ADD_ADDR6;
 	}
+
 	msk->addr_signal = 0;
 
 	return true;