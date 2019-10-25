7c99fed357c101dff502c38a4512990b69612f1f
Peter Krystad <peter.krystad@linux.intel.com>
2019-10-25 15:34:25 +0000

    mptcp: Add handling of incoming MP_JOIN requests
    Process the MP_JOIN option in a SYN packet with the same flow
    as MP_CAPABLE but when the third ACK is received add the
    subflow to the MPTCP socket subflow list instead of adding it to
    the TCP socket accept queue.
    
    The subflow is added at the end of the subflow list so it will not
    interfere with the existing subflows operation and no data is
    expected to be transmitted on it.
    
    Signed-off-by: Peter Krystad <peter.krystad@linux.intel.com>

--- include/linux/tcp.h
+++ include/linux/tcp.h
@@ -106,8 +106,14 @@ struct tcp_options_received {
 		u8      mp_capable : 1,
 			mp_join : 1,
 			dss : 1,
+			backup : 1,
 			version : 4;
 		u8      flags;
+		u8      join_id;
+		u32     token;
+		u32     nonce;
+		u64     thmac;
+		u8      hmac[20];
 		u8	dss_flags;
 		u8	use_map:1,
 			dsn64:1,
