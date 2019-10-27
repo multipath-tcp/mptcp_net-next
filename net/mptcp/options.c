multipath-tcp/mptcp_net-next

commit 6eb607af74751468ec7c7c5ffdade6db8768f599

mptcp: Ignore incoming MPTCP options when not MP_CAPABLE
Any options received on a fallback subflow should be ignored.

squashto: Implement MPTCP receive path

Signed-off-by: Peter Krystad <peter.krystad@linux.intel.com>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/options.c
@@ -394,6 +394,9 @@ void mptcp_incoming_options(struct sock *sk, struct sk_buff *skb,
 	struct mptcp_options_received *mp_opt;
 	struct mptcp_ext *mpext;
 
+	if (!subflow->mp_capable)
+		return;
+
 	mp_opt = &opt_rx->mptcp;
 
 	if (!mp_opt->dss)

commit c559c9eb4f9fadadf035a7c26c442878119e1059

mptcp: fix compilation error
Fixes: 6eb607af7475 (mptcp: Ignore incoming MPTCP options when not MP_CAPABLE)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/options.c
@@ -391,6 +391,7 @@ bool mptcp_synack_options(const struct request_sock *req, unsigned int *size,
 void mptcp_incoming_options(struct sock *sk, struct sk_buff *skb,
 			    struct tcp_options_received *opt_rx)
 {
+	struct mptcp_subflow_context *subflow = mptcp_subflow_ctx(sk);
 	struct mptcp_options_received *mp_opt;
 	struct mptcp_ext *mpext;
 