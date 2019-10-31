a5dae26ddd5909b7d60e3b060c26dcf61ddfa2c2
Peter Krystad <peter.krystad@linux.intel.com>
2019-10-31 02:42:55 +0000

    mptcp: Add path manager interface
    Add enough of a path manager interface to allow sending of ADD_ADDR
    when an incoming MPTCP connection is created. Capable of sending only
    a single IPv4 ADD_ADDR option. The 'pm_data' element of the connection
    sock will need to be expanded to handle multiple interfaces and IPv6.
    
    This is a skeleton interface definition for events generated by
    MPTCP.
    
    Co-developed-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Peter Krystad <peter.krystad@linux.intel.com>

--- include/linux/tcp.h
+++ include/linux/tcp.h
@@ -115,6 +115,15 @@ struct tcp_options_received {
 			use_ack:1,
 			ack64:1,
 			__unused:3;
+		u8	add_addr : 1,
+			family : 4;
+		u8	addr_id;
+		union {
+			struct	in_addr	addr;
+#if IS_ENABLED(CONFIG_IPV6)
+			struct	in6_addr addr6;
+#endif
+		};
 	} mptcp;
 #endif
 };
