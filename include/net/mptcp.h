7aa998f95681b40e132fb119e6994dabea2119b9
Peter Krystad <peter.krystad@linux.intel.com>
2019-11-08 15:22:44 +0000

    mptcp: Add IPv6 support
    Add hooks for IPv6 as needed and initialize MPTCP
    IPv6 support.
    
    Signed-off-by: Peter Krystad <peter.krystad@linux.intel.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- include/net/mptcp.h
+++ include/net/mptcp.h
@@ -146,4 +146,14 @@ static inline bool mptcp_sk_is_subflow(const struct sock *sk)
 }
 
 #endif /* CONFIG_MPTCP */
+
+#if IS_ENABLED(CONFIG_MPTCP_IPV6)
+int mptcpv6_init(void);
+#elif IS_ENABLED(CONFIG_IPV6)
+static inline int mptcpv6_init(void)
+{
+	return 0;
+}
+#endif
+
 #endif /* __NET_MPTCP_H */
