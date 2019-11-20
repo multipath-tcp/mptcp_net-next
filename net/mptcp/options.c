multipath-tcp/mptcp_net-next

commit 7d8b4fe573fb31e0e06e0238d3038bf2f8b0506b

mptcp: take extra established options into account
The previous conflict resolution was simple on purpose. But we need a
special case for the extra options.

Fixes: a17134e4116a (tgupdate: merge t/mptcp-Add-ADD_ADDR-handling base into t/mptcp-Add-ADD_ADDR-handling)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/options.c
@@ -427,16 +427,12 @@ bool mptcp_established_options(struct sock *sk, struct sk_buff *skb,
 		return false;
 
 	opts->suboptions = 0;
+
 	if (mptcp_established_options_mp(sk, &opt_size, remaining, opts))
 		ret = true;
 	else if (mptcp_established_options_dss(sk, skb, &opt_size, remaining,
 						 opts))
 		ret = true;
-	if (mptcp_established_options_addr(sk, &opt_size, remaining, opts)) {
-		*size += opt_size;
-		remaining -= opt_size;
-		ret = true;
-	}
 
 	/* we reserved enough space for the above options, and exceeding the
 	 * TCP option space would be fatal
@@ -446,6 +442,17 @@ bool mptcp_established_options(struct sock *sk, struct sk_buff *skb,
 
 	*size += opt_size;
 	remaining -= opt_size;
+
+	if (mptcp_established_options_addr(sk, &opt_size, remaining, opts)) {
+		if (opt_size > remaining) {
+			pr_debug("est opt: not enough space for addr: %u > %u",
+				 opt_size, remaining);
+		} else {
+			*size += opt_size;
+			remaining -= opt_size;
+			ret = true;
+		}
+	}
 
 	return ret;
 }

commit d66315ad0dff8ee520cd6f0db71a2fc83e68a970

mptcp: remove useless check
mptcp_established_options_addr() already check for tcp option space
exhaustion. The pr_debug() should never be reached.

Fixes: 7d8b4fe573fb (mptcp: take extra established options into account)
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/options.c
@@ -444,14 +444,9 @@ bool mptcp_established_options(struct sock *sk, struct sk_buff *skb,
 	remaining -= opt_size;
 
 	if (mptcp_established_options_addr(sk, &opt_size, remaining, opts)) {
-		if (opt_size > remaining) {
-			pr_debug("est opt: not enough space for addr: %u > %u",
-				 opt_size, remaining);
-		} else {
-			*size += opt_size;
-			remaining -= opt_size;
-			ret = true;
-		}
+		*size += opt_size;
+		remaining -= opt_size;
+		ret = true;
 	}
 
 	return ret;