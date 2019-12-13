83f9d7cee63f6ceebf633b646570392335c4c2f5
Matthieu Baerts <matthieu.baerts@tessares.net>
2019-12-13 14:47:06 +0100

    utils:gen-patch: fix no parameter
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Change-Id: I244babb373e34d646600b87a9cb272bcf970719d

--- utils/gen-patch.sh
+++ utils/gen-patch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git format-patch --notes "${1+--cover-letter}" -o patches/$(git rev-parse --abbrev-ref HEAD) "${1:--1}"
+git format-patch --notes ${1+--cover-letter} -o patches/$(git rev-parse --abbrev-ref HEAD) ${1:--1}
