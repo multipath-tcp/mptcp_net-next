de6a783139f1b35a3c98989e0f5e26e7cd1eea74
Matthieu Baerts <matthieu.baerts@tessares.net>
2019-12-13 19:37:25 +0100

    utils:add patch: apply patch on branch from parent
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Change-Id: Ic0ff93bd26791ab11dcb9cb1c8b0cb63b4581093

--- utils/add_patch.sh
+++ utils/add_patch.sh
@@ -10,12 +10,14 @@ echo "Adding new patch before ${TG_TOP}"
 git checkout "${TG_TOP}"
 [ -f .topdeps ]
 
+PARENT="$(cat .topdeps)"
+git checkout "${PARENT}"
 git branch -f tmp
 git checkout tmp
 git am -3 "${@}" || { echo "ERROR with git am. Please fix in another" \
                            "terminal (up to 'git am --continue') and press" \
                            "enter to continue"; read; }
-git checkout "$(git show "${TG_TOP}":.topdeps)"
+git checkout "${PARENT}"
 tg import "${TG_TOP}"..tmp
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git checkout "${TG_TOP}"
