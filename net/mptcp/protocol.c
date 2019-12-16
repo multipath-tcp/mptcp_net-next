77b04716a41791198fee563e0ad79f335757200c
Paolo Abeni <pabeni@redhat.com>
2019-12-16 22:29:56 +0100

    Squash-to: "mptcp: Implement MPTCP receive path"
    drop duplicate line indroduced by the 'Squash-to' commit.
    
    Note: this will cause some conflict in v1 patches. Resolution
    is trivial, howver the patches must be rebased on top of this
    one.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -559,8 +559,6 @@ void mptcp_finish_connect(struct sock *ssk)
 	subflow->map_subflow_seq = 1;
 	subflow->rel_write_seq = 1;
 
-	subflow->rel_write_seq = 1;
-
 	/* the socket is not connected yet, no msk/subflow ops can access/race
 	 * accessing the field below
 	 */
