d00a1c967b2b61cdb61d016c99adbb7dee62b340
Matthieu Baerts <matthieu.baerts@tessares.net>
2019-12-18 18:05:01 +0100

    ci:virtme: always give summary if errors
    Just in case they all fail, not just the last one.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Change-Id: Icbc769ba276c778d9ee870f66a585378229e5903

--- ci/ci_virtme.sh
+++ ci/ci_virtme.sh
@@ -15,6 +15,14 @@ launch_virtme_ref() {
 	bash -x patches/docker/Dockerfile.virtme.sh patches/virtme.sh
 }
 
+# $1: title ; [ $2: git ref ]
+launch_virtme_ref_log() {
+	if ! launch_virtme_ref "${2:-${1}}"; then
+		EXIT_RC=42
+		ISSUES+=("${1}")
+	fi
+}
+
 # $1: commit title
 get_commit_ref() {
 	git log -1 --format="%H" --grep "^${1}$" net-next..export || true
@@ -24,10 +32,7 @@ get_commit_ref() {
 launch_virtme_commit() { local ref
 	ref=$(get_commit_ref "${1}")
 	if [ -n "${ref}" ]; then
-		if ! launch_virtme_ref "${ref}"; then
-			EXIT_RC=42
-			ISSUES+=("${1}")
-		fi
+		launch_virtme_ref_log "${1}" "${ref}"
 	else
 		echo "Unable to find the commit '${1}'"
 		return 1
@@ -37,7 +42,7 @@ launch_virtme_commit() { local ref
 # We want to test the kselftests at the end of each series we are going to send
 launch_virtme_commit "mptcp: add basic kselftest for mptcp"
 launch_virtme_commit "mptcp: cope with later TCP fallback"
-launch_virtme_ref "export" || exit 42
+launch_virtme_ref_log "export"
 
 if [ "${EXIT_RC}" -ne 0 ]; then
 	echo "Errors with:"
