bfaaab6ed47e1b89f9368952d974ae935bc92f91
Peter Krystad <peter.krystad@linux.intel.com>
2020-01-17 17:56:08 +0000

    mptcp: Implement basic path manager
    Two features: 1) After an incoming connection is fully established
    announce a second local address and 2) When an outgoing connection
    is fully established, and an ADD_ADDR option has been received,
    initiate a secondary subflow to that address.
    
    The second local address may be configured through the
    net.mptcp.pm.announce_addr sysctl variable. If configured
    then for listeners this address will be announced with the
    ADD_ADDR option, and for clients the local socket will be bound
    to this address (or INADDR_ANY if not specified).
    
    Co-developed-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Peter Krystad <peter.krystad@linux.intel.com>

--- net/mptcp/pm.c
+++ net/mptcp/pm.c
@@ -169,6 +169,8 @@ void mptcp_pm_new_connection(struct mptcp_sock *msk, int server_side)
 
 	pm->server_side = server_side;
 	pm->token = msk->token;
+
+	mptcp_basic_new_connection(pm);
 }
 
 void mptcp_pm_fully_established(struct mptcp_sock *msk)
@@ -176,6 +178,8 @@ void mptcp_pm_fully_established(struct mptcp_sock *msk)
 	struct mptcp_pm_data *pm = &msk->pm;
 
 	pr_debug("msk=%p", msk);
+
+	mptcp_basic_fully_established(pm);
 
 	pm->fully_established = 1;
 }
@@ -205,6 +209,9 @@ void mptcp_pm_add_addr(struct mptcp_sock *msk, const struct in_addr *addr,
 	pm->remote_addr = *addr;
 	pm->remote_id = id;
 	pm->remote_family = AF_INET;
+
+	mptcp_basic_add_addr(pm);
+
 	pm->remote_valid = 1;
 }
 
@@ -262,4 +269,5 @@ int mptcp_pm_get_local_id(struct request_sock *req, struct sock *sk,
 
 void mptcp_pm_init(void)
 {
+	mptcp_basic_init();
 }
