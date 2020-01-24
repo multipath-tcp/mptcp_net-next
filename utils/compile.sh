577366d3a7573509be38f09e29e4820c2fb4a0af
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-01-24 17:53:26 +0100

    utils:compile: disable extra
    These are not influenting MPTCP, accelerate compilation from scratch.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Change-Id: Ie3edfd37530933c40d191d7142f3dc8744d2326d

--- utils/compile.sh
+++ utils/compile.sh
@@ -5,6 +5,7 @@ else
 	git log --oneline --no-decorate -1 HEAD
 fi
 
+echo | scripts/config --disable DRM --disable PCCARD --disable ATA --disable MD --disable PPS --disable SOUND --disable USB --disable IOMMU_SUPPORT --disable INPUT_LEDS --disable AGP --disable VGA_ARB --disable EFI --disable WLAN --disable WIRELESS --disable LOGO --disable NFS_FS --disable XFRM_USER --disable INET6_AH --disable INET6_ESP --disable NETDEVICES
 echo | ./scripts/config -e MPTCP -e MPTCP_IPV6
 #echo | ./scripts/config --disable MPTCP || true
 
