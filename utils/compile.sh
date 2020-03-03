33baa71288b6cb134c8453ee50c8df6a9b37e39c
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-03-03 19:57:06 +0100

    utils:compile: allow changing config
    e.g. ./.compile.sh -d MPTCP_IPV6 -d IPV6
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Change-Id: If3d5e1dad7c5f7d90d241f67fcc37c99d2b9ee3a

--- utils/compile.sh
+++ utils/compile.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 if [ -f .topmsg ]; then
 	./.title.sh
 else
@@ -8,5 +8,9 @@ fi
 echo | scripts/config --disable DRM --disable PCCARD --disable ATA --disable MD --disable PPS --disable SOUND --disable USB --disable IOMMU_SUPPORT --disable INPUT_LEDS --disable AGP --disable VGA_ARB --disable EFI --disable WLAN --disable WIRELESS --disable LOGO --disable NFS_FS --disable XFRM_USER --disable INET6_AH --disable INET6_ESP --disable NETDEVICES
 echo | ./scripts/config -e MPTCP -e MPTCP_IPV6 -e MPTCP_HMAC_TEST
 #echo | ./scripts/config --disable MPTCP || true
+
+if [ ${#} -gt 0 ]; then
+	echo | ./scripts/config "${@}"
+fi
 
 make -j$(nproc)
