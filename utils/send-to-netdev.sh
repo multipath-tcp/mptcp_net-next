04d312aca2bbf9b97d367182567e1328acd47923
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-06-08 16:13:57 +0200

    utils:send netdev: dry-run by default
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Change-Id: Icf6fdb8521be551d3f6eb136f8ef01781516d300

--- utils/send-to-netdev.sh
+++ utils/send-to-netdev.sh
@@ -2,4 +2,9 @@
 
 [ -f "${1}" ]
 
-git send-email --to="netdev@vger.kernel.org" --cc-cmd="./scripts/get_maintainer.pl --norolestats '${1}'" --annotate "${1}"
+DRY_RUN=""
+[ "${GO}" != 1 ] && DRY_RUN="--dry-run"
+
+git send-email ${DRY_RUN} --to="netdev@vger.kernel.org" --cc-cmd="./scripts/get_maintainer.pl --norolestats '${1}'" --annotate "${1}"
+
+if [ "${GO}" != 1 ]; then echo "Use GO=1 to actually send it"; fi
