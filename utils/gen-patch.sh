3893fd46e42d5b7fe5b8ead0505b0f61e257b340
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-06-17 14:29:43 +0200

    utils:gen-patch: always set mptcp-next prefix
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Change-Id: I5b229c0aae12cc8bd3a124bbd9b1e3f1c22fe1ce

--- utils/gen-patch.sh
+++ utils/gen-patch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-git format-patch --notes ${1+--cover-letter} -o patches/$(git rev-parse --abbrev-ref HEAD) ${1:--1}
+git format-patch --notes ${1+--cover-letter} --subject-prefix="PATCH mptcp-next" -o patches/$(git rev-parse --abbrev-ref HEAD) ${1:--1}
