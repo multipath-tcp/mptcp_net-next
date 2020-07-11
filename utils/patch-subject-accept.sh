79d5bfe0f927b0038b08dbb06613d8e8c6317486
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-07-11 16:49:16 +0200

    utils: accept patch: do not delegate superseded patches
    The might have been assigned to someone else for the review.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Change-Id: Iae027aafa4e6c803a1d73cebd15b92c44445f072

--- utils/patch-subject-accept.sh
+++ utils/patch-subject-accept.sh
@@ -4,6 +4,7 @@ SUBJECT="${1}"
 [ -n "${SUBJECT}" ]
 
 COMMIT="${2}"
+DELEGATE="matttbe"
 
 get_ids() {
 	bash "-${-}" ./.patch-subject-get-ids.sh "${1}"
@@ -14,10 +15,11 @@ for i in $(get_ids "${SUBJECT}"); do
 	echo "Set state ${STATE} to patch-id ${i}"
 	git-pw patch update \
 		--state "${STATE}" \
-		--delegate matttbe \
+		${DELEGATE:+--delegate "${DELEGATE}"} \
 		${COMMIT:+--commit-ref "${COMMIT}"} \
 		"${i}"
 	STATE="superseded"
+	unset DELEGATE # reviewer might be someone else
 done
 
 if [ -z "${i}" ]; then
