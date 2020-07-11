aaaf38b9aff8b9637477d11acb10cef6de11b026
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-07-11 10:26:18 +0200

    utils:list:accept: remove trailing whitespaces
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Change-Id: Ic1a7900eb1127c43c88824ac03d9bd7d983ccd42

--- utils/list-accepted.sh
+++ utils/list-accepted.sh
@@ -4,7 +4,7 @@ list() {
 	echo -n "By: "
 	git-pw patch list --state "${1}" -f simple -c Submitter | sed "s/ \(\S\+@\S\+\)//g" | tail -n+3 | sort -u | xargs -n2 -I{} echo -n "{}, " | sed 's/, $//g'
 	echo
-	PAGER=cat git-pw patch list --state "${1}" -f simple -c ID -c Name
+	PAGER=cat git-pw patch list --state "${1}" -f simple -c ID -c Name | sed 's/ $//g'
 	echo
 }
 
