19d747b0334f7e4aa8bb53faa8cd4002b8132f70
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-07-15 14:48:55 +0200

    utils:netdev patch: allow bpf and iproute targets
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Change-Id: I051ca2dcdacfea2d8065a0305596e75ff340b55e

--- utils/gen-patch-for-netdev.sh
+++ utils/gen-patch-for-netdev.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -ex
 
-[ "${1}" = "net" ] || [ "${1}" = "net-next" ]
-
-TARGET="${1}"
-shift
+case "${1}" in
+	"net" | "net-next" | "bpf" | "bpf-next" | "iproute2" | "iproute2-next")
+		TARGET="${1}"
+		shift
+		;;
+	*)
+		echo "Unknown target '${1}'"
+		exit 1
+		;;
+esac
 
 git format-patch --subject-prefix="PATCH ${TARGET}" -o "patches/$(git rev-parse --abbrev-ref HEAD)" --notes "${@}"
