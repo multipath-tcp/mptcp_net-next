a0ec44d9d3dcc8e55240bd1f3b1935d1faf35101
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-07-23 18:18:07 +0200

    utils:list gh: same format as the notes
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Change-Id: I190f6d5a4f8ffba57df2a26436436ec6e39875c8

--- utils/list-github.sh
+++ utils/list-github.sh
@@ -8,17 +8,21 @@ my_ghi() {
 
 LAST_WEEK="$(date -dlast-week +%Y-%m-%d)"
 
-echo "== Recently Open =="
+echo "    Recently opened (latest from last week: *TODO*)"
+echo
 my_ghi --state open --since "${LAST_WEEK}"
 
 echo
-echo "== Bugs =="
+echo "    Bugs (open and flagged as \"bug\")"
+echo
 my_ghi --state open -L bug
 
 echo
-echo "== In progress =="
+echo "    In Progress (open and assigned to someone)"
+echo
 my_ghi --state open -N bug | grep " @"
 
 echo
-echo "== Recently Close =="
+echo "    Recently closed (since last week)"
+echo
 my_ghi --state closed --since "${LAST_WEEK}"
