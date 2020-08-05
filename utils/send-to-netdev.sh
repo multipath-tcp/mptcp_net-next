a23b5e8e128102c6a6f29bf7097d0b3937fc8846
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-08-05 10:23:18 +0200

    utils:send to netdev: allow sending multiple patches
    with --cccmd in cmd line, we can only send one patch at a time.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/send-to-netdev.sh
+++ utils/send-to-netdev.sh
@@ -1,10 +1,24 @@
 #!/bin/bash -ex
 
-[ -f "${1}" ]
+[ -f "${1}" ] || [ -d "${1}" ]
+PATCH="${1}"
+shift
 
 DRY_RUN=""
 [ "${GO}" != 1 ] && DRY_RUN="--dry-run"
 
-git send-email ${DRY_RUN} --to="netdev@vger.kernel.org" --cc-cmd="./scripts/get_maintainer.pl --norolestats '${1}'" --annotate "${1}"
+SE_ID="${SE_ID:-linux}"
+
+# https://www.marcusfolkesson.se/blog/get_maintainers-and-git-send-email/
+add_config() {
+	local key="sendemail.${SE_ID}.${1}"
+	if [ -z "$(git config "${key}")" ]; then
+		git config "${key}" "${2}"
+	fi
+}
+add_config "tocmd" "\`pwd\`/scripts/get_maintainer.pl --nogit --nogit-fallback --norolestats --nol"
+add_config "cccmd" "\`pwd\`/scripts/get_maintainer.pl --nogit --nogit-fallback --norolestats --nom"
+
+git send-email ${DRY_RUN} --identity="${SE_ID}" --annotate "${@}" "${PATCH}"
 
 if [ "${GO}" != 1 ]; then echo "Use GO=1 to actually send it"; fi
