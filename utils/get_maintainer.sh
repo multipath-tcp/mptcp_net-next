0538c60125056dd1d2f1943b30a87062f26a4c2d
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-08-05 11:21:20 +0200

    utils:send to netdev: support cover-letter
    get_maintainer.pl doesn't support cover-letter. We need a wrapper.
    
    Here I add all to/cc from all patches.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

---
+++ utils/get_maintainer.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+#
+# cocci_cc - send cover letter to all mailing lists referenced in a patch series
+# intended to be used as 'git send-email --cc-cmd=cocci_cc ...'
+# done by Wolfram Sang in 2012-14, version 20140204 - WTFPLv2
+
+shopt -s extglob
+cd "$(git rev-parse --show-toplevel)" > /dev/null || exit 1
+
+args=()
+# get_maintainer.pl only accepts args without attached parameters
+while true; do
+    if [[ "${1}" == "--"* ]]; then
+        args+=("${1}")
+        shift
+    else
+        break
+    fi
+done
+
+patch="${1}"
+name="$(basename "${patch}")"
+num="${name%%-*}"
+prefix=""
+
+if [[ "${num}" == "v"* ]]; then
+    prefix="${num}-"
+    num="$(echo "${name}" | cut -d- -f2)"
+fi
+
+if [ "${num}" = "0000" ]; then
+    dir="${patch%/*}"
+    pre="${dir}/${prefix}"
+    for f in "${pre}"*.patch; do
+        fname="${f##${pre}}"
+        if [ "${fname%%-*}" = "0000" ]; then
+            continue
+        fi
+        scripts/get_maintainer.pl "${args[@]}" "${f}"
+    done | sort -u
+else
+    scripts/get_maintainer.pl "${args[@]}" "${patch}"
+fi
--- utils/send-to-netdev.sh
+++ utils/send-to-netdev.sh
@@ -16,8 +16,8 @@ add_config() {
 		git config "${key}" "${2}"
 	fi
 }
-add_config "tocmd" "\`pwd\`/scripts/get_maintainer.pl --nogit --nogit-fallback --norolestats --nol"
-add_config "cccmd" "\`pwd\`/scripts/get_maintainer.pl --nogit --nogit-fallback --norolestats --nom"
+add_config "tocmd" "\`pwd\`/.get_maintainer.sh --nogit --nogit-fallback --norolestats --nol"
+add_config "cccmd" "\`pwd\`/.get_maintainer.sh --nogit --nogit-fallback --norolestats --nom"
 
 git send-email ${DRY_RUN} --identity="${SE_ID}" --annotate "${@}" "${PATCH}"
 
