1089b7761bd692eb9dc92bc16094afd7507a63a3
Paolo Abeni <pabeni@redhat.com>
2020-08-15 05:03:24 +0000

    mptcp: rethink 'is writable' conditional
    Currently, when checking for the 'msk is writable' condition,
    we look at the individual subflows write space. That works
    well while we sends data via a single subflow, but will not
    as soon as we will enable concurrent xmit on multiple subflows.
    
    With this change msk become writable when both the the following
    conditions hold:
    - the socket has some free write space
    - there is at least a subflow with write free space
    
    We need to check the free write space ssk condition while not
    helding the ssk socket lock, so cache the free status in a
    subflow-level field, and access it with the appropriate
    barriers.
    
    Additonally we need to set the NOSPACE bit on all subflows
    before blocking.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- net/mptcp/subflow.c
+++ net/mptcp/subflow.c
@@ -996,8 +996,11 @@ static void subflow_write_space(struct sock *sk)
 	struct mptcp_subflow_context *subflow = mptcp_subflow_ctx(sk);
 	struct sock *parent = subflow->conn;
 
-	sk_stream_write_space(sk);
-	if (sk_stream_is_writeable(sk)) {
+	if (!sk_stream_memory_free(sk))
+		return;
+
+	WRITE_ONCE(subflow->writable, true);
+	if (sk_stream_is_writeable(parent)) {
 		set_bit(MPTCP_SEND_SPACE, &mptcp_sk(parent)->flags);
 		smp_mb__after_atomic();
 		/* set SEND_SPACE before sk_stream_write_space clears NOSPACE */
@@ -1199,6 +1202,7 @@ static struct mptcp_subflow_context *subflow_create_ctx(struct sock *sk,
 
 	rcu_assign_pointer(icsk->icsk_ulp_data, ctx);
 	INIT_LIST_HEAD(&ctx->node);
+	WRITE_ONCE(ctx->writable, true);
 
 	pr_debug("subflow=%p", ctx);
 
