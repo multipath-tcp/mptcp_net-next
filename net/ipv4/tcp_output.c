8fd4ed432f3beb840d44be2efc2edebd860f68d3
Paolo Abeni <pabeni@redhat.com>
2020-09-09 05:08:14 +0000

    tcp: propagate MPTCP skb extensions on xmit splits
    When the TCP stack splits a packet on the write queue, the tail
    half currently lose the associated skb extensions, and will not
    carry the DSM on the wire.
    
    The above does not cause functional problems and is allowed by
    the RFC, but interact badly with GRO and RX coalescing, as possible
    candidates for aggregation will carry different TCP options.
    
    This change tries to improve the MPTCP behavior, propagating the
    skb extensions on split.
    
    Additionally, we must prevent the MPTCP stack from updating the
    mapping after the split occur: that will both violate the RFC and
    fool the reader.
    
    Reviewed-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -1569,6 +1569,7 @@ int tcp_fragment(struct sock *sk, enum tcp_queue tcp_queue,
 	if (!buff)
 		return -ENOMEM; /* We'll just try again later. */
 	skb_copy_decrypted(buff, skb);
+	mptcp_skb_ext_copy(buff, skb);
 
 	sk_wmem_queued_add(sk, buff->truesize);
 	sk_mem_charge(sk, buff->truesize);
@@ -2124,6 +2125,7 @@ static int tso_fragment(struct sock *sk, struct sk_buff *skb, unsigned int len,
 	if (unlikely(!buff))
 		return -ENOMEM;
 	skb_copy_decrypted(buff, skb);
+	mptcp_skb_ext_copy(buff, skb);
 
 	sk_wmem_queued_add(sk, buff->truesize);
 	sk_mem_charge(sk, buff->truesize);
@@ -2394,6 +2396,7 @@ static int tcp_mtu_probe(struct sock *sk)
 
 	skb = tcp_send_head(sk);
 	skb_copy_decrypted(nskb, skb);
+	mptcp_skb_ext_copy(nskb, skb);
 
 	TCP_SKB_CB(nskb)->seq = TCP_SKB_CB(skb)->seq;
 	TCP_SKB_CB(nskb)->end_seq = TCP_SKB_CB(skb)->seq + probe_size;
