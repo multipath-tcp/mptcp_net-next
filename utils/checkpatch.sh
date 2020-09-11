34427e8471ce0a7f233e14f4469456ff7a870b2c
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-09-11 15:57:37 +0200

    utils:checkpatch: fix arg orders
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/checkpatch.sh
+++ utils/checkpatch.sh
@@ -2,8 +2,8 @@
 
 checkpatch() {
 	./scripts/checkpatch.pl --strict --codespell --codespellfile \
-		${CP_EXTRA_ARGS} \
 		/usr/lib/python3/dist-packages/codespell_lib/data/dictionary.txt \
+		${CP_EXTRA_ARGS} \
 		"${1}"
 }
 
