836534cf59a4ccfce5c7c16d66f9bfc77b764d7a
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-10-03 12:51:58 +0200

    utils:add_patch: accept patchwork patch/series ID
    Not just files.
    
    Also adapt checkpatch.sh to accept '--git' param.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/add_patch.sh
+++ utils/add_patch.sh
@@ -1,14 +1,29 @@
 #! /bin/bash -ex
 
-[ -f "${1}" ]
-
 TOP="t/upstream"
 TG_TOP=${TG_TOP:-${TOP}}
+
+# $1: type; $2: ID
+is_pw() {
+	[[ "${2}" =~ ^[0-9]+$ ]] || return $?
+	git-pw "${1}" list -c ID -f simple | grep -q "^${2}$"
+}
+
+if [ -f "${1}" ]; then
+	MODE="files"
+elif is_pw series "${1}"; then
+	MODE="series"
+elif is_pw patch "${1}"; then
+	MODE="patch"
+else
+	echo "'${1}': Not a file nor a patchwork ref" >&2
+	exit 1
+fi
 
 tg_up_err() {
 	echo "Please fix the conflicts in another terminal." \
 	     "End with ./.end-conflict.sh, then press Enter to continue."
-	read
+	read -r
 }
 
 
@@ -22,6 +37,52 @@ tg_update() {
 	fi
 }
 
+apply_patches_files() {
+	if ! git am -3 "${@}"; then
+		echo "ERROR with git am. Please fix in another terminal (up to" \
+		     "'git am --continue' included) and press ENTER to continue."
+		read -r
+	fi
+}
+
+apply_patches_git_pw() {
+	for i in "${@}"; do
+		if ! git-pw "${MODE}" apply ${GIT_PW_ARG:+"${GIT_PW_ARG}"} "${i}"; then
+			echo "ERROR with 'git-pw ${MODE} apply ${i}'. Please" \
+			     "resolve in another terminal and press ENTER to" \
+			     "continue."
+			read -r
+		fi
+	done
+}
+
+apply_patches_series() {
+	GIT_PW_ARG="" apply_patches_git_pw "${@}"
+}
+
+apply_patches_patch() {
+	GIT_PW_ARG="--deps" apply_patches_git_pw "${@}"
+}
+
+apply_patches() {
+	"apply_patches_${MODE}" "${@}"
+}
+
+# $1: git base; $2: git end
+checkpatch() {
+	if ! ./.checkpatch.sh --git "${1}..${2}"; then
+		echo "Error with checkpatch. Use 'git rebase -i \"${1}\"' to" \
+		     "fix anything if needed and press Enter to continue."
+		read -r
+	fi
+}
+
+accept_patches() { local subject
+	while read -r subject; do
+		./.patch-subject-accept.sh "${subject}"
+	done <<< "$(git log --format="%s" "${1}")"
+}
+
 
 # the last commit is always a DO-NOT-MERGE one.
 if [ "${TG_TOP}" = "${TOP}" ]; then
@@ -32,13 +93,7 @@ if [ "${TG_TOP}" = "${TOP}" ]; then
 	done
 fi
 
-echo "Adding new patch before ${TG_TOP}"
-
-for PATCH in "${@}"; do
-	echo "Checkpatch on ${PATCH}"
-	./.checkpatch.sh "${PATCH}" || { echo "Error with checkpatch. Press" \
-					      "enter to continue"; read; }
-done
+echo "Adding new patch(es) before ${TG_TOP}"
 
 git checkout "${TG_TOP}"
 [ -f .topdeps ]
@@ -47,19 +102,16 @@ PARENT="$(cat .topdeps)"
 git checkout "${PARENT}"
 git branch -f tmp
 git checkout tmp
-git am -3 "${@}" || { echo "ERROR with git am. Please fix in another" \
-                           "terminal (up to 'git am --continue') and press" \
-                           "enter to continue"; read; }
+apply_patches "${@}"
+checkpatch "${PARENT}" "tmp"
 git checkout "${PARENT}"
 tg import "${PARENT}"..tmp
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git checkout "${TG_TOP}"
-echo ${BRANCH} > .topdeps
+echo "${BRANCH}" > .topdeps
 git commit -sm "tg: switch to ${BRANCH}" .topdeps
 tg_update
 git checkout ${TOP}
 tg_update
 tg push
-for PATCH in "${@}"; do
-	./.patch-file-accept.sh "${PATCH}"
-done
+accept_patches "${PARENT}"..tmp
--- utils/checkpatch.sh
+++ utils/checkpatch.sh
@@ -4,9 +4,13 @@ checkpatch() {
 	./scripts/checkpatch.pl --strict --codespell --codespellfile \
 		/usr/lib/python3/dist-packages/codespell_lib/data/dictionary.txt \
 		${CP_EXTRA_ARGS} \
-		"${1}"
+		"${@}"
 }
 
-for patch in "${@}"; do
-	checkpatch "${patch}"
-done
+if [ "${1}" = "--git" ]; then
+	checkpatch "${@}"
+else
+	for patch in "${@}"; do
+		checkpatch "${patch}"
+	done
+fi
