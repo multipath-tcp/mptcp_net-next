e30a9740f9b1c8400b9d5130cbea6a37e4449e4c
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-12-08 21:09:11 +0100

    utils:lib: display backtrace if set -e
    Easier to debug issues.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/lib.sh
+++ utils/lib.sh
@@ -21,3 +21,51 @@ printinfo() {
 printerr() {
 	print_color "${COLOR_RED}" "${@}" >&2
 }
+
+# Trap to display a message when there is an error (set -e)
+# Src: http://stackoverflow.com/a/185900
+# Available variables: https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
+trap_error() {
+	local cmd="${BASH_COMMAND}"
+	# to keep the last commands from the error and not these, disable 'set -x'
+	if [[ $- =~ x ]]; then
+		set +x
+		local had_set_x=1
+	else
+		local had_set_x=0
+	fi
+
+	local parent_lineno="${1}"
+	local message="${2}"
+	local code="${3:-1}"
+	if [[ -n "${message}" ]] ; then
+		printerr "${message}"
+	else
+		printerr "Error when launching" \
+		         "'${CURRENT_SCRIPT[*]} ${BASH_ARGV[*]}' on or near" \
+		         "line ${parent_lineno} when calling '${cmd}':"
+		local i
+		for (( i=0; i<${#BASH_SOURCE[@]}; i++ )); do
+			printerr " - ${BASH_SOURCE[${i}]}:${FUNCNAME[${i}]}:${BASH_LINENO[${i}-1]}"
+		done
+	fi
+
+	printerr "\nThe command '${cmd}' has failed with error ${code}"
+
+	# if with 'set -e', exit with the right rc
+	if [[ $- =~ e ]]; then
+		exit "${code}"
+	fi
+	if [ -n "${had_set_x}" ]; then
+		set -x
+	fi
+}
+
+# trap to understand where we had an issue but only with 'set -e'
+if [[ $- =~ e ]]; then
+	trap 'trap_error ${LINENO}' ERR
+fi
+
+if [ "${DEBUG:-}" == "1" ]; then
+	set -x
+fi
