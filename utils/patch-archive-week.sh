bd8899e66f23637e5b21b0eb7a67113a807cc7ac
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-12-08 21:09:59 +0100

    utils:patch-archive-week: archive old patches
    Instead of doing that manually.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

---
+++ utils/patch-archive-week.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+
+git-pw patch list \
+	--limit 250 \
+	--state Deferred \
+	--state Accepted \
+	--state Superseded \
+	-f simple \
+	-c ID | \
+		tail -n +3 | \
+		xargs -r git-pw patch update --archived true
+
+# Patchwork on ozlab has an extra state
+git config --local pw.states \
+	new,under-review,accepted,rejected,rfc,not-applicable,changes-requested,awaiting-upstream,superseded,deferred,needs-review-ack
+
+git-pw patch list \
+	--limit 250 \
+	--state New \
+	-f simple \
+	-c ID | \
+		tail -n +3 | \
+		xargs -r git-pw patch update --state needs-review-ack
