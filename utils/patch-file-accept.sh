e480312f5556d16610ba3c6ee4a4a38420ee83fe
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-12-08 20:52:33 +0100

    utils:patch file accept: support subject on 2 lines
    or more.
    
    When parsing an MBOX file, we can have it on two lines.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/patch-file-accept.sh
+++ utils/patch-file-accept.sh
@@ -5,7 +5,22 @@ FILE="${1}"
 
 COMMIT="${2}"
 
-SUBJECT="$(grep "^Subject: " "${FILE}" | head -n1 | sed -e "s/\[.\+\] //g;s/^Subject: //;s/\\r$//g")"
+SUBJECT_2L="$(grep -A1 "^Subject: " "${FILE}" | head -n2)"
+[ -n "${SUBJECT_2L}" ]
+
+# remove 'Subject: '
+SUBJECT_2L="${SUBJECT_2L:9}"
+
+# we can have the subject on 2 lines (or more)
+if echo "${SUBJECT_2L}" | tail -n1 | grep -q "^\S"; then
+	SUBJECT="$(echo "${SUBJECT_2L}" | head -n1)"
+else
+	SUBJECT="$(echo "${SUBJECT_2L}" | tr -d '\n')"
+fi
+
+# remove '[xxx]' and \r
+SUBJECT="$(echo ${SUBJECT} | sed -e "s/\[.\+\] //g;s/\\r$//g")"
+
 [ -n "${SUBJECT}" ]
 
 bash "-${-}" ./.patch-subject-accept.sh "${SUBJECT}" "${COMMIT}"
