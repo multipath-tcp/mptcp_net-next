0fc970aab1c0ff84bb6bd4b2742c7a0671cd8b97
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-12-08 20:48:13 +0100

    utils:am: set patch' SHA, not SoB
    When accepting a patch on Patchwork, take the SHA of the patch we just
    applied, not the SoB we might add.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/am-patch.sh
+++ utils/am-patch.sh
@@ -21,7 +21,7 @@ exit_trap() {
 	fi
 }
 
-am_files() { local nb subject
+am_files() { local nb subject sha
 	if git am -3 -s "${1}" || { git am --abort && git am -s "${1}"; }; then
 		subject="$(grep "^Subject: " "${1}" | head -n1)"
 		if echo "${subject}" | grep -q "\[PATCH.* [0-9]\+/[0-9]\+\] "; then
@@ -29,6 +29,7 @@ am_files() { local nb subject
 			nb=" patch $(echo "${subject}" | \
 				sed "s#.*\[PATCH.* \([0-9]\+/[0-9]\+\)\].\+#\1#g")"
 		fi
+		sha=$(git rev-parse HEAD)
 
 		printf "\n\t- %s: \"squashed\"%s in \"%s\"\n" \
 			"$(git rev-parse --short HEAD)" \
@@ -37,7 +38,7 @@ am_files() { local nb subject
 		printf "\ttrying signed-off\n\n"
 		./.signed-off.sh
 
-		bash ./.patch-file-accept.sh "${1}" "$(git rev-parse HEAD)"
+		bash "-${-}" ./.patch-file-accept.sh "${1}" "${sha}"
 		exit
 	fi
 
