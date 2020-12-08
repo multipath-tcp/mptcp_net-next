multipath-tcp/mptcp_net-next

commit d0fcd968be5e3bcf411111e36a1fa0370a794af3

utils:add_patch: move features to ./.publish.sh
To help in case of conflicts during the update and print useful info.

Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

utils/publish.sh
@@ -1,5 +1,37 @@
 #! /bin/bash -ex
 
-git checkout t/upstream
-tg update
-tg push
+TG_TOP="${TG_TOP:-t/upstream}"
+TG_PUSH="${TG_PUSH:-1}"
+
+tg_up_err() {
+	echo "Please fix the conflicts in another terminal." \
+	     "End with ./.end-conflict.sh, then press Enter to continue."
+	read -r
+}
+
+tg_update() {
+	if ! tg update; then
+		tg_up_err
+
+		while ! tg update --continue; do
+			tg_up_err
+		done
+	fi
+}
+
+
+git checkout "${TG_TOP}"
+tg_update
+
+if [ "${TG_PUSH}" = 1 ]; then
+	OLD_REV="$(git rev-parse --short "origin/${TG_TOP}")"
+	NEW_REV="$(git rev-parse --short "${TG_TOP}")"
+
+	tg push
+
+	echo "New patches:"
+	git log --format="- %h: %s" --reverse --no-merges "${OLD_REV}..${NEW_REV}" | \
+		grep -v -e "^\S\+ tg " -e "^\S\+ tg: "
+
+	echo "- Results: ${OLD_REV}..${NEW_REV}"
+fi

utils/add_patch.sh
@@ -20,23 +20,6 @@ else
 	exit 1
 fi
 
-tg_up_err() {
-	echo "Please fix the conflicts in another terminal." \
-	     "End with ./.end-conflict.sh, then press Enter to continue."
-	read -r
-}
-
-
-tg_update() {
-	if ! tg update; then
-		tg_up_err
-
-		while ! tg update --continue; do
-			tg_up_err
-		done
-	fi
-}
-
 apply_patches_files() {
 	if ! git am -3 "${@}"; then
 		echo "ERROR with git am. Please fix in another terminal (up to" \
@@ -98,20 +81,29 @@ echo "Adding new patch(es) before ${TG_TOP}"
 git checkout "${TG_TOP}"
 [ -f .topdeps ]
 
+# Apply patches in a tmp branch created from the parent commit of TG_TOP
 PARENT="$(cat .topdeps)"
 git checkout "${PARENT}"
 git branch -f tmp
 git checkout tmp
 apply_patches "${@}"
+
+# Make sure all patches are OK
 checkpatch "${PARENT}" "tmp"
+
+# Import new patches with tg import
 git checkout "${PARENT}"
 tg import "${PARENT}"..tmp
+
+# Change the dep of TG_TOP to point to the last new patch
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git checkout "${TG_TOP}"
 echo "${BRANCH}" > .topdeps
 git commit -sm "tg: switch to ${BRANCH}" .topdeps
-tg_update
-git checkout ${TOP}
-tg_update
-tg push
+TG_PUSH=0 TG_TOP="${TG_TOP}" ./.publish.sh
+
+# update the tree
+TG_PUSH=1 TG_TOP="${TOP}" ./.publish.sh
+
+# Mark as done
 accept_patches "${PARENT}"..tmp

commit 3360821088bd141fa7549b2e493dfbf86c1bd30f

utils:publish: fix TG filtering
Fixes: d0fcd96 ("utils:add_patch: move features to ./.publish.sh")
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

utils/publish.sh
@@ -35,7 +35,7 @@ if [ "${TG_PUSH}" = 1 ]; then
 
 	echo "New patches:"
 	git log --format="- %h: %s" --reverse --no-merges "${OLD_REV}..${NEW_REV}" | \
-		grep -v -e "^\S\+ tg " -e "^\S\+ tg: "
+		grep -v -e "^- \S\+ tg " -e "^- \S\+ tg: " || true
 
 	echo "- Results: ${OLD_REV}..${NEW_REV}"
 fi