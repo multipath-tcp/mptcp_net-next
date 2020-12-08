2edabcbd78c832e7ccde7dfd7aa6b50a71e550b5
Matthieu Baerts <matthieu.baerts@tessares.net>
2020-12-08 20:28:41 +0100

    utils:compile: enable IPv6
    Required by MPTCP_IPV6
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/all-compile.sh
+++ utils/all-compile.sh
@@ -10,5 +10,5 @@ set -e
 echo | ./scripts/config -d MPTCP -d MPTCP_IPV6 -d MPTCP_KUNIT_TESTS || true
 make -j$(nproc)
 
-echo | ./scripts/config -e MPTCP -e MPTCP_IPV6 -m MPTCP_KUNIT_TESTS
+echo | ./scripts/config -e MPTCP -e MPTCP_IPV6 -e IPV6 -m KUNIT -m MPTCP_KUNIT_TESTS
 make -j$(nproc)
