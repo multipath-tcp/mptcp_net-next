c99cfc25d3d3dcf0f875df17c7c5ad257bde2f4b
Geliang Tang <geliangtang@gmail.com>
2021-01-20 12:38:42 +0000

    mptcp: create the listening socket for new port
    This patch creates a listening socket when an address with a port-number
    is added by PM netlink. Then binds the new port to the socket, and
    listens for new connections.
    
    When the address is removed or the addresses are flushed by PM netlink,
    release the listening socket.
    
    Reviewed-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: Geliang Tang <geliangtang@gmail.com>

--- net/mptcp/subflow.c
+++ net/mptcp/subflow.c
@@ -1088,9 +1088,9 @@ void mptcpv6_handle_mapped(struct sock *sk, bool mapped)
 }
 #endif
 
-static void mptcp_info2sockaddr(const struct mptcp_addr_info *info,
-				struct sockaddr_storage *addr,
-				unsigned short family)
+void mptcp_info2sockaddr(const struct mptcp_addr_info *info,
+			 struct sockaddr_storage *addr,
+			 unsigned short family)
 {
 	memset(addr, 0, sizeof(*addr));
 	addr->ss_family = family;
