multipath-tcp/mptcp_net-next

commit 641f86dad0189b0710abea8ff2a3fae0cdd28b23

ci:virtme:tap: only write with 'tee'
The main issue was to write to the output file while the tee process was
still active.

While modifying this, certainly better to write everything with tee, one
redirection at a time and extract what needs to be written with 'tap
prefix' and what doesn't need that.

Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

ci/virtme.sh
@@ -127,20 +127,30 @@ prepare() { local old_pwd mode
 TAP_PREFIX="${PWD}/tools/testing/selftests/kselftest/prefix.pl"
 
 # \$1: file ; \$2+: commands
-tap() { local out fname
+tap() { local out fname rc=0
         out="\${1}"
         fname="\$(basename \${out})"
         shift
 
-        echo "TAP version 13" > "\${out}"
-        echo "1..1" >> "\${out}"
+        # init
         {
-                if "\${@}" 2>&1; then
-                        echo "ok 1 test: \${fname}" >> "\${out}"
-                else
-                        echo "not ok 1 test: \${fname} # exit=\${?}" >> "\${out}"
-                fi
+                echo "TAP version 13"
+                echo "1..1"
+        } | tee "\${out}"
+
+        # Exec the command and pipe in tap prefix
+        {
+                "\${@}" 2>&1 || rc=\${?}
         } | "\${TAP_PREFIX}" | tee -a "\${out}"
+
+        # summary
+        {
+                if [ \${rc} -eq 0 ]; then
+                        echo "ok 1 test: \${fname}"
+                else
+                        echo "not ok 1 test: \${fname} # exit=\${rc}"
+                fi
+        } | tee -a "\${out}"
 }
 
 # kunit

commit 27ac043a3cb609d7e419684696d5202c9d76e5ba

ci:virtme:tap: properly detect error
rc variable was updated in a subshell only. It was then never set to
another value in tap() function.

Now we use PIPESTATUS array to check the value of the first execution
and make sure we don't miss an error.

Fixes: 641f86d ("ci:virtme:tap: only write with 'tee'")
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

ci/virtme.sh
@@ -127,7 +127,7 @@ prepare() { local old_pwd mode
 TAP_PREFIX="${PWD}/tools/testing/selftests/kselftest/prefix.pl"
 
 # \$1: file ; \$2+: commands
-tap() { local out tmp fname rc=0
+tap() { local out tmp fname rc
         out="\${1}"
         shift
 
@@ -142,10 +142,9 @@ tap() { local out tmp fname rc=0
         } | tee "\${out}"
 
         # Exec the command and pipe in tap prefix + store for later
-        {
-                "\${@}" 2>&1 || rc=\${?}
-        } | "\${TAP_PREFIX}" | tee "\${tmp}"
+        "\${@}" 2>&1 | "\${TAP_PREFIX}" | tee "\${tmp}"
         # output to stdout now to see the progression
+        rc=\${PIPESTATUS[0]}
 
         # summary
         {