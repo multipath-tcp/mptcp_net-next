1d2a97b377e3a8f85c039fc7e2f7ade7f04260be
Matthieu Baerts <matthieu.baerts@tessares.net>
2021-02-15 16:21:59 +0100

    utils:add patch bottom: exit if TG_TOP is already def
    Then maybe the wrong script was used.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/add_patch_bottom.sh
+++ utils/add_patch_bottom.sh
@@ -1,3 +1,9 @@
 #! /bin/bash -e
+if [ -n "${TG_TOP}" ]; then
+	echo "TG_TOP is already defined: ${TG_TOP}"
+	exit 1
+fi
+
 git checkout t/upstream
+
 TG_TOP=$(tg info --series | head -n1 | awk '{ print $1 }') ./.add_patch.sh "${@}"
