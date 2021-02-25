5f8fb6771495aa2adea32b3f7d09f31dd608d82b
Matthieu Baerts <matthieu.baerts@tessares.net>
2021-02-25 16:21:19 +0100

    utils: list pending: limit one inst. per line
    to improve readability.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/list-pending.sh
+++ utils/list-pending.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
-pwclient list -a no -f "%{id}: %{state}: %{name}" | grep -v ": Accepted:" | grep -v ": Superseded:" | grep -v ": Deferred:"
+pwclient list -a no -f "%{id}: %{state}: %{name}" | \
+	grep -v ": Accepted:" | \
+	grep -v ": Superseded:" | \
+	grep -v ": Deferred:"
 
 [ "${1}" = "NO_AUTH" ] && exit 0
 
 echo
 echo "Duplicated:"
-pwclient list -a no -f "%{state}:%{name}" | grep -v -e "^Superseded:" -e "^Deferred:" | cut -d: -f2- | sed 's/\[.\+\]//g;s/^\s\+//g' | sort | uniq -cd
+pwclient list -a no -f "%{state}:%{name}" | \
+	grep -v -e "^Superseded:" -e "^Deferred:" | \
+	cut -d: -f2- | \
+	sed 's/\[.\+\]//g;s/^\s\+//g' | \
+	sort | \
+	uniq -cd
 
 echo
 echo -n "By: "
-pwclient list -a no -f "%{state}#%{submitter}" | grep -v "^Accepted#" | grep -v "^Superseded#" | grep -v "^Deferred#" | cut -d\# -f2- | cut -d\< -f1 | sed "s/ $//g" | sort -u | xargs -I{} echo -n "{}, "
+pwclient list -a no -f "%{state}#%{submitter}" | \
+	grep -v "^Accepted#" | \
+	grep -v "^Superseded#" | \
+	grep -v "^Deferred#" | \
+	cut -d\# -f2- | \
+	cut -d\< -f1 | \
+	sed "s/ $//g" | \
+	sort -u | \
+	xargs -I{} echo -n "{}, "
 echo
