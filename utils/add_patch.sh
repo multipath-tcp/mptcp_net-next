multipath-tcp/mptcp_net-next

commit b2cdc35acc8398d49e5b4464f261d4bf9ce299aa

utils:add/am patch: accept the mode as 1st arg
To make sure it is clear that what we give is a patch or a series, we
can pass:

  patch XXX

or:

  series YYY

and not just XXX or YYY.

Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

utils/add_patch.sh
@@ -42,8 +42,14 @@ apply_patches_patch() {
 	GIT_PW_ARG="--no-deps" apply_patches_git_pw "${@}"
 }
 
-apply_patches() {
-	"apply_patches_${MODE}" "${@}"
+apply_patches() { local p patches=()
+	for p in "${@}"; do
+		if [ "${p}" = "patch" ] || [ "${p}" = "series" ]; then
+			continue;
+		fi
+		patches+=("${p}")
+	done
+	"apply_patches_${MODE}" "${patches}"
 }
 
 print_rebase_pause() {

utils/am-patch.sh
@@ -4,8 +4,6 @@
 # shellcheck source=./lib.sh
 source ./.lib.sh
 
-PATCH="${1}"
-
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 printinfo "Current branch is: ${BRANCH}"
 head -n2 .topmsg | grep "^Subject: "
@@ -17,7 +15,7 @@ fi
 
 check_sync_upstream || exit 1
 
-MODE=$(SERIES=0 bash "-${-}" ./.get_arg_mode.sh "${PATCH}")
+MODE=$(SERIES=0 bash "-${-}" ./.get_arg_mode.sh "${1}")
 
 TMP_FILE=""
 
@@ -61,14 +59,18 @@ am_series() {
 	exit 1
 }
 
-am_patch() {
+am_patch() { local patch="${1}"
 	TMP_FILE=$(mktemp)
 
-	git-pw patch download "${1}" "${TMP_FILE}"
+	if [ "${patch}" = "patch" ]; then
+		patch="${2}"
+	fi
+
+	git-pw patch download "${patch}" "${TMP_FILE}"
 
 	am_files "${TMP_FILE}"
 }
 
 trap 'exit_trap' EXIT
 
-"am_${MODE}" "${PATCH}"
+"am_${MODE}" "${@}"

utils/get_arg_mode.sh
@@ -15,6 +15,8 @@ is_pw() { local mode id
 
 if [ -f "${1}" ]; then
 	echo "files"
+elif [ "${1}" = "patch" ] || [ "${1}" = "series" ]; then
+	echo "${1}"
 elif [ "${SERIES}" = 1 ] && is_pw series "${1}"; then
 	echo "series"
 elif is_pw patch "${1}" \

commit 94e404a5e89f9a1ec14bcbd63763c11715bb9fc7

utils:add patch: fix support multiple patches
The array was not used properly.

Fixes: b2cdc35 ("utils:add/am patch: accept the mode as 1st arg")
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

utils/add_patch.sh
@@ -49,7 +49,7 @@ apply_patches() { local p patches=()
 		fi
 		patches+=("${p}")
 	done
-	"apply_patches_${MODE}" "${patches}"
+	"apply_patches_${MODE}" "${patches[@]}"
 }
 
 print_rebase_pause() {