4946eca4befe4c4e68ccf12bb3fab204c084c9da
Nicolas Rybowski <nicolas.rybowski@tessares.net>
2021-09-19 05:46:47 +0000

    bpf: add 'bpf_mptcp_sock' structure and helper
    In order to precisely identify the parent MPTCP connection of a subflow,
    it is required to access the mptcp_sock's token which uniquely identify a
    MPTCP connection.
    
    This patch adds a new structure 'bpf_mptcp_sock' exposing the 'token' field
    of the 'mptcp_sock' extracted from a subflow's 'tcp_sock'. It also adds the
    declaration of a new BPF helper of the same name to expose the newly
    defined structure in the userspace BPF API.
    
    This is the foundation to expose more MPTCP-specific fields through BPF.
    
    Currently, it is limited to the field 'token' of the msk but it is
    easily extensible.
    
    Acked-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Acked-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: Nicolas Rybowski <nicolas.rybowski@tessares.net>

--- tools/include/uapi/linux/bpf.h
+++ tools/include/uapi/linux/bpf.h
@@ -4898,6 +4898,14 @@ union bpf_attr {
  *		**-EINVAL** if *flags* is not zero.
  *
  *		**-ENOENT** if architecture does not support branch records.
+ *
+ * struct bpf_mptcp_sock *bpf_mptcp_sock(struct bpf_sock *sk)
+ *	Description
+ *		This helper gets a **struct bpf_mptcp_sock** pointer from a
+ *		**struct bpf_sock** pointer.
+ *	Return
+ *		A **struct bpf_mptcp_sock** pointer on success, or **NULL** in
+ *		case of failure.
  */
 #define __BPF_FUNC_MAPPER(FN)		\
 	FN(unspec),			\
@@ -5077,6 +5085,7 @@ union bpf_attr {
 	FN(get_attach_cookie),		\
 	FN(task_pt_regs),		\
 	FN(get_branch_snapshot),	\
+	FN(mptcp_sock),			\
 	/* */
 
 /* integer value in 'imm' field of BPF_CALL instruction selects which helper
@@ -5422,6 +5431,10 @@ struct bpf_tcp_sock {
 	__u32 delivered_ce;	/* Like the above but only ECE marked packets */
 	__u32 icsk_retransmits;	/* Number of unrecovered [RTO] timeouts */
 	__u32 is_mptcp;		/* Is MPTCP subflow? */
+};
+
+struct bpf_mptcp_sock {
+	__u32 token;		/* msk token */
 };
 
 struct bpf_sock_tuple {
