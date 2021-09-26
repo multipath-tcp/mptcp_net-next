a570f9053023c42a5636fdae945082f157e8aa14
Geliang Tang <geliangtang@gmail.com>
2021-09-26 22:29:35 +0800

    mptcp: infinite mapping receiving
    This patch added the infinite mapping receiving logic. When the infinite
    mapping is received, set the map_data_len of the subflow to 0.
    
    In subflow_check_data_avail, only reset the subflow when the map_data_len
    of the subflow is non-zero.
    
    Signed-off-by: Geliang Tang <geliangtang@gmail.com>
    Message-Id: <3a055280313efe6030d8f0130662a662dd9c734e.1632666254.git.geliangtang@gmail.com>

--- net/mptcp/subflow.c
+++ net/mptcp/subflow.c
@@ -968,6 +968,7 @@ static enum mapping_status get_mapping_status(struct sock *ssk,
 	data_len = mpext->data_len;
 	if (data_len == 0) {
 		MPTCP_INC_STATS(sock_net(ssk), MPTCP_MIB_INFINITEMAPRX);
+		subflow->map_data_len = 0;
 		return MAPPING_INVALID;
 	}
 
@@ -1180,7 +1181,7 @@ fallback:
 			return true;
 		}
 
-		if (subflow->mp_join || subflow->fully_established) {
+		if ((subflow->mp_join || subflow->fully_established) && subflow->map_data_len) {
 			/* fatal protocol error, close the socket.
 			 * subflow_error_report() will introduce the appropriate barriers
 			 */
