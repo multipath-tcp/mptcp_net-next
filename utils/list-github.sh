3dfcd30604e607af70bcdfc2720147891e5e6e4f
Matthieu Baerts <matthieu.baerts@tessares.net>
2021-10-14 12:31:28 +0200

    utils:list gh: add new lines
    Instead of adding them manually.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/list-github.sh
+++ utils/list-github.sh
@@ -13,25 +13,30 @@ echo
 my_ghi --state open --since "${LAST_WEEK}"
 
 echo
+echo
 echo "    Bugs (opened, flagged as \"bug\" and assigned)"
 echo
 my_ghi --state open -L bug | grep " @"
 
+echo
 echo
 echo "    Bugs (opened and flagged as \"bug\" and not assigned)"
 echo
 my_ghi --state open -L bug | grep -v " @"
 
 echo
+echo
 echo "    In Progress (opened, new feature and assigned)"
 echo
 my_ghi --state open -N bug | grep " @"
 
 echo
+echo
 echo "    For later (opened and not assigned assigned)"
 echo
 my_ghi --state open -N bug | grep -v " @"
 
+echo
 echo
 echo "    Recently closed (since last week)"
 echo
