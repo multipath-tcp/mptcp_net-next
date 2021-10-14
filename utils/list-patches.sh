3abbfe577383f55abe59b9cdd3b73d8cd4967864
Matthieu Baerts <matthieu.baerts@tessares.net>
2021-10-14 18:02:32 +0200

    utils:list patches: also support 'Handled Elsewhere'
    It was added to the old scripts but not this one...
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/list-patches.sh
+++ utils/list-patches.sh
@@ -3,7 +3,7 @@
 _dup() {
 	# shellcheck disable=SC2086
 	pwclient list -a no -f "%{state}:%{name}" | \
-		grep ${1} -e "^Superseded:" -e "^Deferred:" -e "^Mainlined:" -e "^Not Applicable:" | \
+		grep ${1} -e "^Superseded:" -e "^Deferred:" -e "^Mainlined:" -e "^Not Applicable:" -e "^Handled Elsewhere:" | \
 		cut -d: -f2- | \
 		sed 's/\[.\+\]//g;s/^\s\+//g' | \
 		sort | \
@@ -44,7 +44,7 @@ list() { local s
 # pwclient to list more than one state
 list_repo() { local w
 	w="$(pwclient list -a no -f "%{state}#%{submitter}" | \
-		grep -v -e "^Accepted#" -e "^Superseded#" -e "^Deferred#" -e "^Mainlined#" -e "^Not Applicable#" | \
+		grep -v -e "^Accepted#" -e "^Superseded#" -e "^Deferred#" -e "^Mainlined#" -e "^Not Applicable#" -e "^Handled Elsewhere#" | \
 		cut -d\# -f2- | \
 		cut -d\< -f1 | \
 		sed "s/ $//g" | \
@@ -55,7 +55,7 @@ list_repo() { local w
 	echo "    our repo (by: ${w:-/}):"
 	echo
 	pwclient list -a no -f "%{id}: %{state}: %{name}" | \
-		grep -v -e ": Accepted:" -e ": Superseded:" -e ": Deferred:" -e ": Mainlined:" -e ": Not Applicable:"
+		grep -v -e ": Accepted:" -e ": Superseded:" -e ": Deferred:" -e ": Mainlined:" -e ": Not Applicable:" -e ": Handled Elsewhere:"
 }
 
 dup "Repo" "-v"
@@ -82,7 +82,7 @@ Pending patches:
 
 EOF
 
-list handle-elsewhere "netdev (if mptcp ML is in cc)"
+list handled-elsewhere "netdev (if mptcp ML is in cc)"
 list_repo
 
 cat <<'EOF'
