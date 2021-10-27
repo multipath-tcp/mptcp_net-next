97b06ba49a683544af44822713f2a7f3dc739a97
Matthieu Baerts <matthieu.baerts@tessares.net>
2021-10-27 21:09:06 +0200

    utils:publish: build takes the export branch now
    No longer the tag because the cache was not used in this case.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/publish.sh
+++ utils/publish.sh
@@ -109,7 +109,7 @@ tg_tag() { local tag
 
 	printinfo "Builds and tests are now in progress:\\n"
 	printinfo "https://cirrus-ci.com/github/multipath-tcp/mptcp_net-next/${tag}"
-	printinfo "https://github.com/multipath-tcp/mptcp_net-next/actions/workflows/build-validation.yml?query=branch:${tag}"
+	printinfo "https://github.com/multipath-tcp/mptcp_net-next/actions/workflows/build-validation.yml?query=branch:${TG_EXPORT}"
 }
 
 tg_export_tag() {
