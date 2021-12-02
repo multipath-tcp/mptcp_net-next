c286dbec73e3732a41ed7102f086c68417dabda1
Mat Martineau <mathew.j.martineau@linux.intel.com>
2021-12-01 16:52:14 -0800

    mptcp: Make kernel path manager check for userspace-managed sockets
    Userspace-managed sockets should not have their subflows or
    advertisements changed by the kernel path manager.
    
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Message-Id: <20211202005216.154787-5-mathew.j.martineau@linux.intel.com>

--- net/mptcp/pm_netlink.c
+++ net/mptcp/pm_netlink.c
@@ -1122,7 +1122,8 @@ static int mptcp_nl_add_subflow_or_signal_addr(struct net *net)
 	while ((msk = mptcp_token_iter_next(net, &s_slot, &s_num)) != NULL) {
 		struct sock *sk = (struct sock *)msk;
 
-		if (!READ_ONCE(msk->fully_established))
+		if (!READ_ONCE(msk->fully_established) ||
+		    (READ_ONCE(msk->pm.pm_type) != MPTCP_PM_TYPE_KERNEL))
 			goto next;
 
 		lock_sock(sk);
@@ -1260,6 +1261,9 @@ static int mptcp_nl_remove_subflow_and_signal_addr(struct net *net,
 		struct sock *sk = (struct sock *)msk;
 		bool remove_subflow;
 
+		if (READ_ONCE(msk->pm.pm_type) != MPTCP_PM_TYPE_KERNEL)
+			goto next;
+
 		if (list_empty(&msk->conn_list)) {
 			mptcp_pm_remove_anno_addr(msk, addr, false);
 			goto next;
@@ -1301,7 +1305,8 @@ static int mptcp_nl_remove_id_zero_address(struct net *net,
 		struct sock *sk = (struct sock *)msk;
 		struct mptcp_addr_info msk_local;
 
-		if (list_empty(&msk->conn_list))
+		if (list_empty(&msk->conn_list) ||
+		    (READ_ONCE(msk->pm.pm_type) != MPTCP_PM_TYPE_KERNEL))
 			goto next;
 
 		local_address((struct sock_common *)msk, &msk_local);
@@ -1410,9 +1415,11 @@ static void mptcp_nl_remove_addrs_list(struct net *net,
 	while ((msk = mptcp_token_iter_next(net, &s_slot, &s_num)) != NULL) {
 		struct sock *sk = (struct sock *)msk;
 
-		lock_sock(sk);
-		mptcp_pm_remove_addrs_and_subflows(msk, rm_list);
-		release_sock(sk);
+		if (READ_ONCE(msk->pm.pm_type) == MPTCP_PM_TYPE_KERNEL) {
+			lock_sock(sk);
+			mptcp_pm_remove_addrs_and_subflows(msk, rm_list);
+			release_sock(sk);
+		}
 
 		sock_put(sk);
 		cond_resched();
@@ -1674,7 +1681,8 @@ static int mptcp_nl_addr_backup(struct net *net,
 	while ((msk = mptcp_token_iter_next(net, &s_slot, &s_num)) != NULL) {
 		struct sock *sk = (struct sock *)msk;
 
-		if (list_empty(&msk->conn_list))
+		if (list_empty(&msk->conn_list) ||
+		    (READ_ONCE(msk->pm.pm_type) != MPTCP_PM_TYPE_KERNEL))
 			goto next;
 
 		lock_sock(sk);
