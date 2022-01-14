943e682026b0d59d9f2d856b68bd70862148a23e
Matthieu Baerts <matthieu.baerts@tessares.net>
2022-01-14 14:22:45 +0100

    utils: list exported commits
    Formatted for the meeting.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

---
+++ utils/list-exported-commits.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git fetch origin
+
+git log --reverse --format="        - [%h] %s (%an)" origin/net-next..origin/export | \
+	sed -e "s/.*git markup: net-next (.*/\n    - Fixes for other trees:\n/;
+	        s/.*git markup: fixes other trees (.*/\n    - Fixes for -net:\n/;
+	        s/.*git markup: fixes net (.*/\n    - Fixes for net-next:\n/;
+	        s/.*git markup: fixes net-next (.*/\n    - Features for net-next:\n/;
+	        /DO-NOT-MERGE/,\$d"
