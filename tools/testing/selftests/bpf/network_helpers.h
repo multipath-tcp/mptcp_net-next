216396cca57c8c9c05cfc898fd69b18ccc12f6d2
Nicolas Rybowski <nicolas.rybowski@tessares.net>
2022-01-28 05:47:38 +0000

    bpf:selftests: add MPTCP test base
    This patch adds a base for MPTCP specific tests.
    
    It is currently limited to the is_mptcp field in case of plain TCP
    connection because there is no easy way to get the subflow sk from a msk
    in userspace. This implies that we cannot lookup the sk_storage attached
    to the subflow sk in the sockops program.
    
    Acked-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Nicolas Rybowski <nicolas.rybowski@tessares.net>

--- tools/testing/selftests/bpf/network_helpers.h
+++ tools/testing/selftests/bpf/network_helpers.h
@@ -21,6 +21,7 @@ struct network_helper_opts {
 	const char *cc;
 	int timeout_ms;
 	bool must_fail;
+	int protocol;
 };
 
 /* ipv4 test vector */
@@ -42,11 +43,14 @@ extern struct ipv6_packet pkt_v6;
 int settimeo(int fd, int timeout_ms);
 int start_server(int family, int type, const char *addr, __u16 port,
 		 int timeout_ms);
+int start_mptcp_server(int family, const char *addr, __u16 port,
+		       int timeout_ms);
 int *start_reuseport_server(int family, int type, const char *addr_str,
 			    __u16 port, int timeout_ms,
 			    unsigned int nr_listens);
 void free_fds(int *fds, unsigned int nr_close_fds);
 int connect_to_fd(int server_fd, int timeout_ms);
+int connect_to_mptcp_fd(int server_fd, int timeout_ms);
 int connect_to_fd_opts(int server_fd, const struct network_helper_opts *opts);
 int connect_fd_to_fd(int client_fd, int server_fd, int timeout_ms);
 int fastopen_connect(int server_fd, const char *data, unsigned int data_len,
