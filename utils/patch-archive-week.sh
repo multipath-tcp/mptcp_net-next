3651cb910a8922385158b5da313810fd11df3711
Matthieu Baerts <matthieu.baerts@tessares.net>
2022-02-04 15:43:10 +0100

    utils: PW state for needs-ack has been renamed
    s/_/-/
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/patch-archive-week.sh
+++ utils/patch-archive-week.sh
@@ -2,7 +2,7 @@
 
 # Patchwork on kernel.org has extra states
 git config --local pw.states \
-	new,under-review,accepted,rejected,rfc,not-applicable,changes-requested,awaiting-upstream,superseded,deferred,mainlined,queued,needs_ack,handled-elsewhere
+	new,under-review,accepted,rejected,rfc,not-applicable,changes-requested,awaiting-upstream,superseded,deferred,mainlined,queued,needs-ack,handled-elsewhere
 
 git-pw patch list \
 	--limit 250 \
@@ -22,4 +22,4 @@ git-pw patch list \
 	-f simple \
 	-c ID | \
 		tail -n +3 | \
-		xargs -r git-pw patch update --state needs_ack
+		xargs -r git-pw patch update --state needs-ack
