4bbc53308b346bac7f148fa9ac443b69f420fdb8
Geliang Tang <geliang.tang@suse.com>
2022-03-08 12:02:52 +0800

    bpf: add skc_to_mptcp_sock helper
    This patch implemented bpf_skc_to_mptcp_sock helper. Defined a new bpf_id
    BTF_SOCK_TYPE_MPTCP, and added a new helper bpf_mptcp_sock_from_subflow()
    to get struct bpf_mptcp_sock from a given subflow socket.
    
    Co-developed-by: Nicolas Rybowski <nicolas.rybowski@tessares.net>
    Signed-off-by: Nicolas Rybowski <nicolas.rybowski@tessares.net>
    Co-developed-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Message-Id: <d6d33be9899118c5f542a6ce43f868d4e9e2766b.1646711943.git.geliang.tang@suse.com>

---
+++ net/mptcp/bpf.c
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Multipath TCP
+ *
+ * Copyright (c) 2020, Tessares SA.
+ *
+ * Author: Nicolas Rybowski <nicolas.rybowski@tessares.net>
+ *
+ */
+
+#include <linux/bpf.h>
+#include "protocol.h"
+
+struct mptcp_sock *bpf_mptcp_sock_from_subflow(struct sock *sk)
+{
+	if (sk && sk_fullsock(sk) && sk->sk_protocol == IPPROTO_TCP && sk_is_mptcp(sk))
+		return mptcp_sk(mptcp_subflow_ctx(sk)->conn);
+
+	return NULL;
+}
+EXPORT_SYMBOL(bpf_mptcp_sock_from_subflow);
