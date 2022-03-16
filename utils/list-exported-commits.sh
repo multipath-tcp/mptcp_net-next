71ac498b0153692e79f0cf1d0e27cf12f0f33394
Matthieu Baerts <matthieu.baerts@tessares.net>
2022-03-16 12:22:36 +0100

    utils:list commits: possibility to change the range
    Useful when re-working the tree.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/list-exported-commits.sh
+++ utils/list-exported-commits.sh
@@ -2,7 +2,7 @@
 
 git fetch origin
 
-git log --reverse --format="        - [%h] %s (%an)" origin/net-next..origin/export | \
+git log --reverse --format="        - [%h] %s (%an)" ${1:-"origin/net-next..origin/export"} | \
 	sed -e "s/.*git markup: net-next (.*/\n    - Fixes for other trees:\n/;
 	        s/.*git markup: fixes other trees (.*/\n    - Fixes for -net:\n/;
 	        s/.*git markup: fixes net (.*/\n    - Fixes for net-next:\n/;
