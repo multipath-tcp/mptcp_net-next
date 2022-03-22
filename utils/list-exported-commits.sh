d5c3ca93a25c359417fc464eff401a0b3bbcd9ff
Matthieu Baerts <matthieu.baerts@tessares.net>
2022-03-22 09:54:33 +0100

    utils:list commits: adapt for new tree
    We have new markups.
    
    We also want to show the commits for the other trees.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/list-exported-commits.sh
+++ utils/list-exported-commits.sh
@@ -2,9 +2,11 @@
 
 git fetch origin
 
-git log --reverse --format="        - [%h] %s (%an)" ${1:-"origin/net-next..origin/export"} | \
-	sed -e "s/.*git markup: net-next (.*/\n    - Fixes for other trees:\n/;
+git log --reverse --format="        - [%h] %s (%an)" "${1:-"origin/net-next..origin/export"}" |
+	sed -e "s/.*git markup: net (.*/\n    - Fixes for other trees:\n/;
 	        s/.*git markup: fixes other trees (.*/\n    - Fixes for -net:\n/;
-	        s/.*git markup: fixes net (.*/\n    - Fixes for net-next:\n/;
+	        s/.*git markup: fixes end common net net-next (.*/\n    - Fixes for net-next:\n/;
 	        s/.*git markup: fixes net-next (.*/\n    - Features for net-next:\n/;
-	        /DO-NOT-MERGE/,\$d"
+	        s/.*git markup: features net-next (.*/\n    - Features for net-next (next):\n/;
+	        s/.*git markup: features net-next-next (.*/\n    - Features for other trees:\n/;
+	        /DO-NOT-MERGE/d;/TopGit-driven merge of branches/d;"
