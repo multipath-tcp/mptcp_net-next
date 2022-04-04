b773a143f84e38a9f9a5fd17acf3c6ef8baa6c9a
Geliang Tang <geliang.tang@suse.com>
2022-04-04 10:09:56 +0800

    mptcp: add sched in mptcp_sock
    This patch adds a new struct member sched in struct mptcp_sock.
    And two helpers mptcp_init_sched() and mptcp_release_sched() to
    init and release it.
    
    Init it with the sysctl scheduler in mptcp_init_sock(), copy the
    scheduler from the parent in mptcp_sk_clone(), and release it in
    __mptcp_destroy_sock().
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Message-Id: <64d8a25cc23fe8720f29f303a136e5cd6001bb73.1649037838.git.geliang.tang@suse.com>

--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -288,6 +288,7 @@ struct mptcp_sock {
 	struct socket	*subflow; /* outgoing connect/listener/!mp_capable */
 	struct sock	*first;
 	struct mptcp_pm_data	pm;
+	struct mptcp_sched_ops	*sched;
 	struct {
 		u32	space;	/* bytes copied in last measurement window */
 		u32	copied; /* bytes copied in this measurement window */
@@ -614,6 +615,9 @@ int mptcp_register_scheduler(struct mptcp_sched_ops *sched);
 void mptcp_unregister_scheduler(struct mptcp_sched_ops *sched);
 struct sock *mptcp_subflow_get_send(struct mptcp_sock *msk);
 void mptcp_sched_init(void);
+int mptcp_init_sched(struct mptcp_sock *msk,
+		     struct mptcp_sched_ops *sched);
+void mptcp_release_sched(struct mptcp_sock *msk);
 
 static inline bool __mptcp_subflow_active(struct mptcp_subflow_context *subflow)
 {
