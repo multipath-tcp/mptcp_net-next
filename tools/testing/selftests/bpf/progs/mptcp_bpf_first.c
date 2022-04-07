e99346b0f23c6bb88cb0081ef5e92988a69e7329
Geliang Tang <geliang.tang@suse.com>
2022-04-07 12:03:19 +0800

    selftests: bpf: add bpf_first scheduler
    This patch implements the simplest MPTCP scheduler, named bpf_first,
    which always picks the first subflow to send data. It's a sample of
    MPTCP BPF scheduler implementations.
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Message-Id: <2ff358a960acc528e430c5826ad8150028a8ee95.1649303876.git.geliang.tang@suse.com>

---
+++ tools/testing/selftests/bpf/progs/mptcp_bpf_first.c
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (c) 2022, SUSE. */
+
+#include <linux/bpf.h>
+#include "bpf_mptcp_helpers.h"
+
+char _license[] SEC("license") = "GPL";
+
+SEC("struct_ops/mptcp_sched_first_init")
+void BPF_PROG(mptcp_sched_first_init, struct mptcp_sock *msk)
+{
+}
+
+SEC("struct_ops/mptcp_sched_first_release")
+void BPF_PROG(mptcp_sched_first_release, struct mptcp_sock *msk)
+{
+}
+
+struct sock *BPF_STRUCT_OPS(bpf_first_get_subflow, struct mptcp_sock *msk)
+{
+	return msk->first;
+}
+
+SEC(".struct_ops")
+struct mptcp_sched_ops first = {
+	.init		= (void *)mptcp_sched_first_init,
+	.release	= (void *)mptcp_sched_first_release,
+	.get_subflow	= (void *)bpf_first_get_subflow,
+	.name		= "bpf_first",
+};
