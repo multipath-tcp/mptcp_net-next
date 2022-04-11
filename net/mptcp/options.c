3b2bbb4557d7bd31ee2078f811bc1c473b20c096
Paolo Abeni <pabeni@redhat.com>
2022-04-11 12:40:05 +0200

    mptcp: add more offered MIBs counter.
    Track the exceptional handling of MPTCP-level offered window
    with a few more counters for observability.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Message-Id: <66a6c06373c3905d5b0ecf4fc082cc3750f5479b.1649672265.git.pabeni@redhat.com>

--- net/mptcp/options.c
+++ net/mptcp/options.c
@@ -1249,8 +1249,11 @@ static void mptcp_set_rwin(struct tcp_sock *tp, void *opt)
 
 			if (rcv_wnd == rcv_wnd_old)
 				break;
-			if (before64(rcv_wnd_new, rcv_wnd))
+			if (before64(rcv_wnd_new, rcv_wnd)) {
+				MPTCP_INC_STATS(sock_net(ssk), MPTCP_MIB_RCVWNDCONFLICTUPDATE);
 				goto raise_win;
+			}
+			MPTCP_INC_STATS(sock_net(ssk), MPTCP_MIB_RCVWNDCONFLICT);
 			rcv_wnd_old = rcv_wnd;
 		};
 		return;
@@ -1278,6 +1281,7 @@ raise_win:
 		/* RFC1323 scaling applied */
 		new_win >>= tp->rx_opt.rcv_wscale;
 		th->window = htons(new_win);
+		MPTCP_INC_STATS(sock_net(ssk), MPTCP_MIB_RCVWNDSHARED);
 	}
 }
 
