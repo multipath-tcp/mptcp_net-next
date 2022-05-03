eb45de25a8d0cc98db03e730ff7c2657574859d7
Geliang Tang <geliang.tang@suse.com>
2022-05-03 18:29:21 +0200

    mptcp: add struct mptcp_sched_ops
    This patch defines struct mptcp_sched_ops, which has three struct members,
    name, owner and list, and three function pointers, init, release and
    get_subflow.
    
    Add the scheduler registering, unregistering and finding functions to add,
    delete and find a packet scheduler on the global list mptcp_sched_list.
    
    For supporting a "redundant" packet scheduler in the future, this patch
    adds a flag in struct mptcp_sched_data named call_again to indicate that
    get_subflow() function needs to be called again.
    
    Acked-by: Paolo Abeni <pabeni@redhat.com>
    Reviewed-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>

--- tools/testing/selftests/bpf/bpf_mptcp_helpers.h
+++ tools/testing/selftests/bpf/bpf_mptcp_helpers.h
@@ -6,6 +6,24 @@
 
 #include "bpf_tcp_helpers.h"
 
+#define MPTCP_SCHED_NAME_MAX	16
+
+struct mptcp_sched_data {
+	struct sock	*sock;
+	bool		call_again;
+};
+
+struct mptcp_sched_ops {
+	char name[MPTCP_SCHED_NAME_MAX];
+
+	void (*init)(const struct mptcp_sock *msk);
+	void (*release)(const struct mptcp_sock *msk);
+
+	void (*get_subflow)(const struct mptcp_sock *msk, bool reinject,
+			    struct mptcp_sched_data *data);
+	void *owner;
+};
+
 struct mptcp_sock {
 	struct inet_connection_sock	sk;
 
