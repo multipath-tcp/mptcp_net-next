e2adeaa028449fae9ef9cce69dee7f611b773a8a
Matthieu Baerts <matthieu.baerts@tessares.net>
2022-05-05 18:29:05 +0200

    utils: remote-empty: workaround for broken 'tg next'
    tg annihilate was not jumping to the next topic causing some issues in
    the dependences. But in fact, 'tg next' was also having the same issue.
    
    Instead, a manual 'tg next' is done with a workaround.
    
    Link: https://github.com/mackyle/topgit/issues/23
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/tg-remove-empty.sh
+++ utils/tg-remove-empty.sh
@@ -8,6 +8,26 @@ tg_empty() {
 	[ -z "$(tg files)" ]
 }
 
+is_dep() {
+	git show "${1}:.topdeps" | grep -q "^${2}$"
+}
+
+tg_next() { local curr next
+	curr="${1}"
+	next="$(tg next)"
+
+	if ! is_dep "${next}" "${curr}"; then
+		# tg next seems to have issues returning the right one
+		next=t/DO-NOT-MERGE-git-markup-fixes-net-next
+		if ! is_dep "${next}" "${curr}"; then
+			>&2 echo -e "\n\n\tError: unable to find next, not $(tg next) for ${curr}\n\n"
+			exit 1
+		fi
+	fi
+
+	echo "${next}"
+}
+
 ./.tg-first.sh
 
 while true; do
@@ -15,13 +35,21 @@ while true; do
 	if tg_empty && ! [[ "${BRANCH}" =~ ^"t/upstream"* ]]; then
 		echo -e "\n\t => Remove ${BRANCH}\n"
 		DEPS="$(head -n1 .topdeps)"
-		tg annihilate # it jumps to the next one
-		tg update
+		NEXT="$(tg_next "${BRANCH}")"
+
+		# the next line comes from tg annihilate and does:
+		#    git rm -f .topdeps .topmsg
+		git read-tree "$(git merge-base "refs/top-bases/${BRANCH}" "refs/heads/${BRANCH}")^{tree}"
+		git commit --no-verify -sm "TopGit branch ${BRANCH} annihilated."
+
+		git checkout "${NEXT}"
 		sed -i "1c\\${DEPS}" .topdeps
-		git commit -sm "tg: parent topic is empty" .topdeps
+		git commit -sm "tg: child topic (${BRANCH}) is empty" .topdeps
+
 		tg update
 		tg push "${BRANCH}"
-		#tg delete "${BRANCH}"
+
+		#tg delete "${BRANCH}" # the next 'tg remote --populate' will get it back
 	elif [ "${BRANCH}" = "t/DO-NOT-MERGE-git-markup-end-common-net-net-next" ]; then
 		echo -e "\n\t => Special case ${BRANCH}: continue on -net tree\n"
 		echo 2 | tg checkout next || break
@@ -33,7 +61,8 @@ while true; do
 		break
 	else
 		echo -e "\n\t => Not empty, skip ${BRANCH}\n"
-		tg checkout next || exit 1
+		NEXT="$(tg_next "${BRANCH}")"
+		git checkout "${NEXT}" || exit 1
 		tg update
 	fi
 done
