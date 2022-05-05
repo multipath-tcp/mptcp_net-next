abf95144062228b627e70ccb00fa0ebc3e11be0b
Matthieu Baerts <matthieu.baerts@tessares.net>
2022-05-05 18:32:52 +0200

    utils: list exp commit: 'Fixes for net-next'
    There are seen at the beginning, not after 'fixes end common net
    net-next' but after 'git markup: net-next'.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/list-exported-commits.sh
+++ utils/list-exported-commits.sh
@@ -3,9 +3,9 @@
 git fetch origin
 
 git log --reverse --format="        - [%h] %s (%an)" "${1:-"origin/net-next..origin/export"}" |
-	sed -e "s/.*git markup: net (.*/\n    - Fixes for other trees:\n/;
+	sed -e "s/.*git markup: net-next (.*/\n    - Fixes for net-next:\n/;
+	        s/.*git markup: net (.*/\n    - Fixes for other trees:\n/;
 	        s/.*git markup: fixes other trees (.*/\n    - Fixes for -net:\n/;
-	        s/.*git markup: fixes end common net net-next (.*/\n    - Fixes for net-next:\n/;
 	        s/.*git markup: fixes net-next (.*/\n    - Features for net-next:\n/;
 	        s/.*git markup: features net-next (.*/\n    - Features for net-next (next):\n/;
 	        s/.*git markup: features net-next-next (.*/\n    - Features for other trees:\n/;
