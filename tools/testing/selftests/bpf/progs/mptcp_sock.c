56cd4c7e34bdecb37484dd0a6d9d50bb6ea8e751
Matthieu Baerts <matthieu.baerts@tessares.net>
2022-05-19 15:59:43 +0200

    tg: revert selftests/bpf: verify token of struct mptcp_sock
    A new version is applied in the next commit.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- tools/testing/selftests/bpf/progs/mptcp_sock.c
+++ tools/testing/selftests/bpf/progs/mptcp_sock.c
@@ -11,7 +11,6 @@ char _license[] SEC("license") = "GPL";
 struct mptcp_storage {
 	__u32 invoked;
 	__u32 is_mptcp;
-	__u32 token;
 };
 
 struct {
@@ -48,8 +47,6 @@ int _sockops(struct bpf_sock_ops *ctx)
 					     BPF_SK_STORAGE_GET_F_CREATE);
 		if (!storage)
 			return 1;
-
-		storage->token = 0;
 	} else {
 		msk = bpf_skc_to_mptcp_sock(sk);
 		if (!msk)
@@ -59,8 +56,6 @@ int _sockops(struct bpf_sock_ops *ctx)
 					     BPF_SK_STORAGE_GET_F_CREATE);
 		if (!storage)
 			return 1;
-
-		storage->token = msk->token;
 	}
 	storage->invoked++;
 	storage->is_mptcp = is_mptcp;
