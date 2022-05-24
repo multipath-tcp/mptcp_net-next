aeb2072f0aff4e9374e077aaec5a907001781394
Geliang Tang <geliang.tang@suse.com>
2022-05-24 10:07:14 +0200

    mptcp: add bpf_mptcp_sched_ops
    This patch implements a new struct bpf_struct_ops, bpf_mptcp_sched_ops.
    Register and unregister the bpf scheduler in .reg and .unreg.
    
    This MPTCP BPF scheduler implementation is similar to BPF TCP CC. And
    net/ipv4/bpf_tcp_ca.c is a frame of reference for this patch.
    
    Acked-by: Paolo Abeni <pabeni@redhat.com>
    Reviewed-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>

--- kernel/bpf/bpf_struct_ops_types.h
+++ kernel/bpf/bpf_struct_ops_types.h
@@ -8,5 +8,9 @@ BPF_STRUCT_OPS_TYPE(bpf_dummy_ops)
 #ifdef CONFIG_INET
 #include <net/tcp.h>
 BPF_STRUCT_OPS_TYPE(tcp_congestion_ops)
+#ifdef CONFIG_MPTCP
+#include <net/mptcp.h>
+BPF_STRUCT_OPS_TYPE(mptcp_sched_ops)
+#endif
 #endif
 #endif
