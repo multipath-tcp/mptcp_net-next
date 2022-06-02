14a3192338680cce63421a9202a72950420361f8
Geliang Tang <geliang.tang@suse.com>
2022-06-02 12:53:42 +0800

    Squash to "mptcp: add bpf_mptcp_sched_ops"
    Please update the subject and commit log:
    
    '''
    bpf: Add bpf_mptcp_sched_ops
    
    This patch implements a new struct bpf_struct_ops, bpf_mptcp_sched_ops.
    Register and unregister the bpf scheduler in .reg and .unreg.
    
    Add write access for the scheduled flag of struct mptcp_subflow_context
    in .btf_struct_access.
    
    This MPTCP BPF scheduler implementation is similar to BPF TCP CC. And
    net/ipv4/bpf_tcp_ca.c is a frame of reference for this patch.
    '''
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Message-Id: <77b282be5d838080861e2d4d0b64192f23bb6594.1654143895.git.geliang.tang@suse.com>

--- net/mptcp/bpf.c
+++ net/mptcp/bpf.c
@@ -42,29 +42,27 @@ static int bpf_mptcp_sched_btf_struct_access(struct bpf_verifier_log *log,
 {
 	size_t end;
 
-	if (atype == BPF_READ)
+	if (atype == BPF_READ) {
 		return btf_struct_access(log, btf, t, off, size, atype,
 					 next_btf_id, flag);
+	}
 
 	if (t != mptcp_sched_type) {
-		bpf_log(log, "only access to mptcp_sched_data is supported\n");
+		bpf_log(log, "only access to mptcp_subflow_context is supported\n");
 		return -EACCES;
 	}
 
 	switch (off) {
-	case offsetof(struct mptcp_sched_data, sock):
-		end = offsetofend(struct mptcp_sched_data, sock);
-		break;
-	case offsetof(struct mptcp_sched_data, call_again):
-		end = offsetofend(struct mptcp_sched_data, call_again);
+	case offsetof(struct mptcp_subflow_context, scheduled):
+		end = offsetofend(struct mptcp_subflow_context, scheduled);
 		break;
 	default:
-		bpf_log(log, "no write support to mptcp_sched_data at off %d\n", off);
+		bpf_log(log, "no write support to mptcp_subflow_context at off %d\n", off);
 		return -EACCES;
 	}
 
 	if (off + size > end) {
-		bpf_log(log, "access beyond mptcp_sched_data at off %u size %u ended at %zu",
+		bpf_log(log, "access beyond mptcp_subflow_context at off %u size %u ended at %zu",
 			off, size, end);
 		return -EACCES;
 	}
@@ -144,7 +142,7 @@ static int bpf_mptcp_sched_init(struct btf *btf)
 {
 	s32 type_id;
 
-	type_id = btf_find_by_name_kind(btf, "mptcp_sched_data",
+	type_id = btf_find_by_name_kind(btf, "mptcp_subflow_context",
 					BTF_KIND_STRUCT);
 	if (type_id < 0)
 		return -EINVAL;
