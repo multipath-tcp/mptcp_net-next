72a814e85b69c1c73d6b36f1f37d8250fd018381
Geliang Tang <geliang.tang@suse.com>
2022-06-10 21:05:32 +0800

    Squash to "selftests/bpf: Add mptcp sched structs"
    Add redundant flag for struct mptcp_sched_ops.
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Message-Id: <bf3ee8197abd0c37dbf331c425bb6e3f170de3ef.1654865847.git.geliang.tang@suse.com>

--- tools/testing/selftests/bpf/bpf_tcp_helpers.h
+++ tools/testing/selftests/bpf/bpf_tcp_helpers.h
@@ -245,6 +245,7 @@ struct mptcp_sched_data {
 
 struct mptcp_sched_ops {
 	char name[MPTCP_SCHED_NAME_MAX];
+	bool redundant;
 
 	void (*init)(const struct mptcp_sock *msk);
 	void (*release)(const struct mptcp_sock *msk);
