2bb8673ff156a66e4455fcc944c913617bb7361f
Paolo Abeni <pabeni@redhat.com>
2022-06-24 18:38:08 +0200

    mptcp: introduce and use mptcp_pm_send_ack()
    The in-kernel PM has a bit of duplicate code related to ack
    generation. Create a new helper factoring out the PM-specific
    needs and use it in a couple of places.
    
    Note that this additionally moves a few unsafe subflow socket
    access under the relevant socket lock.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Message-Id: <a59c3eb1e47c5c32f97c87d66c52f6c0021846bc.1656088406.git.pabeni@redhat.com>

--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -609,7 +609,7 @@ void __init mptcp_subflow_init(void);
 void mptcp_subflow_shutdown(struct sock *sk, struct sock *ssk, int how);
 void mptcp_close_ssk(struct sock *sk, struct sock *ssk,
 		     struct mptcp_subflow_context *subflow);
-void mptcp_subflow_send_ack(struct sock *ssk);
+void __mptcp_subflow_send_ack(struct sock *ssk);
 void mptcp_subflow_reset(struct sock *ssk);
 void mptcp_subflow_queue_clean(struct sock *ssk);
 void mptcp_sock_graft(struct sock *sk, struct socket *parent);
