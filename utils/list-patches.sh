a18a43f2636734d9b21e23c9b08e90786955a8ed
Matthieu Baerts <matthieu.baerts@tessares.net>
2022-06-25 13:05:24 +0200

    utils: switch from pwclient to git-pw
    Easier not to mix the different tools but also because pwclient seems no
    longer returning what is requested.
    
    pwclient was mainly used because we don't need to specify all states we
    want: by default, it lists '*' which is not possible with git-pw, we
    need to use '--state x' multiple times. But anyway, we always have to
    filter the state.
    
    Also a good thing with 'git-pw' is that it can search for patches with a
    certain name.
    
    While at it, we also grab more details when listing all patches: series,
    url, submitter
    
    We can also remove no longer used scripts and merge
    patch-subject-get-ids.sh in patch-subject-accept.sh as it is only used
    there.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/list-patches.sh
+++ utils/list-patches.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
+# $@: list of --state X
 _dup() {
-	# shellcheck disable=SC2086
-	pwclient list -a no -f "%{state}:%{name}" | \
-		grep ${1} -e "^Deferred:" -e "^Mainlined:" -e "^Not Applicable:" -e "^Handled Elsewhere:" | \
-		grep -v "^Superseded:" | \
-		cut -d: -f2- | \
-		sed 's/\[.\+\]//g;s/^\s\+//g' | \
-		sort | \
-		uniq -cd
+	git-pw patch list --limit 250 -c Name -f simple "${@}" |
+		sed 's/\[.\+\]//g;s/^\s\+//g' |
+		tail -n+3 |
+		sort | uniq -cd
+}
+
+dup_Upstream() {
+	_dup --state deferred \
+	     --state mainlined \
+	     --state not-applicable \
+	     --state handled-elsewhere
+}
+
+dup_Repo() {
+	_dup --state new \
+	     --state under-review \
+	     --state accepted \
+	     --state rejected \
+	     --state rfc \
+	     --state not-applicable \
+	     --state changes-requested \
+	     --state queued \
+	     --state awaiting-upstream \
+	     --state needs-ack
 }
 
 dup() { local d
-	d="$(_dup "${2}")"
+	d="$(dup_"${1}")"
 	if [ -n "${d}" ]; then
 		echo "There are duplicated entries for ${1}:"
 		echo "${d}"
@@ -24,44 +41,91 @@ dup() { local d
 	echo
 }
 
-list() { local s
-	if [ -z "${2}" ] && [ "$(git-pw patch list --state "${1}" -f csv -c ID | wc -l)" -le 1 ]; then
+list() { local list desc id name state tag n patch submitter series sid url
+	desc="${1}"
+	shift
+
+	if [ -z "${desc}" ] && [ "$(git-pw patch list --limit 250 --state "${@}" -f csv -c ID | wc -l)" -le 1 ]; then
 		return 0
 	fi
 
-	echo -n "    ${2:-${1}} (by: "
-	s=$(git-pw patch list --limit 250 --state "${1}" -f simple -c Submitter | \
-		sed "s/ \(\S\+@\S\+\)//g" | \
-		tail -n+3 | \
-		sort -u | \
-		xargs -r -I{} echo -n "{}, ")
-	echo "${s}" | sed 's/, $//g;s/^$/\//g;s/$/):/g'
+	echo -n "    ${desc:-${1}} (by: "
+
+	list=$(git-pw patch list --sort date --limit 250 --state "${@}" -f simple -c ID -c Submitter |
+		sed "s/ \(\S\+@\S\+\)//g" |
+		tail -n+3)
+	if [ -z "${list}" ]; then
+		echo "/):"
+		echo
+		echo "/"
+		echo
+		echo
+		return
+	fi
+
+	echo "${list}" | cut -d\  -f2- |
+		sort -u |
+		xargs -r -I{} echo -n "{}, " |
+		sed 's/, $//g;s/^$/\//g;s/$/):/g'
 	echo
-	s="$(PAGER="cat" git-pw patch list --limit 250 --state "${1}" -f simple -c ID -c Name | \
-		tail -n+3)"
-	echo "${s}" | sed 's/^$/\//g'
 	echo
+
+	for id in $(echo "${list}" | cut -d\  -f1); do
+		patch="$(git-pw patch show --format csv "${id}" | sed 's/""/#/g')"
+		name=$(echo "${patch}" | grep '^"Name",' | cut -d\" -f4)
+
+		echo -n "${id}: ${name//#/\"}"
+
+		tag=$(echo "${name}" | sed 's/.*\(\[.\+\]\).*/\1/')
+		if [ "${tag:0:1}" = '[' ]; then
+			n=$(echo "${tag}" | sed 's#.*\([0-9]\+/[0-9]\+\).*#\1#')
+			if [[ "${n:0:1}" == [0-9] ]]; then
+				# if we have multiple patches, we only display info
+				# for the last one
+				if [ "${n%/*}" != "${n#*/}" ]; then
+					echo
+					continue
+				else
+					n="series"
+				fi
+			fi
+		fi
+
+		echo ":"
+
+		state=$(echo "${patch}" | grep '^"State",' | cut -d\" -f4)
+		series=$(echo "${patch}" | grep '^"Series",' | cut -d\" -f4)
+		sid=${series%% *}
+		series=${series#* }
+		submitter=$(echo "${patch}" | grep '^"Submitter",' | cut -d\" -f4 | sed 's/ \(\S\+@\S\+\)//')
+		url=$(echo "${patch}" | grep '^"URL",' | cut -d\" -f4)
+
+		# No need to display more info for this "RESEND" series
+		if [ "${sid}" = 489001 ] || [ "${sid}" = 489003 ]; then
+			continue
+		fi
+
+		# more than one state
+		if [ "${#}" -gt 1 ]; then
+			[ "${state}" = "rfc" ] && state=RFC || state=${state//-/ }
+			echo "      - State: ${state^}"
+		fi
+
+		if [ "${n}" = "series" ]; then
+			url="https://patchwork.kernel.org/project/mptcp/list/?series=${sid}&state=*&archive=both"
+
+			echo "      - Series: ${series//#/\"}"
+		fi
+
+		echo "      - Submitter: ${submitter//#/\"}"
+		echo "      - URL: ${url//#/\"}"
+		echo
+	done
+
 	echo
 }
 
-# pwclient to list more than one state
-list_repo() { local w
-	w="$(pwclient list -a no -f "%{state}#%{submitter}" | \
-		grep -v -e "^Accepted#" -e "^Superseded#" -e "^Deferred#" -e "^Mainlined#" -e "^Not Applicable#" -e "^Handled Elsewhere#" | \
-		cut -d\# -f2- | \
-		cut -d\< -f1 | \
-		sed "s/ $//g" | \
-		sort -u | \
-		xargs -I{} echo -n "{}, " | \
-		sed 's/, $//g')"
-
-	echo "    our repo (by: ${w:-/}):"
-	echo
-	pwclient list -a no -f "%{id}: %{state}: %{name}" | \
-		grep -v -e ": Accepted:" -e ": Superseded:" -e ": Deferred:" -e ": Mainlined:" -e ": Not Applicable:" -e ": Handled Elsewhere:"
-}
-
-dup "Repo" "-v"
+dup "Repo"
 dup "Upstream"
 
 
@@ -73,8 +137,8 @@ Accepted patches:
 
 EOF
 
-list Mainlined "netdev (if mptcp ML is in cc)"
-list Accepted "our repo"
+list "netdev (if mptcp ML is in cc)" mainlined
+list "our repo" accepted
 
 cat <<'EOF'
 
@@ -85,8 +149,15 @@ Pending patches:
 
 EOF
 
-list handled-elsewhere "netdev (if mptcp ML is in cc)"
-list_repo
+list "netdev (if mptcp ML is in cc)" handled-elsewhere
+list "our repo" new \
+	--state under-review \
+	--state rejected \
+	--state rfc \
+	--state changes-requested \
+	--state awaiting-upstream \
+	--state queued \
+	--state needs-ack
 
 cat <<'EOF'
 
@@ -94,5 +165,5 @@ cat <<'EOF'
 Extra, just in case
 
 EOF
-list Deferred
-list not-applicable
+list "" deferred
+list "" not-applicable
--- utils/patch-subject-accept.sh
+++ utils/patch-subject-accept.sh
@@ -1,17 +1,28 @@
 #!/bin/bash -e
 
-SUBJECT="${1}"
-[ -n "${SUBJECT}" ]
+SUBJECT="${1:?}"
+
+# remote dot at the end if any
+[[ "${SUBJECT}" =~ "."$ ]] && SUBJECT="${SUBJECT:0:-1}"
 
 COMMIT="${2}"
 DELEGATE="matttbe"
 
 get_ids() {
-	bash "-${-}" ./.patch-subject-get-ids.sh "${1}"
+	git-pw patch list --limit 250 --sort date --format simple -c ID \
+		--state new \
+		--state under-review \
+		--state rfc \
+		--state changes-requested \
+		--state awaiting-upstream \
+		--state queued \
+		--state needs-ack \
+		"${SUBJECT}" |
+			tail -n+3
 }
 
 STATE="accepted"
-for i in $(get_ids "${SUBJECT}"); do
+for i in $(get_ids); do
 	echo "Set state ${STATE} to patch-id ${i}"
 	git-pw patch update \
 		--state "${STATE}" \
