2c27d7a3c28d13efb6e981b805df5d30faee1a56
Mat Martineau <mathew.j.martineau@linux.intel.com>
2022-06-29 15:35:50 -0700

    mptcp: Acquire the subflow socket lock before modifying MP_PRIO flags
    When setting up a subflow's flags for sending MP_PRIO MPTCP options, the
    subflow socket lock was not held while reading and modifying several
    struct members that are also read and modified in mptcp_write_options().
    
    Acquire the subflow socket lock earlier and send the MP_PRIO ACK with
    that lock already acquired. Add a new variant of the
    mptcp_subflow_send_ack() helper to use with the subflow lock held.
    
    v2: Squashed helper function commit
    
    Fixes: 067065422fcd ("mptcp: add the outgoing MP_PRIO support")
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Message-Id: <20220629223550.655928-3-mathew.j.martineau@linux.intel.com>

--- net/mptcp/pm_netlink.c
+++ net/mptcp/pm_netlink.c
@@ -729,11 +729,13 @@ static int mptcp_pm_nl_mp_prio_send_ack(struct mptcp_sock *msk,
 		struct sock *ssk = mptcp_subflow_tcp_sock(subflow);
 		struct sock *sk = (struct sock *)msk;
 		struct mptcp_addr_info local;
+		bool slow;
 
 		local_address((struct sock_common *)ssk, &local);
 		if (!mptcp_addresses_equal(&local, addr, addr->port))
 			continue;
 
+		slow = lock_sock_fast(ssk);
 		if (subflow->backup != bkup)
 			msk->last_snd = NULL;
 		subflow->backup = bkup;
@@ -741,7 +743,8 @@ static int mptcp_pm_nl_mp_prio_send_ack(struct mptcp_sock *msk,
 		subflow->request_bkup = bkup;
 
 		pr_debug("send ack for mp_prio");
-		mptcp_subflow_send_ack(ssk);
+		__mptcp_subflow_send_ack(ssk);
+		unlock_sock_fast(ssk, slow);
 
 		return 0;
 	}
