2bab0d3b8f0d6d054128df0e673da578c067f817
Geliang Tang <geliang.tang@suse.com>
2022-10-02 22:25:09 +0800

    Squash to "mptcp: add get_subflow wrappers"
    Please update the commit log:
    
    '''
    This patch defines two new wrappers mptcp_sched_get_send() and
    mptcp_sched_get_retrans(), invoke get_subflow() of msk->sched in them.
    
    Set the subflow pointers array in struct mptcp_sched_data before invoking
    get_subflow(), then it can be used in get_subflow() in the BPF contexts.
    
    Check the subflow scheduled flags to test which subflow or subflows are
    picked by the scheduler.
    
    Move sock_owned_by_me() and the fallback check code from
    mptcp_subflow_get_send/retrans() into the wrappers.
    '''
    Message-Id: <564b5c387644eb2b83c7bb03e0fe638d16acf6bc.1664720538.git.geliang.tang@suse.com>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -1547,7 +1547,7 @@ void __mptcp_push_pending(struct sock *sk, unsigned int flags)
 			int ret = 0;
 
 			prev_ssk = ssk;
-			ssk = mptcp_sched_get_send(msk);
+			ssk = mptcp_subflow_get_send(msk);
 
 			/* First check. If the ssk has changed since
 			 * the last round, release prev_ssk
@@ -1616,7 +1616,7 @@ static void __mptcp_subflow_push_pending(struct sock *sk, struct sock *ssk)
 			 * check for a different subflow usage only after
 			 * spooling the first chunk of data
 			 */
-			xmit_ssk = first ? ssk : mptcp_sched_get_send(mptcp_sk(sk));
+			xmit_ssk = first ? ssk : mptcp_subflow_get_send(mptcp_sk(sk));
 			if (!xmit_ssk)
 				goto out;
 			if (xmit_ssk != ssk) {
@@ -2478,7 +2478,7 @@ static void __mptcp_retrans(struct sock *sk)
 	mptcp_clean_una_wakeup(sk);
 
 	/* first check ssk: need to kick "stale" logic */
-	ssk = mptcp_sched_get_retrans(msk);
+	ssk = mptcp_subflow_get_retrans(msk);
 	dfrag = mptcp_rtx_head(sk);
 	if (!dfrag) {
 		if (mptcp_data_fin_enabled(msk)) {
@@ -3196,7 +3196,7 @@ void __mptcp_check_push(struct sock *sk, struct sock *ssk)
 		return;
 
 	if (!sock_owned_by_user(sk)) {
-		struct sock *xmit_ssk = mptcp_sched_get_send(mptcp_sk(sk));
+		struct sock *xmit_ssk = mptcp_subflow_get_send(mptcp_sk(sk));
 
 		if (xmit_ssk == ssk)
 			__mptcp_subflow_push_pending(sk, ssk);
