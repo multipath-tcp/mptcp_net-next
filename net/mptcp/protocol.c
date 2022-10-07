d94a668338acb13e270f0c46d2446db218a494b2
Menglong Dong <imagedong@tencent.com>
2022-10-07 17:29:20 +0800

    mptcp: introduce 'sk' to replace 'sock->sk' in mptcp_listen()
    'sock->sk' is used frequently in mptcp_listen(). Therefore, we can
    introduce the 'sk' and replace 'sock->sk' with it.
    
    Signed-off-by: Menglong Dong <imagedong@tencent.com>
    Reviewed-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Message-Id: <20221007092922.13169-2-imagedong@tencent.com>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -3642,12 +3642,13 @@ static int mptcp_stream_connect(struct socket *sock, struct sockaddr *uaddr,
 static int mptcp_listen(struct socket *sock, int backlog)
 {
 	struct mptcp_sock *msk = mptcp_sk(sock->sk);
+	struct sock *sk = sock->sk;
 	struct socket *ssock;
 	int err;
 
 	pr_debug("msk=%p", msk);
 
-	lock_sock(sock->sk);
+	lock_sock(sk);
 	ssock = __mptcp_nmpc_socket(msk);
 	if (!ssock) {
 		err = -EINVAL;
@@ -3655,16 +3656,16 @@ static int mptcp_listen(struct socket *sock, int backlog)
 	}
 
 	mptcp_token_destroy(msk);
-	inet_sk_state_store(sock->sk, TCP_LISTEN);
-	sock_set_flag(sock->sk, SOCK_RCU_FREE);
+	inet_sk_state_store(sk, TCP_LISTEN);
+	sock_set_flag(sk, SOCK_RCU_FREE);
 
 	err = ssock->ops->listen(ssock, backlog);
-	inet_sk_state_store(sock->sk, inet_sk_state_load(ssock->sk));
+	inet_sk_state_store(sk, inet_sk_state_load(ssock->sk));
 	if (!err)
-		mptcp_copy_inaddrs(sock->sk, ssock->sk);
+		mptcp_copy_inaddrs(sk, ssock->sk);
 
 unlock:
-	release_sock(sock->sk);
+	release_sock(sk);
 	return err;
 }
 
