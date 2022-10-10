285db6420293959e680648c11160a6100cafcf4f
Dmytro Shytyi <dmytro@shytyi.net>
2022-10-10 22:18:07 +0000

    mptcp: add subflow_v(4,6)_send_synack()
    In this patch we add skb to the msk, dequeue it from sk, remove TSs and
    do skb mapping.
    
    Signed-off-by: Dmytro Shytyi <dmytro@shytyi.net>
    Message-Id: <20221010221809.1792-4-dmytro@shytyi.net>

--- net/mptcp/fastopen.c
+++ net/mptcp/fastopen.c
@@ -4,6 +4,84 @@
 
 #include "protocol.h"
 
+struct mptcp_skb_cb {
+	u64 map_seq;
+	u64 end_seq;
+	u32 offset;
+	u8  has_rxtstamp:1;
+};
+
+#define MPTCP_SKB_CB(__skb)	((struct mptcp_skb_cb *)&((__skb)->cb[0]))
+
+void subflow_fastopen_send_synack_set_params(struct mptcp_subflow_context *subflow,
+					     struct request_sock *req)
+{
+	struct tcp_request_sock *treq = tcp_rsk(req);
+	struct sock *ssk = subflow->tcp_sock;
+	struct sock *sk = subflow->conn;
+	struct mptcp_sock *msk;
+	struct sk_buff *skb;
+	struct tcp_sock *tp;
+	u32 offset;
+
+	msk = mptcp_sk(sk);
+	tp = tcp_sk(ssk);
+
+	/* mark subflow/msk as "mptfo" */
+	msk->is_mptfo = 1;
+
+	skb = skb_peek(&ssk->sk_receive_queue);
+
+	/* dequeue the skb from sk receive queue */
+	__skb_unlink(skb, &ssk->sk_receive_queue);
+	skb_ext_reset(skb);
+	skb_orphan(skb);
+
+	/* set the skb mapping */
+	tp->copied_seq += tp->rcv_nxt - treq->rcv_isn - 1;
+	subflow->map_seq = mptcp_subflow_get_mapped_dsn(subflow);
+	subflow->ssn_offset = tp->copied_seq - 1;
+
+	/* innitialize MPTCP_CB */
+	offset = tp->copied_seq - TCP_SKB_CB(skb)->seq;
+	MPTCP_SKB_CB(skb)->map_seq = mptcp_subflow_get_mapped_dsn(subflow);
+	MPTCP_SKB_CB(skb)->end_seq = MPTCP_SKB_CB(skb)->map_seq +
+				     (skb->len - offset);
+	MPTCP_SKB_CB(skb)->offset = offset;
+	MPTCP_SKB_CB(skb)->has_rxtstamp = TCP_SKB_CB(skb)->has_rxtstamp;
+
+	mptcp_data_lock(sk);
+
+	mptcp_set_owner_r(skb, sk);
+	__skb_queue_tail(&msk->receive_queue, skb);
+
+	(sk)->sk_data_ready(sk);
+
+	mptcp_data_unlock(sk);
+}
+
+void __mptcp_pre_connect(struct mptcp_sock *msk, struct sock *ssk,
+			 struct msghdr *msg, size_t size)
+{
+	struct tcp_sock *tp;
+	struct sk_buff *skb;
+	struct ubuf_info *uarg;
+
+	lock_sock(ssk);
+
+	tp = tcp_sk(ssk);
+
+	skb = tcp_write_queue_tail(ssk);
+	uarg = msg_zerocopy_realloc(ssk, size, skb_zcopy(skb));
+	tp->fastopen_req = kzalloc(sizeof(*tp->fastopen_req),
+				   ssk->sk_allocation);
+	tp->fastopen_req->data = msg;
+	tp->fastopen_req->size = size;
+	tp->fastopen_req->uarg = uarg;
+
+	release_sock(ssk);
+}
+
 void mptcp_gen_msk_ackseq_fastopen(struct mptcp_sock *msk, struct mptcp_subflow_context *subflow,
 				   struct mptcp_options_received mp_opt)
 {
