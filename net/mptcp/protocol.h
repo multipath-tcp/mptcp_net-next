2533607a21d7b3a98a75c976c7b06d432d51b8af
Geliang Tang <geliang.tang@suse.com>
2022-11-08 14:45:09 +0800

    mptcp: use get_send wrapper
    This patch defines the packet scheduler wrapper mptcp_sched_get_send(),
    invoke data_init() and get_subflow() of msk->sched in it.
    
    Set data->reinject to false in mptcp_sched_get_send(). If msk->sched is
    NULL, use default functions mptcp_subflow_get_send() to send data.
    
    Move sock_owned_by_me() check and fallback check into the wrapper from
    mptcp_subflow_get_send().
    
    Add the multiple subflows support for __mptcp_push_pending() and
    __mptcp_subflow_push_pending(). Use get_send() wrapper instead of
    mptcp_subflow_get_send() in them.
    
    Check the subflow scheduled flags to test which subflow or subflows are
    picked by the scheduler, use them to send data.
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Message-Id: <1fd54289137d2fe466c13ae43686101853cc3881.1667889809.git.geliang.tang@suse.com>

--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -640,6 +640,8 @@ int mptcp_init_sched(struct mptcp_sock *msk,
 void mptcp_release_sched(struct mptcp_sock *msk);
 void mptcp_subflow_set_scheduled(struct mptcp_subflow_context *subflow,
 				 bool scheduled);
+struct sock *mptcp_subflow_get_send(struct mptcp_sock *msk);
+int mptcp_sched_get_send(struct mptcp_sock *msk);
 
 static inline bool __tcp_can_send(const struct sock *ssk)
 {
--- net/mptcp/sched.c
+++ net/mptcp/sched.c
@@ -112,3 +112,40 @@ void mptcp_sched_data_set_contexts(const struct mptcp_sock *msk,
 	for (; i < MPTCP_SUBFLOWS_MAX; i++)
 		data->contexts[i] = NULL;
 }
+
+int mptcp_sched_get_send(struct mptcp_sock *msk)
+{
+	struct mptcp_subflow_context *subflow;
+	struct mptcp_sched_data data;
+	struct sock *ssk = NULL;
+
+	sock_owned_by_me((const struct sock *)msk);
+
+	mptcp_for_each_subflow(msk, subflow) {
+		if (READ_ONCE(subflow->scheduled))
+			return 0;
+	}
+
+	/* the following check is moved out of mptcp_subflow_get_send */
+	if (__mptcp_check_fallback(msk)) {
+		if (msk->first &&
+		    __tcp_can_send(msk->first) &&
+		    sk_stream_memory_free(msk->first)) {
+			mptcp_subflow_set_scheduled(mptcp_subflow_ctx(msk->first), true);
+			return 0;
+		}
+		return -EINVAL;
+	}
+
+	if (!msk->sched) {
+		ssk = mptcp_subflow_get_send(msk);
+		if (!ssk)
+			return -EINVAL;
+		mptcp_subflow_set_scheduled(mptcp_subflow_ctx(ssk), true);
+		return 0;
+	}
+
+	data.reinject = false;
+	msk->sched->data_init(msk, &data);
+	return msk->sched->get_subflow(msk, &data);
+}
