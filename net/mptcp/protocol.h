465f50477783be978316c0f0573e7dd672149ba3
Dmytro Shytyi <dmytro@shytyi.net>
2022-11-14 10:15:31 +0100

    mptcp: add subflow_v(4,6)_send_synack()
    In this patch we add skb to the msk, dequeue it from sk, remove TSs and
    do skb mapping.
    
    Co-developed-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Co-developed-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Dmytro Shytyi <dmytro@shytyi.net>

--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -840,6 +840,9 @@ bool mptcp_userspace_pm_active(const struct mptcp_sock *msk);
 // Fast Open Mechanism functions begin
 void mptcp_gen_msk_ackseq_fastopen(struct mptcp_sock *msk, struct mptcp_subflow_context *subflow,
 				   const struct mptcp_options_received *mp_opt);
+void mptcp_set_owner_r(struct sk_buff *skb, struct sock *sk);
+void subflow_fastopen_send_synack_set_params(struct mptcp_subflow_context *subflow,
+					     struct request_sock *req);
 // Fast Open Mechanism functions end
 
 static inline bool mptcp_pm_should_add_signal(struct mptcp_sock *msk)
