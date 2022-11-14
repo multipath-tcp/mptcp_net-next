fb0ce5bead560d5469082c1cc31f705aca36c035
Paolo Abeni <pabeni@redhat.com>
2022-11-14 12:54:05 +0100

    Squash-to: "mptcp: implement delayed seq generation for passive fastopen"
    We should not use double forward slashes for comments. The affending
    comments could be removed altogether.
    
    Additionally fix the  SPDX license indentifier
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Message-Id: <2b4b21555e06719ee3923f4ed1b0f00c2a3ef507.1668426733.git.pabeni@redhat.com>

--- net/mptcp/fastopen.c
+++ net/mptcp/fastopen.c
@@ -1,5 +1,7 @@
-/* SPDX-License-Identifier: GPL-2.0
- * MPTCP Fast Open Mechanism. Copyright (c) 2021-2022, Dmytro SHYTYI
+// SPDX-License-Identifier: GPL-2.0
+/* MPTCP Fast Open Mechanism
+ *
+ * Copyright (c) 2021-2022, Dmytro SHYTYI
  */
 
 #include "protocol.h"
--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -848,13 +848,11 @@ void mptcp_event_addr_announced(const struct sock *ssk, const struct mptcp_addr_
 void mptcp_event_addr_removed(const struct mptcp_sock *msk, u8 id);
 bool mptcp_userspace_pm_active(const struct mptcp_sock *msk);
 
-// Fast Open Mechanism functions begin
 void mptcp_gen_msk_ackseq_fastopen(struct mptcp_sock *msk, struct mptcp_subflow_context *subflow,
 				   const struct mptcp_options_received *mp_opt);
 void mptcp_set_owner_r(struct sk_buff *skb, struct sock *sk);
 void subflow_fastopen_send_synack_set_params(struct mptcp_subflow_context *subflow,
 					     struct request_sock *req);
-// Fast Open Mechanism functions end
 
 static inline bool mptcp_pm_should_add_signal(struct mptcp_sock *msk)
 {
