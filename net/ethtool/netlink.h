torvalds/linux

commit dc0b98a1758f0d2296349ca23ac88804b922e88d

ethtool: Add and use ethnl_update_bool.
Signed-off-by: David S. Miller <davem@davemloft.net>

net/ethtool/netlink.h
@@ -138,6 +138,32 @@ static inline void ethnl_update_bool32(u32 *dst, const struct nlattr *attr,
 }
 
 /**
+ * ethnl_update_bool() - updateb bool used as bool from NLA_U8 attribute
+ * @dst:  value to update
+ * @attr: netlink attribute with new value or null
+ * @mod:  pointer to bool for modification tracking
+ *
+ * Use the bool value from NLA_U8 netlink attribute @attr to set bool variable
+ * pointed to by @dst to 0 (if zero) or 1 (if not); do nothing if @attr is
+ * null. Bool pointed to by @mod is set to true if this function changed the
+ * logical value of *dst, otherwise it is left as is.
+ */
+static inline void ethnl_update_bool(bool *dst, const struct nlattr *attr,
+				     bool *mod)
+{
+	u8 val;
+
+	if (!attr)
+		return;
+	val = !!nla_get_u8(attr);
+	if (!!*dst == val)
+		return;
+
+	*dst = val;
+	*mod = true;
+}
+
+/**
  * ethnl_update_binary() - update binary data from NLA_BINARY attribute
  * @dst:  value to update
  * @len:  destination buffer length

net/ethtool/mm.c
@@ -225,7 +225,7 @@ int ethnl_set_mm(struct sk_buff *skb, struct genl_info *info)
 	ethnl_update_u32(&cfg.verify_time, tb[ETHTOOL_A_MM_VERIFY_TIME], &mod);
 	ethnl_update_bool(&cfg.tx_enabled, tb[ETHTOOL_A_MM_TX_ENABLED], &mod);
 	ethnl_update_bool(&cfg.pmac_enabled, tb[ETHTOOL_A_MM_PMAC_ENABLED],
-			  &mod);
+			    &mod);
 	ethnl_update_u32(&cfg.tx_min_frag_size,
 			 tb[ETHTOOL_A_MM_TX_MIN_FRAG_SIZE], &mod);
 

multipath-tcp/mptcp_net-next

commit b8b068bfa286a58ff4900d756a1ca78961919679

net: ethtool: fix up for "ethtool: Add and use ethnl_update_bool."
After merging the net-next tree, today's linux-next build (powerpc
ppc64_defconfig) failed like this:

  In file included from net/ethtool/netlink.c:6:
  net/ethtool/netlink.h:177:20: error: redefinition of 'ethnl_update_bool'
    177 | static inline void ethnl_update_bool(bool *dst, const struct nlattr *attr,
        |                    ^~~~~~~~~~~~~~~~~
  net/ethtool/netlink.h:125:20: note: previous definition of 'ethnl_update_bool' with type 'void(bool *, const struct nlattr *, bool *)' {aka 'void(_Bool *, const struct nlattr *, _Bool *)'}
    125 | static inline void ethnl_update_bool(bool *dst, const struct nlattr *attr,
        |                    ^~~~~~~~~~~~~~~~~

Caused by:

  commit dc0b98a1758f ("ethtool: Add and use ethnl_update_bool.")

merging badly with:

  commit 7c494a7749a7 ("net: ethtool: netlink: introduce ethnl_update_bool()")

from the net tree.

The version from net-next -- commit 7c494a7749a7 ("net: ethtool:
netlink: introduce ethnl_update_bool()") -- has been taken, keeping the
one from net.

Fixes: dc0b98a1758f ("ethtool: Add and use ethnl_update_bool.")
Co-developed-by: Matthieu Baerts <matthieu.baerts@tessares.net>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/ethtool/netlink.h
@@ -164,32 +164,6 @@ static inline void ethnl_update_bool32(u32 *dst, const struct nlattr *attr,
 }
 
 /**
- * ethnl_update_bool() - updateb bool used as bool from NLA_U8 attribute
- * @dst:  value to update
- * @attr: netlink attribute with new value or null
- * @mod:  pointer to bool for modification tracking
- *
- * Use the bool value from NLA_U8 netlink attribute @attr to set bool variable
- * pointed to by @dst to 0 (if zero) or 1 (if not); do nothing if @attr is
- * null. Bool pointed to by @mod is set to true if this function changed the
- * logical value of *dst, otherwise it is left as is.
- */
-static inline void ethnl_update_bool(bool *dst, const struct nlattr *attr,
-				     bool *mod)
-{
-	u8 val;
-
-	if (!attr)
-		return;
-	val = !!nla_get_u8(attr);
-	if (!!*dst == val)
-		return;
-
-	*dst = val;
-	*mod = true;
-}
-
-/**
  * ethnl_update_binary() - update binary data from NLA_BINARY attribute
  * @dst:  value to update
  * @len:  destination buffer length