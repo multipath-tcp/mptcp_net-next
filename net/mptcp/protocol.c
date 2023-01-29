b27a45e00dcf2395c9fec5e6451cb966194b7a61
Paolo Abeni <pabeni@redhat.com>
2023-01-29 05:49:52 +0000

    DO-NOT-MERGE: mptcp: use kmalloc on kasan build
    Helps detection UaF, which apparently kasan misses
    with kmem_cache allocator.
    
    We also need to always set the SOCK_RCU_FREE flag, to
    preserved the current code leveraging SLAB_TYPESAFE_BY_RCU.
    This latter change will make unreachable some existing
    errors path, but I don't see other options.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -2723,6 +2723,10 @@ static int __mptcp_init_sock(struct sock *sk)
 	timer_setup(&msk->sk.icsk_retransmit_timer, mptcp_retransmit_timer, 0);
 	timer_setup(&sk->sk_timer, mptcp_timeout_timer, 0);
 
+#if IS_ENABLED(CONFIG_KASAN)
+	sock_set_flag(sk, SOCK_RCU_FREE);
+#endif
+
 	return 0;
 }
 
@@ -3139,7 +3143,9 @@ struct sock *mptcp_sk_clone(const struct sock *sk,
 	msk->setsockopt_seq = mptcp_sk(sk)->setsockopt_seq;
 	mptcp_init_sched(msk, mptcp_sk(sk)->sched);
 
+#if !IS_ENABLED(CONFIG_KASAN)
 	sock_reset_flag(nsk, SOCK_RCU_FREE);
+#endif
 	/* will be fully established after successful MPC subflow creation */
 	inet_sk_state_store(nsk, TCP_SYN_RECV);
 
@@ -3908,6 +3914,12 @@ static int mptcp_napi_poll(struct napi_struct *napi, int budget)
 	return work_done;
 }
 
+#if IS_ENABLED(CONFIG_KASAN)
+#define MPTCP_USE_SLAB		0
+#else
+#define MPTCP_USE_SLAB		1
+#endif
+
 void __init mptcp_proto_init(void)
 {
 	struct mptcp_delegated_action *delegated;
@@ -3931,7 +3943,7 @@ void __init mptcp_proto_init(void)
 	mptcp_pm_init();
 	mptcp_token_init();
 
-	if (proto_register(&mptcp_prot, 1) != 0)
+	if (proto_register(&mptcp_prot, MPTCP_USE_SLAB) != 0)
 		panic("Failed to register MPTCP proto.\n");
 
 	inet_register_protosw(&mptcp_protosw);
@@ -3984,7 +3996,7 @@ int __init mptcp_proto_v6_init(void)
 	mptcp_v6_prot.slab = NULL;
 	mptcp_v6_prot.obj_size = sizeof(struct mptcp6_sock);
 
-	err = proto_register(&mptcp_v6_prot, 1);
+	err = proto_register(&mptcp_v6_prot, MPTCP_USE_SLAB);
 	if (err)
 		return err;
 
