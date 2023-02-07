0d8001c40073b5e6ffcfa22e267ed8cf7a637295
Matthieu Baerts <matthieu.baerts@tessares.net>
2023-02-07 13:58:11 +0100

    utils: git commit: also check export-net
    Useful info.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/git-has-commit.sh
+++ utils/git-has-commit.sh
@@ -12,6 +12,6 @@ fi
 
 for c in "${@}"; do
 	git log --oneline -1 "${c}"
-	git branch --remote --contains "${c}" netdev-net/main netdev-next/main origin/export
+	git branch --remote --contains "${c}" netdev-net/main netdev-next/main origin/export origin/export-net
 	git tag --list v"[5-9]*" --contains="${c}" | sort -V | head -n1
 done
