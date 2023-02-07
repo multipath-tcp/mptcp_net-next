8c24eecdae5abcebdc9d0afa0a7bb3de8b829698
Matthieu Baerts <matthieu.baerts@tessares.net>
2023-02-07 11:02:19 +0100

    utils: append stable: support more cases
    - Skip b4 tracking commit
    - Insert before last SoB if no fixes
    - Print messages
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/append-cc-stable.sh
+++ utils/append-cc-stable.sh
@@ -1,14 +1,33 @@
 #! /bin/bash
 git filter-repo --message-callback '
         if b"Cc: stable@vger.kernel.org" in message:
+          print("  [ Skip ] stable is already being Cced")
           return message
+
+        if b"--- b4-submit-tracking ---" in message:
+          print("  [ Skip ] b4 tracking branch")
+          return message
+
         m = []
         added = False
-        for line in message.splitlines():
-          m.append(line)
-          if not added and line.startswith(b"Fixes: "):
-            added = True
-            m.append(b"Cc: stable@vger.kernel.org")
-        if not added:
+        has_fixes = b"\nFixes: " in message
+
+        def _ins(msg):
+          nonlocal added
+          added = True
           m.append(b"Cc: stable@vger.kernel.org")
+          print("  [Insert] (" + msg + ") " + m[0].decode("utf-8"))
+
+        for line in message.splitlines():
+          if not added and not has_fixes and line.startswith(b"Signed-off-by: "):
+            _ins("before SoB ")
+
+          m.append(line)
+
+          if not added and has_fixes and line.startswith(b"Fixes: "):
+            _ins("after Fixes")
+
+        if not added:
+          _ins("at the end")
+
         return b"\n".join(m)' --refs "${1?}"
