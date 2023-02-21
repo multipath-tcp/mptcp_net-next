b958b13e351c67e99e028a8e4bf6d8976a8d3778
Paolo Abeni <pabeni@redhat.com>
2023-02-21 05:53:19 +0000

    mptcp: drop legacy code
    After the previous commits the PM worker can't race anymore
    with the unaccepted subflow close and disposal, as the msk
    keeps a reference to such subflow.
    
    We can remove the now irrelevant and confusing checks explicitly
    preventing the mentioned race.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Reviewed-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/mptcp/subflow.c
+++ net/mptcp/subflow.c
@@ -1944,11 +1944,10 @@ static void subflow_ulp_release(struct sock *ssk)
 
 	sk = ctx->conn;
 	if (sk) {
-		/* if the msk has been orphaned, keep the ctx
-		 * alive, will be freed by __mptcp_close_ssk(),
-		 * when the subflow is still unaccepted
+		/* if the subflow has been closed by the TCP stack, keep
+		 * the ctx alive, will be freed by __mptcp_close_ssk()
 		 */
-		release = ctx->disposable || list_empty(&ctx->node);
+		release = ctx->disposable;
 		sock_put(sk);
 	}
 
