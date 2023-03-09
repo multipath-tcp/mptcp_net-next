multipath-tcp/mptcp_net-next

commit 2517e6c6c7cd0245ef3a14694435089e42e43ba7

mptcp: improve last merge conflict resolution
Paolo suggested a simpler version: we don't need to go through the
WARN_ON_ONCE() because fallback clears is_mptcp, so is not covered by
this scenario.

Fixes: d8ede34f63e6 ("tgupdate: merge remotes/origin/t/mptcp-simplify-subflow_syn_recv_sock onto t/mptcp-simplify-subflow_syn_recv_sock base")
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/subflow.c
@@ -882,11 +882,6 @@ create_child:
 			SUBFLOW_REQ_INC_STATS(req, MPTCP_MIB_JOINACKRX);
 			tcp_rsk(req)->drop_req = true;
 		}
-
-		goto out;
-
-fallback:
-		mptcp_subflow_drop_ctx(child);
 	}
 
 out:
@@ -906,6 +901,10 @@ dispose_child:
 	req->rsk_ops->send_reset(sk, skb);
 
 	/* The last child reference will be released by the caller */
+	return child;
+
+fallback:
+	mptcp_subflow_drop_ctx(child);
 	return child;
 }
 

commit 082329a7644a06017cc9f7f5fd2611e04b096cce

mptcp: fix compilation error
  error: label 'out' defined but not used [-Werror=unused-label]

Fixes: 2517e6c6c7cd ("mptcp: improve last merge conflict resolution")
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/subflow.c
@@ -884,7 +884,6 @@ create_child:
 		}
 	}
 
-out:
 	/* check for expected invariant - should never trigger, just help
 	 * catching eariler subtle bugs
 	 */