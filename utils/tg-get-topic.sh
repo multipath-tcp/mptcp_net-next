afc58ea9e999a62530f74bedae941755a456e2b5
Matthieu Baerts <matthieu.baerts@tessares.net>
2023-03-09 09:50:41 +0100

    utils: get topic: force using t/upstream series
    This series covers most of our topics.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/tg-get-topic.sh
+++ utils/tg-get-topic.sh
@@ -1,3 +1,3 @@
 #! /bin/bash -e
 : "${1?}"
-tg info --series | grep "\[PATCH\] ${*}$" | sed 's/^. //g' | awk '{ print $1 }'
+tg info --series=t/upstream | grep "\[PATCH\] ${*}$" | sed 's/^. //g' | awk '{ print $1 }'
