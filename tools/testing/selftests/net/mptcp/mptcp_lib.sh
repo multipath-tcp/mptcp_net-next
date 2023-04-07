0d8ad6e9f815baa2d61c3f82c2bf694ec06864c2
Matthieu Baerts <matthieu.baerts@tessares.net>
2023-04-07 19:06:42 +0200

    selftests: mptcp: lib: skip if missing symbol
    Selftests are supposed to run on any kernels, including the old ones not
    supporting all MPTCP features.
    
    New functions are now available to easily detect if a feature is
    missing. It is going to be used in the following commits. In order to
    ease the backport of such future patches, it would be good if this
    present commit is backported up to the introduction of MPTCP selftests,
    hence the Fixes tag below: this type of check was supposed to be done
    from the beginning.
    
    Link: https://github.com/multipath-tcp/mptcp_net-next/issues/368
    Fixes: 048d19d444be ("mptcp: add basic kselftest for mptcp")
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Message-Id: <20230406-mptcp-issue-368-selftests-old-kernels-v1-8-246ac567724d@tessares.net>

--- tools/testing/selftests/net/mptcp/mptcp_lib.sh
+++ tools/testing/selftests/net/mptcp/mptcp_lib.sh
@@ -1,11 +1,34 @@
 #! /bin/bash
 # SPDX-License-Identifier: GPL-2.0
 
+readonly KSFT_FAIL=1
 readonly KSFT_SKIP=4
 
 mptcp_lib_check_mptcp() {
 	if [ ! -f "/proc/sys/net/mptcp/enabled" ]; then
 		echo "SKIP: MPTCP support is not available"
 		exit ${KSFT_SKIP}
+	fi
+}
+
+mptcp_lib_check_kallsyms() {
+	if [ ! -f /proc/kallsyms ]; then
+		echo "SKIP: CONFIG_KALLSYMS is missing"
+		exit ${KSFT_SKIP}
+	fi
+}
+
+# $1: part of a symbol to look at, add '$' at the end for full name
+mptcp_lib_kallsyms_has() {
+	local sym="${1}"
+
+	if ! grep -q " ${sym}" /proc/kallsyms; then
+		# We want our CI to complain if a symbol is not found
+		if [ "${SELFTESTS_MPTCP_LIB_EXTRA_CHECKS:-}" = "1" ]; then
+			echo "ERROR: ${sym} symbol has not been found"
+			exit ${KSFT_FAIL}
+		fi
+
+		return 1
 	fi
 }
