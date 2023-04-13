a7dd41a3681cb52009f7516a57d0fb4755996e57
Geliang Tang <geliang.tang@suse.com>
2023-04-13 17:12:37 +0800

    mptcp: don't clear userspace pm addr id
    Don't clear the addr id in mptcp_userspace_pm_get_local_id(), clear it
    in mptcp_pm_nl_get_local_id() instead.
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Message-Id: <aa8f725b79aef07e0d29ed4ad64a34aa61a51aff.1681376983.git.geliang.tang@suse.com>

--- net/mptcp/pm_netlink.c
+++ net/mptcp/pm_netlink.c
@@ -1055,8 +1055,8 @@ static int mptcp_pm_nl_create_listen_socket(struct sock *sk,
 
 int mptcp_pm_nl_get_local_id(struct mptcp_sock *msk, struct sock_common *skc)
 {
+	struct mptcp_addr_info skc_local = { 0 };
 	struct mptcp_pm_addr_entry *entry;
-	struct mptcp_addr_info skc_local;
 	struct mptcp_addr_info msk_local;
 	struct pm_nl_pernet *pernet;
 	int ret = -1;
