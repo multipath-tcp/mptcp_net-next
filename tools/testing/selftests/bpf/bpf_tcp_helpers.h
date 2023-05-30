3b36eefe1d0db7297c3a303616a00d13b8927619
Geliang Tang <geliang.tang@suse.com>
2023-05-30 21:17:34 +0800

    selftests/bpf: Add bpf_burst scheduler
    This patch implements the burst BPF MPTCP scheduler, named bpf_burst,
    which is the default scheduler in protocol.c.
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Message-Id: <bd209c2426adc1a210b1ded5fc87f42dc833d632.1685452619.git.geliang.tang@suse.com>

--- tools/testing/selftests/bpf/bpf_tcp_helpers.h
+++ tools/testing/selftests/bpf/bpf_tcp_helpers.h
@@ -234,12 +234,15 @@ extern void tcp_cong_avoid_ai(struct tcp_sock *tp, __u32 w, __u32 acked) __ksym;
 #define MPTCP_SUBFLOWS_MAX	8
 
 struct mptcp_subflow_context {
+	unsigned long avg_pacing_rate;
 	__u32	backup : 1;
+	__u8	stale_count;
 	struct	sock *tcp_sock;	    /* tcp sk backpointer */
 } __attribute__((preserve_access_index));
 
 struct mptcp_sched_data {
 	struct sock	*last_snd;
+	int		snd_burst;
 	bool		reinject;
 	struct mptcp_subflow_context *contexts[MPTCP_SUBFLOWS_MAX];
 } __attribute__((preserve_access_index));
