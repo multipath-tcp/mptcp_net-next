b084e79d86dad3b7ce1abc1272e424b7554b5398
Matthieu Baerts <matthieu.baerts@tessares.net>
2023-06-06 12:16:09 +0200

    utils: virtme bisect: kill stress-ng
    STRESS_PID was only set in the background, not available in the main
    process. So we need to manually kill stress-ng.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- utils/virtme_bisect.sh
+++ utils/virtme_bisect.sh
@@ -3,8 +3,7 @@
 : "${MODE:=normal}"
 : "${STRESS:=0}"
 
-STRESS_PID=
-stress() { local i=0
+stress() { local i=0 pid
 	while [[ $((i++)) -lt 500 ]]; do
 		# shellcheck disable=SC2009 # we grep on the args
 		ps aux | grep "[/]run/virtme/guesttools/virtme-init" && break
@@ -12,20 +11,20 @@ stress() { local i=0
 	done
 	sleep 30
 	stress-ng --cpu 24 --io 24 --vm 24 --vm-bytes 1G --timeout 60m &
-	STRESS_PID=$!
+	pid=$!
 
-	echo -e "\n\n=== Stress in progress ($i -- ${STRESS_PID}) ===\n"
-	wait ${STRESS_PID}
+	echo -e "\n\n=== Stress in progress ($i -- ${pid}) ===\n"
+	wait ${pid}
 }
 
 exit_trap() {
 	rc=$?
 
-	echo -e "${0}: exit trap (stress: ${STRESS_PID} ; $(jobs -p))"
+	echo -e "${0}: exit trap (stress: ${STRESS} ; $(jobs -p))"
 
 	docker ps --filter ancestor=mptcp/mptcp-upstream-virtme-docker --format='{{.ID}}' | xargs -r docker stop
 	jobs -p | xargs -r kill
-	echo "${STRESS_PID}" | xargs -r kill
+	[ "${STRESS}" = 1  ] && pkill stress-ng
 	sleep 1
 
 	return ${rc}
