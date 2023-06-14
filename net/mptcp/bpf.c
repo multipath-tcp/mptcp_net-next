12b7afad801dccc88c98144a57426d516da90a02
Geliang Tang <geliang.tang@suse.com>
2023-06-14 10:20:57 +0800

    Squash to "bpf: Add bpf_mptcp_sched_kfunc_set"
    Export mptcp_subflow_ctx_by_pos too.
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Message-Id: <6b07055614d61ee2c2c5378f45953d5e762f475c.1686709116.git.geliang.tang@suse.com>

--- net/mptcp/bpf.c
+++ net/mptcp/bpf.c
@@ -172,6 +172,7 @@ struct bpf_struct_ops bpf_mptcp_sched_ops = {
 BTF_SET8_START(bpf_mptcp_sched_kfunc_ids)
 BTF_ID_FLAGS(func, mptcp_subflow_set_scheduled)
 BTF_ID_FLAGS(func, mptcp_sched_data_set_contexts)
+BTF_ID_FLAGS(func, mptcp_subflow_ctx_by_pos)
 BTF_SET8_END(bpf_mptcp_sched_kfunc_ids)
 
 static const struct btf_kfunc_id_set bpf_mptcp_sched_kfunc_set = {
