0a920c696b80fda63cfa2a737fb84ab0a199a816
Geliang Tang <geliang.tang@suse.com>
2023-07-18 11:36:42 +0800

    Squash to "selftests/bpf: Add bpf_bkup test"
    Start server with PORT_1.
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Link: https://lore.kernel.org/r/860ce049383d29483314c55b4fd098420fa93055.1689650435.git.geliang.tang@suse.com
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- tools/testing/selftests/bpf/prog_tests/mptcp.c
+++ tools/testing/selftests/bpf/prog_tests/mptcp.c
@@ -402,7 +402,7 @@ static void test_bkup(void)
 	nstoken = sched_init("subflow backup", "bpf_bkup");
 	if (!ASSERT_OK_PTR(nstoken, "sched_init:bpf_bkup"))
 		goto fail;
-	server_fd = start_mptcp_server(AF_INET, ADDR_1, 0, 0);
+	server_fd = start_mptcp_server(AF_INET, ADDR_1, PORT_1, 0);
 	client_fd = connect_to_fd(server_fd, 0);
 
 	send_data(server_fd, client_fd, "bpf_bkup");
