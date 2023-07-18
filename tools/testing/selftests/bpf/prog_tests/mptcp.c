877ca908b3b0df7c6af64b922db1c52e8369585e
Geliang Tang <geliang.tang@suse.com>
2023-07-18 11:36:40 +0800

    Squash to "selftests/bpf: Add bpf_first test"
    Start server with PORT_1.
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Message-Id: <9008280de471fb3dac1db9d8260b955b25d9e28c.1689650435.git.geliang.tang@suse.com>

--- tools/testing/selftests/bpf/prog_tests/mptcp.c
+++ tools/testing/selftests/bpf/prog_tests/mptcp.c
@@ -369,7 +369,7 @@ static void test_first(void)
 	nstoken = sched_init("subflow", "bpf_first");
 	if (!ASSERT_OK_PTR(nstoken, "sched_init:bpf_first"))
 		goto fail;
-	server_fd = start_mptcp_server(AF_INET, ADDR_1, 0, 0);
+	server_fd = start_mptcp_server(AF_INET, ADDR_1, PORT_1, 0);
 	client_fd = connect_to_fd(server_fd, 0);
 
 	send_data(server_fd, client_fd, "bpf_first");
