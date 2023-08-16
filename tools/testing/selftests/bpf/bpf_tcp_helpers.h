24331da9157f86b2525ffe436b9c4dfd4537f638
Geliang Tang <geliang.tang@suse.com>
2023-08-16 13:57:56 +0200

    selftests/bpf: Add bpf_rr scheduler
    This patch implements the round-robin BPF MPTCP scheduler, named bpf_rr,
    which always picks the next available subflow to send data. If no such
    next subflow available, picks the first one.
    
    Reviewed-by: Mat Martineau <martineau@kernel.org>
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>

--- tools/testing/selftests/bpf/bpf_tcp_helpers.h
+++ tools/testing/selftests/bpf/bpf_tcp_helpers.h
@@ -270,5 +270,10 @@ extern void mptcp_sched_data_set_contexts(const struct mptcp_sock *msk,
 					  struct mptcp_sched_data *data) __ksym;
 extern struct mptcp_subflow_context *
 mptcp_subflow_ctx_by_pos(const struct mptcp_sched_data *data, unsigned int pos) __ksym;
+static __always_inline struct sock *
+mptcp_subflow_tcp_sock(const struct mptcp_subflow_context *subflow)
+{
+	return subflow->tcp_sock;
+}
 
 #endif
