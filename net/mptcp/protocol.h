multipath-tcp/mptcp_net-next

commit 574847a8b558eba174a6738a31b3480b5faa70c2

Squash to "bpf: Export more bpf_burst related functions"
Add "ignore -Wmissing-prototypes" for bpf_mptcp_subflow_queues_empty(),
drop the declarations in protocol.h.

Drop bpf_mptcp_subflow_memory_free(), it will be defined in BPF context.

Update bpf_mptcp_subflow_queues_empty().

Signed-off-by: Geliang Tang <geliang.tang@suse.com>
Link: https://lore.kernel.org/r/644d34afc1b3b8d72b7c87baf099417d0be561ec.1691036765.git.geliang.tang@suse.com
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/protocol.h
@@ -642,8 +642,6 @@ void mptcp_subflow_queue_clean(struct sock *sk, struct sock *ssk);
 void mptcp_sock_graft(struct sock *sk, struct socket *parent);
 u64 mptcp_wnd_end(const struct mptcp_sock *msk);
 void mptcp_set_timeout(struct sock *sk);
-bool bpf_mptcp_subflow_memory_free(const struct sock *sk);
-bool bpf_mptcp_subflow_queues_empty(const struct sock *sk);
 struct sock *__mptcp_nmpc_sk(struct mptcp_sock *msk);
 bool __mptcp_close(struct sock *sk, long timeout);
 void mptcp_cancel_work(struct sock *sk);

net/mptcp/bpf.c
@@ -145,15 +145,16 @@ struct bpf_struct_ops bpf_mptcp_sched_ops = {
 	.name		= "mptcp_sched_ops",
 };
 
-bool bpf_mptcp_subflow_memory_free(const struct sock *sk)
+__diag_push();
+__diag_ignore_all("-Wmissing-prototypes",
+		  "kfuncs which will be used in BPF programs");
+
+bool bpf_mptcp_subflow_queues_empty(struct sock *sk)
 {
-	return sk_stream_memory_free(sk);
+	return tcp_rtx_queue_empty(sk);
 }
 
-bool bpf_mptcp_subflow_queues_empty(const struct sock *sk)
-{
-	return tcp_rtx_and_write_queues_empty(sk);
-}
+__diag_pop();
 
 BTF_SET8_START(bpf_mptcp_sched_kfunc_ids)
 BTF_ID_FLAGS(func, mptcp_subflow_set_scheduled)
@@ -162,7 +163,7 @@ BTF_ID_FLAGS(func, mptcp_subflow_ctx_by_pos)
 BTF_ID_FLAGS(func, mptcp_subflow_active)
 BTF_ID_FLAGS(func, mptcp_set_timeout)
 BTF_ID_FLAGS(func, mptcp_wnd_end)
-BTF_ID_FLAGS(func, bpf_mptcp_subflow_memory_free)
+BTF_ID_FLAGS(func, tcp_stream_memory_free)
 BTF_ID_FLAGS(func, bpf_mptcp_subflow_queues_empty)
 BTF_ID_FLAGS(func, mptcp_pm_subflow_chk_stale)
 BTF_SET8_END(bpf_mptcp_sched_kfunc_ids)

commit 11ace9718934cd184ce580b823dc4792777a2b2c

mptcp: partially revert "Squash to "bpf: Export more bpf_burst related functions""
To avoid this error reported by Sparse:

  $ make C=1 net/mptcp/bpf.o
  net/mptcp/bpf.c:152:6: warning: symbol 'bpf_mptcp_subflow_queues_empty' was not declared. Should it be static?

Fixes: 574847a8b558 ("Squash to "bpf: Export more bpf_burst related functions"")
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

net/mptcp/protocol.h
@@ -642,6 +642,7 @@ void mptcp_subflow_queue_clean(struct sock *sk, struct sock *ssk);
 void mptcp_sock_graft(struct sock *sk, struct socket *parent);
 u64 mptcp_wnd_end(const struct mptcp_sock *msk);
 void mptcp_set_timeout(struct sock *sk);
+bool bpf_mptcp_subflow_queues_empty(struct sock *sk);
 struct sock *__mptcp_nmpc_sk(struct mptcp_sock *msk);
 bool __mptcp_close(struct sock *sk, long timeout);
 void mptcp_cancel_work(struct sock *sk);