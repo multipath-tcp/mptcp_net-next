d078f82a094afe02253a3fd7c92a7fd98f333b70
Geliang Tang <geliang.tang@suse.com>
2023-10-12 16:52:01 +0800

    Squash to "mptcp: add mptcpi_subflows_total counter"
    When the socket shuts down, it's state is TCPF_FIN_WAIT2. Add this into
    __mptcp_has_initial_subflow.
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Message-Id: <6e9e20ed1350ac793b682042ae32be6808c1a1e4.1697100600.git.geliang.tang@suse.com>

--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -1081,7 +1081,8 @@ static inline bool __mptcp_has_initial_subflow(const struct mptcp_sock *msk)
 {
 	struct sock *ssk = READ_ONCE(msk->first);
 
-	return ssk && inet_sk_state_load(ssk) != TCP_CLOSE;
+	return ssk &&
+	       !((1 << inet_sk_state_load(ssk)) & (TCPF_FIN_WAIT2 | TCPF_CLOSE));
 }
 
 static inline void mptcp_do_fallback(struct sock *ssk)
