725c55d7c72247deadaca821ef467d57fbbec650
Geliang Tang <geliang.tang@suse.com>
2023-11-22 19:31:02 +0800

    selftests: mptcp: sockopt: print every test result
    Only total test results are printed out in mptcp_sockopt.sh:
    
     PASS: all packets had packet mark set
     PASS: SOL_MPTCP getsockopt has expected information
     PASS: TCP_INQ cmsg/ioctl -t tcp
     PASS: TCP_INQ cmsg/ioctl -6 -t tcp
     PASS: TCP_INQ cmsg/ioctl -r tcp
     PASS: TCP_INQ cmsg/ioctl -6 -r tcp
    
    This patch prints more info for every test result in each test
    group:
    
     transfer ipv4                                                [ OK ]
     mark ipv4                                                    [ OK ]
     transfer ipv6                                                [ OK ]
     mark ipv6                                                    [ OK ]
     PASS: all packets had packet mark set
     sockopt v4                                                   [ OK ]
     sockopt v6                                                   [ OK ]
     PASS: SOL_MPTCP getsockopt has expected information
     TCP_INQ: -t tcp                                              [ OK ]
     PASS: TCP_INQ cmsg/ioctl -t tcp
     TCP_INQ: -6 -t tcp                                           [ OK ]
     PASS: TCP_INQ cmsg/ioctl -6 -t tcp
     TCP_INQ: -r tcp                                              [ OK ]
     PASS: TCP_INQ cmsg/ioctl -r tcp
     TCP_INQ: -6 -r tcp                                           [ OK ]
     PASS: TCP_INQ cmsg/ioctl -6 -r tcp
     TCP_INQ: -r tcp -t tcp                                       [ OK ]
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Message-Id: <969ecf72809657fea20fc896975856920e7dc333.1700652422.git.geliang.tang@suse.com>

--- tools/testing/selftests/net/mptcp/mptcp_sockopt.sh
+++ tools/testing/selftests/net/mptcp/mptcp_sockopt.sh
@@ -173,6 +173,7 @@ do_transfer()
 	wait $spid
 	local rets=$?
 
+	printf "%-25s %35s" "transfer ${ip}" " "
 	if [ ${rets} -ne 0 ] || [ ${retc} -ne 0 ]; then
 		echo " client exit code $retc, server $rets" 1>&2
 		echo -e "\nnetns ${listener_ns} socket stat for ${port}:" 1>&2
@@ -181,12 +182,15 @@ do_transfer()
 		echo -e "\nnetns ${connector_ns} socket stat for ${port}:" 1>&2
 		ip netns exec ${connector_ns} ss -Menita 1>&2 -o "dport = :$port"
 
+		mptcp_lib_print_err "[ FAIL ]"
 		mptcp_lib_result_fail "transfer ${ip}"
 
 		ret=1
 		return 1
 	fi
+	mptcp_lib_print_ok "[ OK ]"
 
+	printf "%-25s %35s" "mark ${ip}" " "
 	if [ $local_addr = "::" ];then
 		check_mark $listener_ns 6 || retc=1
 		check_mark $connector_ns 6 || retc=1
@@ -202,8 +206,10 @@ do_transfer()
 	mptcp_lib_result_code "${rets}" "transfer ${ip}"
 
 	if [ $retc -eq 0 ] && [ $rets -eq 0 ];then
+		mptcp_lib_print_ok "[ OK ]"
 		return 0
 	fi
+	mptcp_lib_print_err "[ FAIL ]"
 
 	return 1
 }
@@ -232,23 +238,27 @@ do_mptcp_sockopt_tests()
 	ip netns exec "$ns3" ./mptcp_sockopt
 	lret=$?
 
+	printf "%-25s %35s" "sockopt v4" " "
 	if [ $lret -ne 0 ]; then
 		echo "FAIL: SOL_MPTCP getsockopt" 1>&2
 		mptcp_lib_result_fail "sockopt v4"
 		ret=$lret
 		return
 	fi
+	mptcp_lib_print_ok "[ OK ]"
 	mptcp_lib_result_pass "sockopt v4"
 
 	ip netns exec "$ns3" ./mptcp_sockopt -6
 	lret=$?
 
+	printf "%-25s %35s" "sockopt v6" " "
 	if [ $lret -ne 0 ]; then
 		echo "FAIL: SOL_MPTCP getsockopt (ipv6)" 1>&2
 		mptcp_lib_result_fail "sockopt v6"
 		ret=$lret
 		return
 	fi
+	mptcp_lib_print_ok "[ OK ]"
 	mptcp_lib_result_pass "sockopt v6"
 }
 
@@ -271,6 +281,7 @@ run_tests()
 
 do_tcpinq_test()
 {
+	printf "%-25s %35s" "TCP_INQ: $*" " "
 	ip netns exec "$ns3" ./mptcp_inq "$@"
 	local lret=$?
 	if [ $lret -ne 0 ];then
@@ -279,6 +290,7 @@ do_tcpinq_test()
 		mptcp_lib_result_fail "TCP_INQ: $*"
 		return $lret
 	fi
+	mptcp_lib_print_ok "[ OK ]"
 
 	echo "PASS: TCP_INQ cmsg/ioctl $@"
 	mptcp_lib_result_pass "TCP_INQ: $*"
