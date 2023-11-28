f246cbe887b47ed3f5faeb8fddac8e88fda7f4ab
Geliang Tang <geliang.tang@suse.com>
2023-11-28 22:14:05 +0800

    mptcp: add ftrace callback of tcp_set_state
    This patch adds a new function mptcp_check_state(), in it if switch from
    or to ESTABLISH state, increment or decrement the newly added counter
    MPTCP_MIB_CURRESTAB.
    
    Instead of invoking mptcp_check_state() in tcp_set_state() directly, here
    add a new file trace.c, in it use ftrace to hook a callback function to
    tcp_set_state(), named mptcp_state_callback(). mptcp_check_state() is
    invoked in the callback function.
    
    Closes: https://github.com/multipath-tcp/mptcp_net-next/issues/460
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Message-Id: <6f4baef54a7631cdc1460138bc7e52f2c432c69b.1701180777.git.geliang.tang@suse.com>

---
+++ net/mptcp/trace.c
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Multipath TCP
+ *
+ * Copyright (c) 2023, SUSE.
+ */
+
+#define pr_fmt(fmt) "MPTCP: " fmt
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/ftrace.h>
+#include <net/sock.h>
+#include <net/mptcp.h>
+#include "protocol.h"
+#include "mib.h"
+
+static void mptcp_check_state(struct sock *sk, int oldstate, int state)
+{
+	switch (state) {
+	case TCP_ESTABLISHED:
+		if (oldstate != TCP_ESTABLISHED)
+			MPTCP_INC_STATS(sock_net(sk), MPTCP_MIB_CURRESTAB);
+		break;
+
+	default:
+		if (oldstate == TCP_ESTABLISHED)
+			MPTCP_DEC_STATS(sock_net(sk), MPTCP_MIB_CURRESTAB);
+	}
+}
+
+static void notrace mptcp_state_callback(unsigned long ip,
+					 unsigned long parent_ip,
+					 struct ftrace_ops *op,
+					 struct ftrace_regs *fregs)
+{
+	struct pt_regs *regs;
+	int oldstate, state;
+	struct sock *sk;
+
+	regs = ftrace_get_regs(fregs);
+	if (!regs)
+		return;
+
+	sk = (struct sock *)regs_get_kernel_argument(regs, 0);
+	if (!sk)
+		return;
+
+	oldstate = sk->sk_state;
+	state = regs_get_kernel_argument(regs, 1);
+
+	if (sk_is_mptcp(sk))
+		mptcp_check_state(sk, oldstate, state);
+}
+
+static struct ftrace_ops mptcp_state_ops __read_mostly = {
+	.func		= mptcp_state_callback,
+	.flags		= FTRACE_OPS_FL_SAVE_REGS,
+};
+
+static __init int mptcp_ftrace_init(void)
+{
+	int ret;
+
+	ret = ftrace_set_filter(&mptcp_state_ops, "tcp_set_state",
+				strlen("tcp_set_state"), 0);
+	return ret ?: register_ftrace_function(&mptcp_state_ops);
+}
+late_initcall(mptcp_ftrace_init);
