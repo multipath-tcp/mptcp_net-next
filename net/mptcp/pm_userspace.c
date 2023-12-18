3671964ff95eeade198ff2905d816b54ee52397c
Geliang Tang <geliang.tang@linux.dev>
2023-12-18 17:23:41 +0800

    mptcp: implement mptcp_userspace_pm_get_addr
    This patch implements mptcp_userspace_pm_get_addr() to get an address
    from userspace pm address list according the given 'token' and 'id'.
    Use nla_get_u32() to get the u32 value of 'token', then pass it to
    mptcp_token_get_sock() to get the msk. Pass 'msk' and 'id' to the helper
    mptcp_userspace_pm_lookup_addr_by_id() to get the address entry. Put
    this entry to userspace using mptcp_pm_nl_put_entry_info().
    
    Signed-off-by: Geliang Tang <geliang.tang@linux.dev>
    Message-Id: <e831379dfb40da04591aa4574c661e505eb03701.1702891194.git.geliang.tang@linux.dev>

--- net/mptcp/pm_userspace.c
+++ net/mptcp/pm_userspace.c
@@ -596,3 +596,26 @@ int mptcp_userspace_pm_dump_addr(struct sk_buff *msg,
 	cb->args[0] = id;
 	return msg->len;
 }
+
+int mptcp_userspace_pm_get_addr(struct net *net, struct genl_info *info,
+				u32 token, u8 id)
+{
+	struct mptcp_pm_addr_entry *entry;
+	struct mptcp_sock *msk;
+	int ret = -EINVAL;
+
+	msk = mptcp_token_get_sock(net, token);
+	if (!msk)
+		return ret;
+
+	if (!mptcp_pm_is_userspace(msk))
+		return ret;
+
+	entry = mptcp_userspace_pm_lookup_addr_by_id(msk, id);
+	if (!entry) {
+		GENL_SET_ERR_MSG(info, "address not found");
+		return ret;
+	}
+
+	return mptcp_pm_nl_put_entry_info(info, entry);
+}
--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -1032,6 +1032,8 @@ int mptcp_pm_nl_get_local_id(struct mptcp_sock *msk, struct mptcp_addr_info *skc
 int mptcp_userspace_pm_get_local_id(struct mptcp_sock *msk, struct mptcp_addr_info *skc);
 int mptcp_userspace_pm_dump_addr(struct sk_buff *msg,
 				 struct netlink_callback *cb);
+int mptcp_userspace_pm_get_addr(struct net *net, struct genl_info *info,
+				u32 token, u8 id);
 
 void __init mptcp_pm_nl_init(void);
 void mptcp_pm_nl_work(struct mptcp_sock *msk);
