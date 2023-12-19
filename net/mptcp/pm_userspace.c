c56ca252ee81802548ddda7e536b907ad43f9c00
Geliang Tang <geliang.tang@linux.dev>
2023-12-19 16:19:13 +0800

    mptcp: implement mptcp_userspace_pm_dump_addr
    This patch implements mptcp_userspace_pm_dump_addr() to dump addresses
    from userspace pm address list. Use mptcp_token_get_sock() to get the
    msk from the given token, if userspace PM is enabled in it, traverse
    each address entry in address list, put every entry to userspace using
    mptcp_pm_nl_put_entry_msg().
    
    Signed-off-by: Geliang Tang <geliang.tang@linux.dev>
    Message-Id: <66f2d1ffbdece95a755594d7d2c209b771424d11.1702973751.git.geliang.tang@linux.dev>

--- net/mptcp/pm_userspace.c
+++ net/mptcp/pm_userspace.c
@@ -550,3 +550,54 @@ set_flags_err:
 	sock_put(sk);
 	return ret;
 }
+
+int mptcp_userspace_pm_dump_addr(struct sk_buff *msg,
+				 struct netlink_callback *cb)
+{
+	const struct genl_info *info = genl_info_dump(cb);
+	struct net *net = sock_net(msg->sk);
+	struct mptcp_pm_addr_entry *entry;
+	struct mptcp_sock *msk;
+	int id = cb->args[0];
+	int ret = -EINVAL;
+	struct sock *sk;
+	void *hdr;
+
+	if (!info->attrs[MPTCP_PM_ATTR_TOKEN])
+		return ret;
+
+	msk = mptcp_token_get_sock(net, nla_get_u32(info->attrs[MPTCP_PM_ATTR_TOKEN]));
+	if (!msk)
+		return ret;
+
+	if (!mptcp_pm_is_userspace(msk))
+		return ret;
+
+	sk = (struct sock *)msk;
+
+	lock_sock(sk);
+	spin_lock_bh(&msk->pm.lock);
+	list_for_each_entry(entry, &msk->pm.userspace_pm_local_addr_list, list) {
+		if (entry->addr.id <= id)
+			continue;
+
+		hdr = genlmsg_put(msg, NETLINK_CB(cb->skb).portid,
+				  cb->nlh->nlmsg_seq, &mptcp_genl_family,
+				  NLM_F_MULTI, MPTCP_PM_CMD_GET_ADDR);
+		if (!hdr)
+			break;
+
+		if (mptcp_nl_fill_addr(msg, entry) < 0) {
+			genlmsg_cancel(msg, hdr);
+			break;
+		}
+
+		id = entry->addr.id;
+		genlmsg_end(msg, hdr);
+	}
+	spin_unlock_bh(&msk->pm.lock);
+	release_sock(sk);
+
+	cb->args[0] = id;
+	return msg->len;
+}
