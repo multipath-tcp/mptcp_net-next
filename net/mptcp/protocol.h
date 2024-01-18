105ab6e1a2585f5981de74c8054774024a211840
Geliang Tang <tanggeliang@kylinos.cn>
2024-01-18 14:11:47 +0800

    mptcp: implement mptcp_userspace_pm_dump_addr
    This patch implements mptcp_userspace_pm_dump_addr() to dump addresses
    from userspace pm address list. Use mptcp_token_get_sock() to get the
    msk from the given token, if userspace PM is enabled in it, traverse
    each address entry in address list, put every entry to userspace using
    mptcp_pm_nl_put_entry_msg().
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <603ed98d214c9038bf0091dec4de50d7441a4d88.1705558030.git.tanggeliang@kylinos.cn>

--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -1032,6 +1032,8 @@ bool mptcp_pm_rm_addr_signal(struct mptcp_sock *msk, unsigned int remaining,
 int mptcp_pm_get_local_id(struct mptcp_sock *msk, struct sock_common *skc);
 int mptcp_pm_nl_get_local_id(struct mptcp_sock *msk, struct mptcp_addr_info *skc);
 int mptcp_userspace_pm_get_local_id(struct mptcp_sock *msk, struct mptcp_addr_info *skc);
+int mptcp_userspace_pm_dump_addr(struct sk_buff *msg,
+				 struct netlink_callback *cb);
 
 void __init mptcp_pm_nl_init(void);
 void mptcp_pm_nl_work(struct mptcp_sock *msk);
