ab6eec4814bceb954abc68bbf0763e518513b4b0
Geliang Tang <tanggeliang@kylinos.cn>
2024-02-03 15:51:05 +0800

    bpf, btf: Check btf for register_bpf_struct_ops
    Similar to the handling in the functions __register_btf_kfunc_id_set()
    and register_btf_id_dtor_kfuncs(), this patch uses the newly added
    helper register_check_missing_btf() and IS_ERR() to check the return
    value of btf_get_module_btf().
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <e86da7508219125aed45d869cc02161acee418aa.1706946547.git.tanggeliang@kylinos.cn>

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -8805,7 +8805,9 @@ int __register_bpf_struct_ops(struct bpf_struct_ops *st_ops)
 
 	btf = btf_get_module_btf(st_ops->owner);
 	if (!btf)
-		return -EINVAL;
+		return register_check_missing_btf(st_ops->owner, "structs");
+	if (IS_ERR(btf))
+		return PTR_ERR(btf);
 
 	log = kzalloc(sizeof(*log), GFP_KERNEL | __GFP_NOWARN);
 	if (!log) {
