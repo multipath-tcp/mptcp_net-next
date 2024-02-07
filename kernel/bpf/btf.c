175123dba69ce0a0b4a3580089a1035c6263d560
Geliang Tang <tanggeliang@kylinos.cn>
2024-02-07 22:07:54 +0800

    bpf, btf: Fix error checks for btf_get_module_btf
    To let the modules loaded, commit 3de4d22cc9ac ("bpf, btf: Warn but
    return no error for NULL btf from __register_btf_kfunc_id_set()")
    changes the return value of __register_btf_kfunc_id_set() from -ENOENT
    to 0 when btf is NULL.
    
    A better way to do this is to enable CONFIG_MODULE_ALLOW_BTF_MISMATCH.
    
    An error code -ENOENT should indeed be returned when kernel module BTF
    mismatch detected except CONFIG_MODULE_ALLOW_BTF_MISMATCH is enabled in
    __register_btf_kfunc_id_set().
    
    The same in register_btf_id_dtor_kfuncs(), give the modules a chance
    to be loaded when CONFIG_MODULE_ALLOW_BTF_MISMATCH is enabled.
    
    Fixes: 3de4d22cc9ac ("bpf, btf: Warn but return no error for NULL btf from __register_btf_kfunc_id_set()")
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Reported-by: kernel test robot <lkp@intel.com>
    Message-Id: <d79e5dd6b4a189252af696a10df8ce585e9cb46d.1707314646.git.tanggeliang@kylinos.cn>

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -8014,8 +8014,11 @@ static int __register_btf_kfunc_id_set(enum btf_kfunc_hook hook,
 			pr_err("missing vmlinux BTF, cannot register kfuncs\n");
 			return -ENOENT;
 		}
-		if (kset->owner && IS_ENABLED(CONFIG_DEBUG_INFO_BTF_MODULES))
+		if (kset->owner && IS_ENABLED(CONFIG_DEBUG_INFO_BTF_MODULES) &&
+		    !IS_ENABLED(CONFIG_MODULE_ALLOW_BTF_MISMATCH)) {
 			pr_warn("missing module BTF, cannot register kfuncs\n");
+			return -ENOENT;
+		}
 		return 0;
 	}
 	if (IS_ERR(btf))
@@ -8122,7 +8125,8 @@ int register_btf_id_dtor_kfuncs(const struct btf_id_dtor_kfunc *dtors, u32 add_c
 			pr_err("missing vmlinux BTF, cannot register dtor kfuncs\n");
 			return -ENOENT;
 		}
-		if (owner && IS_ENABLED(CONFIG_DEBUG_INFO_BTF_MODULES)) {
+		if (owner && IS_ENABLED(CONFIG_DEBUG_INFO_BTF_MODULES) &&
+		    !IS_ENABLED(CONFIG_MODULE_ALLOW_BTF_MISMATCH)) {
 			pr_err("missing module BTF, cannot register dtor kfuncs\n");
 			return -ENOENT;
 		}
