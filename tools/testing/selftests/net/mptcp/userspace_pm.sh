a6f2ed0f92515d9c76fa6f76fd88c4a1473e2b7d
Geliang Tang <tanggeliang@kylinos.cn>
2024-02-25 21:03:58 +0800

    selftests: mptcp: extract mptcp_lib_check_expected
    Extract the main part of check_expected() in userspace_pm.sh to a new
    function mptcp_lib_check_expected() in mptcp_lib.sh. It will be used
    in both mptcp_john.sh and userspace_pm.sh.
    
    check_expected_one() is moved into mptcp_lib.sh too as a sub function
    of mptcp_lib_check_expected().
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <d555f375345ded8d39ce48077f46073529654dd2.1708866087.git.tanggeliang@kylinos.cn>

--- tools/testing/selftests/net/mptcp/userspace_pm.sh
+++ tools/testing/selftests/net/mptcp/userspace_pm.sh
@@ -5,7 +5,7 @@
 # code but we accept it.
 #shellcheck disable=SC2086
 
-# Some variables are used below but indirectly, see check_expected_one()
+# Some variables are used below but indirectly
 #shellcheck disable=SC2034
 
 . "$(dirname "${0}")/mptcp_lib.sh"
@@ -233,46 +233,15 @@ make_connection()
 	fi
 }
 
-# $1: var name ; $2: prev ret
-check_expected_one()
-{
-	local var="${1}"
-	local exp="e_${var}"
-	local prev_ret="${2}"
-
-	if [ "${!var}" = "${!exp}" ]
-	then
-		return 0
-	fi
-
-	if [ "${prev_ret}" = "0" ]
-	then
-		return 2
-	fi
-
-	_printf "\tExpected value for '%s': '%s', got '%s'.\n" \
-		"${var}" "${!exp}" "${!var}"
-	return 1
-}
-
 # $@: all var names to check
 check_expected()
 {
 	local rc=0
-	local var
 
-	for var in "${@}"
-	do
-		check_expected_one "${var}" "${rc}" || rc="${?}"
-		if [ "${rc}" -eq 2 ]
-		then
-			break
-		fi
-	done
-
-	if [ ${rc} -eq 0 ]
+	mptcp_lib_check_expected "${@}" || rc="${?}"
+	if [ "${rc}" -eq 0 ]
 	then
-		test_pass
+		mptcp_lib_result_pass "${test_name}"
 		return 0
 	elif [ "${rc}" -eq 2 ]
 	then
