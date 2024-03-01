ae6b207fe119cfdea27f83973cbfc00646a358b4
Geliang Tang <tanggeliang@kylinos.cn>
2024-03-01 14:46:31 +0800

    selftests: mptcp: connect: add dedicated port counter
    This patch adds a new dedicated counter 'PORT' instead of TEST_COUNT to
    increase port numbers in mptcp_connect.sh.
    
    This can avoid outputting discontinuous test counters.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <d5fb21d260eb93fafafe67dbb8886e74fd22aaf1.1709275290.git.tanggeliang@kylinos.cn>

--- tools/testing/selftests/net/mptcp/mptcp_connect.sh
+++ tools/testing/selftests/net/mptcp/mptcp_connect.sh
@@ -33,6 +33,7 @@ do_tcp=0
 checksum=false
 filesize=0
 connect_per_transfer=1
+PORT=0
 
 if [ $tc_loss -eq 100 ];then
 	tc_loss=1%
@@ -314,7 +315,7 @@ do_transfer()
 	local extra_args="$7"
 
 	local port
-	port=$((10000+TEST_COUNT))
+	port=$((10000+PORT++))
 	TEST_COUNT=$((TEST_COUNT+1))
 
 	if [ "$rcvbuf" -gt 0 ]; then
@@ -710,7 +711,7 @@ EOF
 
 	echo "INFO: test $msg"
 
-	TEST_COUNT=10000
+	PORT=10000
 	local extra_args="-o TRANSPARENT"
 	do_transfer ${listener_ns} ${connector_ns} MPTCP MPTCP \
 		    ${connect_addr} ${local_addr} "${extra_args}"
