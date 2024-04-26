17729d5ea5dcc34356d42bd56cfff249e308ebbe
Geliang Tang <tanggeliang@kylinos.cn>
2024-04-26 05:48:57 +0000

    selftests/bpf: Add bpf_first scheduler & test
    This patch implements the simplest MPTCP scheduler, named bpf_first,
    which always picks the first subflow to send data. It's a sample of
    MPTCP BPF scheduler implementations.
    
    This patch defines MPTCP_SCHED_TEST macro, a template for all scheduler
    tests. Every scheduler is identified by argument name, and use sysctl
    to set net.mptcp.scheduler as "bpf_name" to use this sched. Add two
    veth net devices to simulate the multiple addresses case. Use 'ip mptcp
    endpoint' command to add the new endpoint ADDR2 to PM netlink. Arguments
    addr1/add2 means whether the data has been sent on the first/second
    subflow or not. Send data and check bytes_sent of 'ss' output after it
    using send_data_and_verify().
    
    Using MPTCP_SCHED_TEST macro to add a new test for this bpf_first
    scheduler, the arguments "1 0" means data has been only sent on the
    first subflow ADDR1. Run this test by RUN_MPTCP_TEST macro.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Acked-by: Paolo Abeni <pabeni@redhat.com>
    Reviewed-by: Mat Martineau <martineau@kernel.org>
    Reviewed-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- tools/testing/selftests/bpf/prog_tests/mptcp.c
+++ tools/testing/selftests/bpf/prog_tests/mptcp.c
@@ -9,6 +9,7 @@
 #include "network_helpers.h"
 #include "mptcp_sock.skel.h"
 #include "mptcpify.skel.h"
+#include "mptcp_bpf_first.skel.h"
 
 #define NS_TEST "mptcp_ns"
 #define WITH_DATA	true
@@ -34,6 +35,7 @@
 #ifndef TCP_CA_NAME_MAX
 #define TCP_CA_NAME_MAX	16
 #endif
+#define MPTCP_SCHED_NAME_MAX	16
 
 struct __mptcp_info {
 	__u8	mptcpi_subflows;
@@ -495,6 +497,49 @@ fail:
 	cleanup_netns(nstoken);
 }
 
+static void test_bpf_sched(struct bpf_object *obj, char *sched,
+			   bool addr1, bool addr2)
+{
+	char bpf_sched[MPTCP_SCHED_NAME_MAX] = "bpf_";
+	struct nstoken *nstoken;
+	struct bpf_link *link;
+	struct bpf_map *map;
+
+	if (!ASSERT_LT(strlen(bpf_sched) + strlen(sched),
+		       MPTCP_SCHED_NAME_MAX, "Scheduler name too long"))
+		return;
+
+	map = bpf_object__find_map_by_name(obj, sched);
+	link = bpf_map__attach_struct_ops(map);
+	if (CHECK(!link, sched, "attach_struct_ops: %d\n", errno))
+		return;
+
+	nstoken = sched_init("subflow", strcat(bpf_sched, sched));
+	if (CHECK(!nstoken, sched, "sched_init: %d\n", errno))
+		goto fail;
+
+	send_data_and_verify(sched, addr1, addr2);
+
+fail:
+	cleanup_netns(nstoken);
+	bpf_link__destroy(link);
+}
+
+#define MPTCP_SCHED_TEST(sched, addr1, addr2)			\
+static void test_##sched(void)					\
+{								\
+	struct mptcp_bpf_##sched *skel;				\
+								\
+	skel = mptcp_bpf_##sched##__open_and_load();		\
+	if (!ASSERT_OK_PTR(skel, "open_and_load:" #sched))	\
+		return;						\
+								\
+	test_bpf_sched(skel->obj, #sched, addr1, addr2);	\
+	mptcp_bpf_##sched##__destroy(skel);			\
+}
+
+MPTCP_SCHED_TEST(first, WITH_DATA, WITHOUT_DATA);
+
 #define RUN_MPTCP_TEST(suffix)					\
 do {								\
 	if (test__start_subtest(#suffix))			\
@@ -506,4 +551,5 @@ void test_mptcp(void)
 	RUN_MPTCP_TEST(base);
 	RUN_MPTCP_TEST(mptcpify);
 	RUN_MPTCP_TEST(default);
+	RUN_MPTCP_TEST(first);
 }
