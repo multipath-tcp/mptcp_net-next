e40e492829d823947e762e8aa981a8e2f7ded549
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2024-05-07 11:06:23 +0200

    utils: end: remove leading space
    So we can double click on the line we need.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- utils/end-conflict.sh
+++ utils/end-conflict.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 git commit -s --no-edit
-printf " - %s: conflict in %s\n" \
+printf "%s%s: conflict in %s\n" "- " \
 	"$(git rev-parse --short HEAD)" \
 	"$(git rev-parse --abbrev-ref HEAD)"
--- utils/end-squash.sh
+++ utils/end-squash.sh
@@ -4,7 +4,7 @@ git am --continue
 SHA="$(git rev-parse --short HEAD)"
 COMMIT="$(git rev-parse HEAD)"
 TITLE="$(git log -1 --pretty="format:%s")"
-printf " - %s: \"squashed\" (with conflicts) in \"%s\"\n" \
+printf "%s%s: \"squashed\" (with conflicts) in \"%s\"\n" "- " \
 	"${SHA}" \
 	"$(./.title.sh)"
 ./.signed-off.sh
