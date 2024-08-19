679f154da316c4458e229129713edcfdb7e272fd
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2024-08-19 18:55:17 +0200

    utils: checkpatch: check b4 and prefix
    For the moment, 'b4 prep --check' is launching checkpatch. We cannot
    change the command yet, the bug should be applied "soon".
    
    A warning for the prefix has been added, as often I forgot to change it.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- utils/checkpatch-b4.sh
+++ utils/checkpatch-b4.sh
@@ -6,4 +6,18 @@ if [ "${branch:0:3}" != "b4/" ]; then
 	exit 1
 fi
 
-./.checkpatch.sh --git "$(b4 prep --show-info start-commit).."
+rc=0
+./.checkpatch.sh --git "$(b4 prep --show-info start-commit).." || rc=$?
+
+echo
+b4 prep --check
+
+prefixes="$(b4 prep --show-info prefixes)"
+echo
+echo "Prefixes: ${prefixes}"
+if [[ "${prefixes}" == *"-next"* ]] &&
+   git log --format="%b" "$(b4 prep --show-info series-range)" | grep -q "^Fixes: "; then
+	echo -e "\n\tWARNING: Series is for ${prefixes}, but there are patches with 'Fixes' tags\n"
+	exit 1
+fi
+exit ${rc}
