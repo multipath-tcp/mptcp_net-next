e69c1c7d48b12896da37fbfb44d433b5565fda29
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2024-08-21 13:28:32 +0200

    selftests: mptcp: reset the last TS before the first test
    Just to slightly improve the precision of the duration of the first
    test.
    
    In mptcp_join.sh, the last append_prev_results is now done as soon as
    the last test is over: this will add the last result in the list, and
    get a more precise time for this last test.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Message-Id: <20240821-mptcp-ksft-time-v1-3-c4ea94f41244@kernel.org>

--- tools/testing/selftests/net/mptcp/mptcp_join.sh
+++ tools/testing/selftests/net/mptcp/mptcp_join.sh
@@ -3868,9 +3868,11 @@ if [ ${#tests[@]} -eq 0 ]; then
 	tests=("${all_tests_names[@]}")
 fi
 
+mptcp_lib_subtests_last_ts_reset
 for subtests in "${tests[@]}"; do
 	"${subtests}"
 done
+append_prev_results
 
 if [ ${ret} -ne 0 ]; then
 	echo
@@ -3881,7 +3883,6 @@ if [ ${ret} -ne 0 ]; then
 	echo
 fi
 
-append_prev_results
 mptcp_lib_result_print_all_tap
 
 exit $ret
