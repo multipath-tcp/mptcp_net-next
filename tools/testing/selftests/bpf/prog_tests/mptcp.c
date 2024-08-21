3089f4d654fcb1a84f8d1d282f0796254694f41d
Geliang Tang <tanggeliang@kylinos.cn>
2024-08-21 15:50:05 +0800

    selftests/bpf: Add getsockopt to inspect mptcp subflow
    This patch adds a "cgroup/getsockopt" way to inspect the subflows of a
    mptcp socket.
    
    mptcp_for_each_stubflow() and other helpers related to list_dentry are
    added into progs/mptcp_bpf.h.
    
    Add an extra "cgroup/getsockopt" prog to walk the msk->conn_list and use
    bpf_core_cast to cast a pointer to tcp_sock for readonly. It will allow
    to inspect all the fields in a tcp_sock.
    
    Suggested-by: Martin KaFai Lau <martin.lau@kernel.org>
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <b408bd6c62d50c91e02b995c1a09aae9d2fc619e.1724226530.git.tanggeliang@kylinos.cn>

--- tools/testing/selftests/bpf/prog_tests/mptcp.c
+++ tools/testing/selftests/bpf/prog_tests/mptcp.c
@@ -383,6 +383,7 @@ static void run_subflow(char *new)
 {
 	int server_fd, client_fd, err;
 	char cc[TCP_CA_NAME_MAX];
+	unsigned int mark;
 	socklen_t len;
 
 	server_fd = start_mptcp_server(AF_INET, ADDR_1, PORT_1, 0);
@@ -405,6 +406,15 @@ static void run_subflow(char *new)
 	ASSERT_OK(ss_search(ADDR_1, new), "ss_search new cc");
 	ASSERT_OK(ss_search(ADDR_2, cc), "ss_search default cc");
 
+	len = sizeof(mark);
+	err = getsockopt(client_fd, SOL_SOCKET, SO_MARK, &mark, &len);
+	if (!ASSERT_OK(err, "getsockopt(client_fd, SO_MARK)"))
+		goto close_client;
+
+	len = sizeof(cc);
+	err = getsockopt(client_fd, SOL_TCP, TCP_CONGESTION, cc, &len);
+	ASSERT_OK(err, "getsockopt(client_fd, TCP_CONGESTION)");
+
 close_client:
 	close(client_fd);
 close_server:
@@ -416,6 +426,7 @@ static void test_subflow(void)
 	int cgroup_fd, prog_fd, err;
 	struct mptcp_subflow *skel;
 	struct nstoken *nstoken;
+	struct bpf_link *link;
 
 	cgroup_fd = test__join_cgroup("/mptcp_subflow");
 	if (!ASSERT_GE(cgroup_fd, 0, "join_cgroup: mptcp_subflow"))
@@ -441,6 +452,11 @@ static void test_subflow(void)
 	if (endpoint_init("subflow") < 0)
 		goto close_netns;
 
+	link = bpf_program__attach_cgroup(skel->progs._getsockopt_subflow,
+					  cgroup_fd);
+	if (!ASSERT_OK_PTR(link, "getsockopt prog"))
+		goto close_netns;
+
 	run_subflow(skel->data->cc);
 
 close_netns:
@@ -449,6 +465,7 @@ skel_destroy:
 	mptcp_subflow__destroy(skel);
 close_cgroup:
 	close(cgroup_fd);
+	bpf_link__destroy(link);
 }
 
 static struct nstoken *sched_init(char *flags, char *sched)
