aeae1f65f80a001dfb059b1c5027906db287c03d
Geliang Tang <tanggeliang@kylinos.cn>
2024-09-10 13:37:25 +0800

    bpf: Register mptcp tracing kfunc set
    Since the kfuncs mptcp_subflow_active() and mptcp_subflow_set_scheduled()
    are invoked in the mptcp_subflow bpf_iter test in a ftrace hook for
    mptcp_sched_get_send(), it's necessary to register them into a
    BPF_PROG_TYPE_TRACING type kfunc set together with the bpf_iter
    mptcp_subflow helpers bpf_iter_mptcp_subflow_new()/_next()/_destroy().
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <868a19c46b7ec5592d1173e52470bb62c9a0344d.1725946276.git.tanggeliang@kylinos.cn>

--- net/mptcp/bpf.c
+++ net/mptcp/bpf.c
@@ -260,9 +260,12 @@ __bpf_kfunc bool bpf_mptcp_subflow_queues_empty(struct sock *sk)
 __bpf_kfunc_end_defs();
 
 BTF_KFUNCS_START(bpf_mptcp_sched_kfunc_ids)
+BTF_ID_FLAGS(func, bpf_iter_mptcp_subflow_new)
+BTF_ID_FLAGS(func, bpf_iter_mptcp_subflow_next)
+BTF_ID_FLAGS(func, bpf_iter_mptcp_subflow_destroy)
+BTF_ID_FLAGS(func, mptcp_subflow_active)
 BTF_ID_FLAGS(func, mptcp_subflow_set_scheduled)
 BTF_ID_FLAGS(func, bpf_mptcp_subflow_ctx_by_pos)
-BTF_ID_FLAGS(func, mptcp_subflow_active)
 BTF_ID_FLAGS(func, mptcp_set_timeout)
 BTF_ID_FLAGS(func, mptcp_wnd_end)
 BTF_ID_FLAGS(func, tcp_stream_memory_free)
@@ -280,6 +283,8 @@ static int __init bpf_mptcp_kfunc_init(void)
 	int ret;
 
 	ret = register_btf_fmodret_id_set(&bpf_mptcp_fmodret_set);
+	ret = ret ?: register_btf_kfunc_id_set(BPF_PROG_TYPE_TRACING,
+					       &bpf_mptcp_sched_kfunc_set);
 	ret = ret ?: register_btf_kfunc_id_set(BPF_PROG_TYPE_STRUCT_OPS,
 					       &bpf_mptcp_sched_kfunc_set);
 #ifdef CONFIG_BPF_JIT
