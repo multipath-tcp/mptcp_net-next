1368e2e9aa5fc0e8a46c68f0b594cf8b60c0f4c5
Geliang Tang <tanggeliang@kylinos.cn>
2024-10-04 20:50:49 +0800

    bpf: Add mptcp_sock acquire and release helpers
    KF_TRUSTED_ARGS flag is used for bpf_iter_mptcp_subflow_new, it indicates
    that the all pointer arguments are valid. It's necessary to add a
    KF_ACQUIRE helper to get valid "msk".
    
    This patch adds bpf_mptcp_sock_acquire() and bpf_mptcp_sock_release()
    helpers for this. Increase sk->sk_refcnt in _acquire() and decrease it
    in _release(). Register them with KF_ACQUIRE flag and KF_RELEASE flag.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <93161d30c383af13faece05d6e47fd75ff61a697.1728046021.git.tanggeliang@kylinos.cn>

--- net/mptcp/bpf.c
+++ net/mptcp/bpf.c
@@ -260,6 +260,22 @@ __bpf_kfunc void bpf_iter_mptcp_subflow_destroy(struct bpf_iter_mptcp_subflow *i
 {
 }
 
+__bpf_kfunc struct mptcp_sock *bpf_mptcp_sock_acquire(struct mptcp_sock *msk)
+{
+	struct sock *sk = (struct sock *)msk;
+
+	if (sk && refcount_inc_not_zero(&sk->sk_refcnt))
+		return msk;
+	return NULL;
+}
+
+__bpf_kfunc void bpf_mptcp_sock_release(struct mptcp_sock *msk)
+{
+	struct sock *sk = (struct sock *)msk;
+
+	WARN_ON_ONCE(!sk || !refcount_dec_not_one(&sk->sk_refcnt));
+}
+
 __bpf_kfunc struct mptcp_subflow_context *
 bpf_mptcp_subflow_ctx_by_pos(const struct mptcp_sched_data *data, unsigned int pos)
 {
@@ -284,6 +300,8 @@ BTF_ID_FLAGS(func, mptcp_subflow_active)
 BTF_ID_FLAGS(func, bpf_iter_mptcp_subflow_new, KF_ITER_NEW | KF_TRUSTED_ARGS)
 BTF_ID_FLAGS(func, bpf_iter_mptcp_subflow_next, KF_ITER_NEXT | KF_RET_NULL)
 BTF_ID_FLAGS(func, bpf_iter_mptcp_subflow_destroy, KF_ITER_DESTROY)
+BTF_ID_FLAGS(func, bpf_mptcp_sock_acquire, KF_ACQUIRE | KF_RET_NULL)
+BTF_ID_FLAGS(func, bpf_mptcp_sock_release, KF_RELEASE)
 BTF_KFUNCS_END(bpf_mptcp_common_kfunc_ids)
 
 static const struct btf_kfunc_id_set bpf_mptcp_common_kfunc_set = {
