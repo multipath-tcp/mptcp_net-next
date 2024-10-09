bcc8f295045e80d1096e63d96976197d222831e4
Geliang Tang <tanggeliang@kylinos.cn>
2024-10-09 17:45:46 +0800

    selftests/bpf: Add mptcp_subflow bpf_iter test prog
    This patch adds a ftrace hook for mptcp_sched_get_send() to test the newly
    added mptcp_subflow bpf_iter. This test simulates a typical mptcp packet
    scheduler, which selects a subflow from multiple subflows of an mptcp
    socket to send data.
    
    Export mptcp_subflow helpers bpf_iter_mptcp_subflow_new/_next/_destroy,
    bpf_mptcp_sock_acquire/_release and other helpers into bpf_experimental.h.
    
    Use _acquire() to acquire the msk, then use bpf_for_each(mptcp_subflow) to
    walk the subflow list of this msk. Invoke kfuncs mptcp_subflow_active() and
    bpf_mptcp_subflow_tcp_sock() in the loop to pick a subsocket. Finally use
    bpf_mptcp_subflow_ctx() to get the subflow context of this subsocket and
    use mptcp_subflow_set_scheduled() to set it as being scheduled.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <dffa6284797ec2e3e8c8a1f6891f012bd1efdc20.1728466623.git.tanggeliang@kylinos.cn>

--- tools/testing/selftests/bpf/progs/mptcp_bpf.h
+++ tools/testing/selftests/bpf/progs/mptcp_bpf.h
@@ -43,8 +43,17 @@ mptcp_subflow_tcp_sock(const struct mptcp_subflow_context *subflow)
 }
 
 /* ksym */
+extern bool mptcp_subflow_active(struct mptcp_subflow_context *subflow) __ksym;
 extern void mptcp_subflow_set_scheduled(struct mptcp_subflow_context *subflow,
 					bool scheduled) __ksym;
+
+extern struct mptcp_sock *bpf_mptcp_sock_acquire(struct mptcp_sock *msk) __ksym;
+extern void bpf_mptcp_sock_release(struct mptcp_sock *msk) __ksym;
+
+extern struct mptcp_subflow_context *
+bpf_mptcp_subflow_ctx(const struct sock *sk) __ksym;
+extern struct sock *
+bpf_mptcp_subflow_tcp_sock(const struct mptcp_subflow_context *subflow) __ksym;
 
 extern struct mptcp_subflow_context *
 bpf_mptcp_subflow_ctx_by_pos(const struct mptcp_sched_data *data, unsigned int pos) __ksym;
