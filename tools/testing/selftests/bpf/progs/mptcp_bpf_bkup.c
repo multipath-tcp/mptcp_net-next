8b3fbeec942ad2c2913ba4bbb4ff9b199c269131
Geliang Tang <tanggeliang@kylinos.cn>
2024-10-11 17:27:17 +0800

    Squash to "selftests/bpf: Add bpf_bkup scheduler & test"
    Use the newly added bpf_for_each() helper to walk the conn_list.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <035577c49107a9a76c8d1f509743c22c0d33514d.1728638310.git.tanggeliang@kylinos.cn>

--- tools/testing/selftests/bpf/progs/mptcp_bpf_bkup.c
+++ tools/testing/selftests/bpf/progs/mptcp_bpf_bkup.c
@@ -20,26 +20,16 @@ SEC("struct_ops")
 int BPF_PROG(bpf_bkup_get_subflow, struct mptcp_sock *msk,
 	     struct mptcp_sched_data *data)
 {
-	int nr = -1;
+	struct mptcp_subflow_context *subflow;
 
-	for (int i = 0; i < data->subflows && i < MPTCP_SUBFLOWS_MAX; i++) {
-		struct mptcp_subflow_context *subflow;
-
-		subflow = bpf_mptcp_subflow_ctx_by_pos(data, i);
-		if (!subflow)
-			break;
-
+	bpf_for_each(mptcp_subflow, subflow, msk) {
 		if (!BPF_CORE_READ_BITFIELD_PROBED(subflow, backup) ||
 		    !BPF_CORE_READ_BITFIELD_PROBED(subflow, request_bkup)) {
-			nr = i;
+			mptcp_subflow_set_scheduled(subflow, true);
 			break;
 		}
 	}
 
-	if (nr != -1) {
-		mptcp_subflow_set_scheduled(bpf_mptcp_subflow_ctx_by_pos(data, nr), true);
-		return -1;
-	}
 	return 0;
 }
 
