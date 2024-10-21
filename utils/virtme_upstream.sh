d1fe459a76758888fe3c445b1a44b4f859cdff12
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2024-10-21 12:06:47 +0200

    utils: virtme: ups: split stable/net
    Easier to read.
    
    While at it, No need to check for a version in the prefix, the sublevel
    one will be different than 0.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- utils/virtme_upstream.sh
+++ utils/virtme_upstream.sh
@@ -7,24 +7,27 @@ exit_trap() {
    fi
 }
 
+is_stable() {
+   [ "$(awk '/^SUBLEVEL = / { print $3; exit }' Makefile)" != 0 ]
+}
+
+is_net() {
+   [ "$(b4 prep --show-info prefixes 2>/dev/null)" = "net" ]
+}
+
 if [ -f .virtme-exec-run ]; then
    mv .virtme-exec-run .virtme-exec-run-old
 fi
 
-is_stable() { local prefix sublevel
-   prefix=$(b4 prep --show-info prefixes 2>/dev/null)
-   sublevel=$(awk '/^SUBLEVEL = / { print $3; exit }' Makefile)
-   if [ "${prefix}" = "net" ] || [[ "${prefix}" = [0-9]"."[0-9]* ]] || [ "${sublevel}" != 0 ]; then
-      echo 1
-   else
-      echo 0
-   fi
-}
-
 trap 'exit_trap' EXIT
 
-INPUT_BUILD_SKIP_PERF=1 \
-   VIRTME_PACKETDRILL_STABLE=$(is_stable) \
-   INPUT_SELFTESTS_MPTCP_LIB_OVERRIDE_FLAKY=0 \
-   "./${VIRTME_SH:-.virtme.sh}" "${@:-expect-all}"
+if is_stable || is_net; then
+   PACKETDRILL_STABLE=1
+else
+   PACKETDRILL_STABLE=0
+fi
 
+INPUT_BUILD_SKIP_PERF=1 \
+   VIRTME_PACKETDRILL_STABLE=${PACKETDRILL_STABLE} \
+   INPUT_SELFTESTS_MPTCP_LIB_OVERRIDE_FLAKY=0 \
+   "./${VIRTME_SH:-.virtme.sh}" "${@:-auto-all}"
