eff79403ec6b7506d3edbbae6f1feb281296c8b3
Geliang Tang <tanggeliang@kylinos.cn>
2024-11-21 10:45:41 +0800

    Squash to "selftests/bpf: Add bpf_first scheduler & test"
    Split get_subflow() interface into two: get_send() and get_retrans().
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <e57ac76105430bab4bbc5ac4f62c77e883dd7f29.1732153672.git.tanggeliang@kylinos.cn>

--- tools/testing/selftests/bpf/progs/mptcp_bpf_first.c
+++ tools/testing/selftests/bpf/progs/mptcp_bpf_first.c
@@ -16,18 +16,32 @@ void BPF_PROG(mptcp_sched_first_release, struct mptcp_sock *msk)
 {
 }
 
-SEC("struct_ops")
-int BPF_PROG(bpf_first_get_subflow, struct mptcp_sock *msk,
-	     struct mptcp_sched_data *data)
+static int bpf_first_get_subflow(struct mptcp_sock *msk,
+				 struct mptcp_sched_data *data)
 {
 	mptcp_subflow_set_scheduled(bpf_mptcp_subflow_ctx_by_pos(data, 0), true);
 	return 0;
 }
 
-SEC(".struct_ops")
+SEC("struct_ops")
+int BPF_PROG(bpf_first_get_send, struct mptcp_sock *msk,
+	     struct mptcp_sched_data *data)
+{
+	return bpf_first_get_subflow(msk, data);
+}
+
+SEC("struct_ops")
+int BPF_PROG(bpf_first_get_retrans, struct mptcp_sock *msk,
+	     struct mptcp_sched_data *data)
+{
+	return bpf_first_get_subflow(msk, data);
+}
+
+SEC(".struct_ops.link")
 struct mptcp_sched_ops first = {
 	.init		= (void *)mptcp_sched_first_init,
 	.release	= (void *)mptcp_sched_first_release,
-	.get_subflow	= (void *)bpf_first_get_subflow,
+	.get_send	= (void *)bpf_first_get_send,
+	.get_retrans	= (void *)bpf_first_get_retrans,
 	.name		= "bpf_first",
 };
