a297bc6319aec895837d12c49abfb2f82c22b7ba
Geliang Tang <tanggeliang@kylinos.cn>
2024-11-21 15:10:07 +0800

    Squash to "bpf: Add mptcp_subflow bpf_iter"
    Address Martin's comments in v1:
    
    - bpf_iter_mptcp_subflow_new returns -EINVAL when msk socket lock isn't
      held.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <8e8c6d39d8de8ab2bb4cdd8965c1981a3920bc9e.1732172851.git.tanggeliang@kylinos.cn>

--- net/mptcp/bpf.c
+++ net/mptcp/bpf.c
@@ -252,7 +252,8 @@ bpf_iter_mptcp_subflow_new(struct bpf_iter_mptcp_subflow *it,
 	if (!msk)
 		return -EINVAL;
 
-	msk_owned_by_me(msk);
+	if (!lockdep_sock_is_held((const struct sock *)msk))
+		return -EINVAL;
 
 	kit->pos = &msk->conn_list;
 	return 0;
