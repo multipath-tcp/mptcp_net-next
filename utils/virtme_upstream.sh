eb4858e246994ba28f4e2d73f0be7c9200a8f7ed
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2024-12-02 19:23:46 +0100

    utils: virtme: upstream: set INPUT_VIRTME_EXEC_RUN
    Instead of renaming the file, cleaner.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- utils/virtme_upstream.sh
+++ utils/virtme_upstream.sh
@@ -1,12 +1,5 @@
 #! /bin/bash
 
-exit_trap() {
-   # shellcheck disable=SC2317 # trap
-   if [ -f .virtme-exec-run-old ]; then
-      mv .virtme-exec-run-old .virtme-exec-run
-   fi
-}
-
 is_stable() {
    [ "$(awk '/^SUBLEVEL = / { print $3; exit }' Makefile)" != 0 ]
 }
@@ -14,10 +7,6 @@ is_stable() {
 is_net() {
    [ "$(b4 prep --show-info prefixes 2>/dev/null)" = "net" ]
 }
-
-if [ -f .virtme-exec-run ]; then
-   mv .virtme-exec-run .virtme-exec-run-old
-fi
 
 trap 'exit_trap' EXIT
 
@@ -30,6 +19,7 @@ else
 fi
 
 INPUT_BUILD_SKIP_PERF=1 \
+   INPUT_VIRTME_EXEC_RUN="${INPUT_VIRTME_EXEC_RUN:-/dev/null}" \
    VIRTME_PACKETDRILL_STABLE=${PACKETDRILL_STABLE} \
    INPUT_SELFTESTS_MPTCP_LIB_OVERRIDE_FLAKY=0 \
    INPUT_BUILD_SUFFIX=${SUFFIX} \
