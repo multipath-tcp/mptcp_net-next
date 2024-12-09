858469115d76268eb4f36daca68beae2533e5252
Geliang Tang <tanggeliang@kylinos.cn>
2024-12-09 17:49:22 +0800

    Squash to "selftests/bpf: Add bpf_first scheduler & test"
    Use the newly added bpf_for_each() helper to walk the conn_list.
    
    Drop bpf_mptcp_subflow_ctx_by_pos declaration.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <bf46c44e85605c045436005ce8873108fa1c8867.1733737459.git.tanggeliang@kylinos.cn>

--- tools/testing/selftests/bpf/progs/mptcp_bpf.h
+++ tools/testing/selftests/bpf/progs/mptcp_bpf.h
@@ -55,7 +55,4 @@ bpf_mptcp_subflow_tcp_sock(const struct mptcp_subflow_context *subflow) __ksym;
 extern void mptcp_subflow_set_scheduled(struct mptcp_subflow_context *subflow,
 					bool scheduled) __ksym;
 
-extern struct mptcp_subflow_context *
-bpf_mptcp_subflow_ctx_by_pos(const struct mptcp_sched_data *data, unsigned int pos) __ksym;
-
 #endif
