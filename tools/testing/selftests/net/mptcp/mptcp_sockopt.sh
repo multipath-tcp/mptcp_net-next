be8e6be3662e9926ebfa85f70402ce00a754b8e9
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2025-01-08 19:40:38 +0100

    selftests: mptcp: move stats info in case of errors to lib.sh
    A few MPTCP selftests are using the same code to print stats in case of
    error. This code can then be moved to mptcp_lib.sh.
    
    No behaviour changes intended, except to print the error in red and to
    stderr, like most error messages.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Message-Id: <20250108-slft-mptcp-conn-misc-impr-v1-2-bdfbdba48a1f@kernel.org>

--- tools/testing/selftests/net/mptcp/mptcp_sockopt.sh
+++ tools/testing/selftests/net/mptcp/mptcp_sockopt.sh
@@ -192,11 +192,7 @@ do_transfer()
 	print_title "Transfer ${ip:2}"
 	if [ ${rets} -ne 0 ] || [ ${retc} -ne 0 ]; then
 		mptcp_lib_pr_fail "client exit code $retc, server $rets"
-		echo -e "\nnetns ${listener_ns} socket stat for ${port}:" 1>&2
-		ip netns exec ${listener_ns} ss -Menita 1>&2 -o "sport = :$port"
-
-		echo -e "\nnetns ${connector_ns} socket stat for ${port}:" 1>&2
-		ip netns exec ${connector_ns} ss -Menita 1>&2 -o "dport = :$port"
+		mptcp_lib_pr_err_stats "${listener_ns}" "${connector_ns}" "${port}"
 
 		mptcp_lib_result_fail "transfer ${ip}"
 
