bf5c2a4b6df957d5d39e2f2a9403195d43006c2b
Geliang Tang <tanggeliang@kylinos.cn>
2025-02-07 18:36:02 +0800

    selftests/bpf: Add mptcp_userspace_pm_addr bpf_iter subtest
    This patch adds a test program for the newly added mptcp_userspace_pm_addr
    bpf_iter in SEC "cgroup/getsockopt". This test iterates over all address
    entries on the local address list of userspace PM and check whether each
    one is an IPv4mapped address.
    
    Export mptcp_userspace_pm_addr helpers _new/_next/_destroy into
    bpf_experimental.h. Use bpf_mptcp_sock_acquire() to acquire the msk, then
    lock the msk pm lock and use bpf_for_each(mptcp_userspace_pm_addr) to walk
    the local address list of this msk. Invoke bpf_ipv4_is_private_10() in the
    loop to check whether the address is a "10.*" one. Then Add the address ID
    of each entry to local veriable local_ids.
    
    Out of the loop, unlock the msk pm lock and use bpf_mptcp_sock_release() to
    release the msk. Finally, assign local_ids to global variable ids so that
    the application can obtain this value.
    
    Add a subtest named test_iters_address to load and verify the newly added
    mptcp_userspace_pm_addr type bpf_iter example in test_mptcp.
    
    Since mptcp_userspace_pm_addr bpf_iter iterates over all address entries
    on the local address list of userspace PM. A set of userspace PM helpers
    are added.
    
    Set pm_type and start "pm_nl_ctl events" command to save pm events into
    the log file /tmp/bpf_userspace_pm_events in userspace_pm_init(). Kill
    "pm_nl_ctl" command and remove the log file in userspace_pm_cleanup().
    Parse the log file in userspace_pm_add_subflow() to get the token, sport
    and dport values, then use "pm_nl_ctl csf" command to create a subflow.
    
    Use the helper userspace_pm_add_subflow() to add 3 new subflow endpoints.
    Send a byte of message to start the mptcp connection, and receive the
    message.
    
    getsockopt() is invoked to trigger the "cgroup/getsockopt" test program.
    Check if skel->bss->ids equals 60 to verify whether the program loops
    three times as expected.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <950bb28cb0d973add9306655a19c306b72278956.1738924354.git.tanggeliang@kylinos.cn>

--- tools/testing/selftests/bpf/progs/mptcp_bpf_iters.c
+++ tools/testing/selftests/bpf/progs/mptcp_bpf_iters.c
@@ -61,3 +61,37 @@ out:
 	bpf_mptcp_sock_release(msk);
 	return 1;
 }
+
+SEC("cgroup/getsockopt")
+int iters_address(struct bpf_sockopt *ctx)
+{
+	struct mptcp_pm_addr_entry *entry;
+	struct bpf_sock *sk = ctx->sk;
+	struct mptcp_sock *msk;
+	int local_ids = 0;
+
+	if (ctx->level != SOL_TCP || ctx->optname != TCP_IS_MPTCP)
+		return 1;
+
+	msk = bpf_skc_to_mptcp_sock(sk);
+	if (!msk || msk->pm.server_side)
+		return 1;
+
+	bpf_spin_lock_bh(&msk->pm.lock);
+	bpf_for_each(mptcp_userspace_pm_addr, entry, (struct sock *)sk) {
+		/* Here MPTCP-specific path manager kfunc can be called:
+		 * this test is not doing anything really useful, only to
+		 * verify the iteration works.
+		 */
+
+		if (!bpf_ipv4_is_private_10(entry->addr.addr.s_addr))
+			break;
+
+		local_ids += entry->addr.id;
+	}
+	bpf_spin_unlock_bh(&msk->pm.lock);
+
+	ids = local_ids;
+
+	return 1;
+}
