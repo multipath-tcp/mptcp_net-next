98550fd88b92995c6162d342e6fb907e7c2a59f7
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2025-02-11 12:37:32 +0100

    utils: upstream: clean build dir on stable
    It looks like it only happens when building stable when objtool fails to
    build.
    
    Or maybe it is due to a switch to a different toolchain? Anyway, it
    looks safe to clean, and only rely on the caches when building stable
    versions.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- utils/virtme_upstream.sh
+++ utils/virtme_upstream.sh
@@ -8,9 +8,11 @@ is_net() {
    [ "$(b4 prep --show-info prefixes 2>/dev/null)" = "net" ]
 }
 
+CLEAN=0
 if is_stable || is_net; then
    SUFFIX=$(make kernelversion | cut -d. -f1-2)
    PACKETDRILL_STABLE=1
+   is_stable && [[ "${1}" != "vm"* ]] && CLEAN=1
 else
    SUFFIX=
    PACKETDRILL_STABLE=0
@@ -20,5 +22,6 @@ INPUT_BUILD_SKIP_PERF=1 \
    INPUT_VIRTME_EXEC_RUN="${INPUT_VIRTME_EXEC_RUN:-/dev/null}" \
    VIRTME_PACKETDRILL_STABLE=${PACKETDRILL_STABLE} \
    INPUT_SELFTESTS_MPTCP_LIB_OVERRIDE_FLAKY=0 \
+   INPUT_CLEAN=${CLEAN} \
    INPUT_BUILD_SUFFIX=${SUFFIX} \
    "./${VIRTME_SH:-.virtme.sh}" "${@:-auto-all}"
