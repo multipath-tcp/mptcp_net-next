f86b88283c5101866e8f7a1b7810d06b740198b1
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2025-02-11 12:32:45 +0100

    utils: checkpatch b4: b4 -c check-cmd is not ready
    The patch is still in review [1].
    
    Link: https://lore.kernel.org/20240926-cmdline-config-override-multivals-v1-1-80ec93afc5a1@kernel.org
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- utils/checkpatch-b4.sh
+++ utils/checkpatch-b4.sh
@@ -7,7 +7,9 @@ if [ "${branch:0:3}" != "b4/" ]; then
 fi
 
 rc=0
-b4 -c "b4.prep-perpatch-check-cmd=./scripts/checkpatch.pl -q --terse --no-summary --mailback --showfile --strict --codespell --codespellfile /usr/lib/python3/dist-packages/codespell_lib/data/dictionary.txt --max-line-length=80" prep --check || rc=$?
+#b4 -c "b4.prep-perpatch-check-cmd=./scripts/checkpatch.pl -q --terse --no-summary --mailback --showfile --strict --codespell --codespellfile /usr/lib/python3/dist-packages/codespell_lib/data/dictionary.txt --max-line-length=80" prep --check || rc=$?
+./.checkpatch.sh --git "$(b4 prep --show-info start-commit).." || rc=$?
+b4 prep --check
 
 prefixes="$(b4 prep --show-info prefixes)"
 echo
