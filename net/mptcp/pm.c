ec0dc1a1901c1fdd511c428a3472c994425e06f3
Geliang Tang <tanggeliang@kylinos.cn>
2025-03-03 12:22:52 +0800

    mptcp: sysctl: add available_path_managers
    Similarly to net.mptcp.available_schedulers, this patch adds a new one
    net.mptcp.available_path_managers to list the available path mangers.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <4af75cf9a0ebfef70ca6151d05624b8d5abc723b.1740975633.git.tanggeliang@kylinos.cn>

--- net/mptcp/pm.c
+++ net/mptcp/pm.c
@@ -1079,3 +1079,22 @@ void mptcp_pm_unregister(struct mptcp_pm_ops *pm)
 	list_del_rcu(&pm->list);
 	spin_unlock(&mptcp_pm_list_lock);
 }
+
+/* Build string with list of available path manager values.
+ * Similar to tcp_get_available_congestion_control()
+ */
+void mptcp_pm_get_available(char *buf, size_t maxlen)
+{
+	struct mptcp_pm_ops *pm;
+	size_t offs = 0;
+
+	rcu_read_lock();
+	list_for_each_entry_rcu(pm, &mptcp_pm_list, list) {
+		offs += snprintf(buf + offs, maxlen - offs, "%s%s",
+				 offs == 0 ? "" : " ", pm->name);
+
+		if (WARN_ON_ONCE(offs >= maxlen))
+			break;
+	}
+	rcu_read_unlock();
+}
