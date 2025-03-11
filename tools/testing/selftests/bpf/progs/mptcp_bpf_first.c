6857dbc0497b3504b2b4397788145305f1fe5ef7
Geliang Tang <tanggeliang@kylinos.cn>
2025-03-11 23:38:22 +0100

    selftests/bpf: Add bpf_first scheduler & test
    This patch implements the simplest MPTCP scheduler, named bpf_first,
    which always picks the first subflow to send data. It's a sample of
    MPTCP BPF scheduler implementations.
    
    This patch defines MPTCP_SCHED_TEST macro, a template for all scheduler
    tests. Every scheduler is identified by argument name, and use sysctl
    to set net.mptcp.scheduler as "bpf_name" to use this sched. Add two
    veth net devices to simulate the multiple addresses case. Use 'ip mptcp
    endpoint' command to add the new endpoint ADDR2 to PM netlink. Arguments
    addr1/add2 means whether the data has been sent on the first/second
    subflow or not. Send data and check bytes_sent of 'ss' output after it
    using send_data_and_verify().
    
    Using MPTCP_SCHED_TEST macro to add a new test for this bpf_first
    scheduler, the arguments "1 0" means data has been only sent on the
    first subflow ADDR1. Run this test by RUN_MPTCP_TEST macro.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Acked-by: Paolo Abeni <pabeni@redhat.com>
    Reviewed-by: Mat Martineau <martineau@kernel.org>
    Reviewed-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

---
+++ tools/testing/selftests/bpf/progs/mptcp_bpf_first.c
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (c) 2022, SUSE. */
+
+#include "mptcp_bpf.h"
+#include <bpf/bpf_tracing.h>
+
+char _license[] SEC("license") = "GPL";
+
+SEC("struct_ops")
+void BPF_PROG(mptcp_sched_first_init, struct mptcp_sock *msk)
+{
+}
+
+SEC("struct_ops")
+void BPF_PROG(mptcp_sched_first_release, struct mptcp_sock *msk)
+{
+}
+
+SEC("struct_ops")
+int BPF_PROG(bpf_first_get_send, struct mptcp_sock *msk)
+{
+	struct mptcp_subflow_context *subflow;
+
+	subflow = bpf_mptcp_subflow_ctx(msk->first);
+	if (!subflow)
+		return -1;
+
+	mptcp_subflow_set_scheduled(subflow, true);
+	return 0;
+}
+
+SEC(".struct_ops")
+struct mptcp_sched_ops first = {
+	.init		= (void *)mptcp_sched_first_init,
+	.release	= (void *)mptcp_sched_first_release,
+	.get_send	= (void *)bpf_first_get_send,
+	.name		= "bpf_first",
+};
