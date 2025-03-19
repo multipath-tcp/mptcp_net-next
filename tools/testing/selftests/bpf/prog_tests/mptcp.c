a9f93eb773be3fd7048e7c5185d0598bbb942b2a
Geliang Tang <tanggeliang@kylinos.cn>
2025-03-19 18:33:51 +0100

    selftests/bpf: Add bpf_bkup scheduler & test
    This patch implements the backup flag test scheduler, named bpf_bkup,
    which picks the first non-backup subflow to send data.
    
    Using MPTCP_SCHED_TEST macro to add a new test for this bpf_bkup
    scheduler, the arguments "1 0" means data has been only sent on the
    first subflow ADDR1. Run this test by RUN_MPTCP_TEST macro.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Reviewed-by: Mat Martineau <martineau@kernel.org>
    Reviewed-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- tools/testing/selftests/bpf/prog_tests/mptcp.c
+++ tools/testing/selftests/bpf/prog_tests/mptcp.c
@@ -13,6 +13,7 @@
 #include "mptcp_subflow.skel.h"
 #include "mptcp_bpf_iters.skel.h"
 #include "mptcp_bpf_first.skel.h"
+#include "mptcp_bpf_bkup.skel.h"
 
 #define NS_TEST "mptcp_ns"
 #define ADDR_1	"10.0.1.1"
@@ -683,6 +684,18 @@ static void test_first(void)
 	mptcp_bpf_first__destroy(skel);
 }
 
+static void test_bkup(void)
+{
+	struct mptcp_bpf_bkup *skel;
+
+	skel = mptcp_bpf_bkup__open_and_load();
+	if (!ASSERT_OK_PTR(skel, "open_and_load: bkup"))
+		return;
+
+	test_bpf_sched(skel->obj, "bkup", WITH_DATA, WITHOUT_DATA);
+	mptcp_bpf_bkup__destroy(skel);
+}
+
 void test_mptcp(void)
 {
 	if (test__start_subtest("base"))
@@ -697,4 +710,6 @@ void test_mptcp(void)
 		test_default();
 	if (test__start_subtest("first"))
 		test_first();
+	if (test__start_subtest("bkup"))
+		test_bkup();
 }
