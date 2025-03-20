3aeab05e4884b101a89fc167d0ed96447f6f031a
Geliang Tang <tanggeliang@kylinos.cn>
2025-03-20 17:32:42 +0100

    selftests/bpf: Add bpf_bkup scheduler & test
    This patch implements the backup flag test scheduler, named bpf_bkup,
    which picks the first non-backup subflow to send data.
    
    Using MPTCP_SCHED_TEST macro to add a new test for this bpf_bkup
    scheduler, the arguments "1 0" means data has been only sent on the
    first subflow ADDR1. Run this test by RUN_MPTCP_TEST macro.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Reviewed-by: Mat Martineau <martineau@kernel.org>
    Reviewed-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

---
+++ tools/testing/selftests/bpf/progs/mptcp_bpf_bkup.c
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (c) 2022, SUSE. */
+
+#include "mptcp_bpf.h"
+#include <bpf/bpf_tracing.h>
+
+char _license[] SEC("license") = "GPL";
+
+SEC("struct_ops")
+void BPF_PROG(mptcp_sched_bkup_init, struct mptcp_sock *msk)
+{
+}
+
+SEC("struct_ops")
+void BPF_PROG(mptcp_sched_bkup_release, struct mptcp_sock *msk)
+{
+}
+
+SEC("struct_ops")
+int BPF_PROG(bpf_bkup_get_send, struct mptcp_sock *msk)
+{
+	struct mptcp_subflow_context *subflow;
+
+	bpf_for_each(mptcp_subflow, subflow, (struct sock *)msk) {
+		if (!BPF_CORE_READ_BITFIELD_PROBED(subflow, backup) ||
+		    !BPF_CORE_READ_BITFIELD_PROBED(subflow, request_bkup)) {
+			mptcp_subflow_set_scheduled(subflow, true);
+			break;
+		}
+	}
+
+	return 0;
+}
+
+SEC(".struct_ops")
+struct mptcp_sched_ops bkup = {
+	.init		= (void *)mptcp_sched_bkup_init,
+	.release	= (void *)mptcp_sched_bkup_release,
+	.get_send	= (void *)bpf_bkup_get_send,
+	.name		= "bpf_bkup",
+};
