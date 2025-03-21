bbdbc3180abc489d96cb4a35883630ccadf46ec9
Geliang Tang <tanggeliang@kylinos.cn>
2025-03-21 09:45:01 +0800

    mptcp: pm: add add_addr_received() interface
    This patch adds an optional .add_addr_received interface for struct
    mptcp_pm_ops and invokes it in mptcp_pm_add_addr_received(). A new helper
    mptcp_pm_add_addr_recv() is added to allow the MPTCP_PM_ADD_ADDR_RECEIVED
    worker can be invoke from the in-kernel PM.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <2d7bb2ff8960c244aee00ec314f23edd0f5577df.1742521397.git.tanggeliang@kylinos.cn>

--- include/net/mptcp.h
+++ include/net/mptcp.h
@@ -132,6 +132,10 @@ struct mptcp_pm_ops {
 	bool (*allow_new_subflow)(struct mptcp_sock *msk);
 	bool (*accept_new_subflow)(const struct mptcp_sock *msk);
 
+	/* optional */
+	int (*add_addr_received)(struct mptcp_sock *msk,
+				 const struct mptcp_addr_info *addr);
+
 	char			name[MPTCP_PM_NAME_MAX];
 	struct module		*owner;
 	struct list_head	list;
