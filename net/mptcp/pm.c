fc3dfd121cb4448afbeb8a67969fd5d2bdd77128
Geliang Tang <tanggeliang@kylinos.cn>
2025-03-27 14:04:14 +0800

    mptcp: pm: update pm lock order in mptcp_pm_worker
    Later functions that cannot hold the mptcp pm lock will be called from
    the PM worker, so this patch modifies the order of holding the lock
    at the beginning of this function and releasing the lock at the end.
    
    The new order is to obtain a copy of pm->status while holding the mptcp
    pm lock, then read the copy after releasing the lock.
    
    For each PM status flag, hold the lock, clear this flag of pm->status,
    and then call each handling function before or after releasing the lock
    as needed.
    
    Finally, hold the lock before calling __mptcp_pm_kernel_worker() and
    release it afterwards.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <739e3667698915fa690d08c751c1846bd9442df2.1743054942.git.tanggeliang@kylinos.cn>

--- net/mptcp/pm.c
+++ net/mptcp/pm.c
@@ -932,26 +932,33 @@ void mptcp_pm_subflow_chk_stale(const struct mptcp_sock *msk, struct sock *ssk)
 void mptcp_pm_worker(struct mptcp_sock *msk)
 {
 	struct mptcp_pm_data *pm = &msk->pm;
+	u8 status;
 
 	msk_owned_by_me(msk);
 
 	if (!(pm->status & MPTCP_PM_WORK_MASK))
 		return;
 
-	spin_lock_bh(&msk->pm.lock);
+	spin_lock_bh(&pm->lock);
+	status = READ_ONCE(pm->status);
+	spin_unlock_bh(&pm->lock);
 
-	pr_debug("msk=%p status=%x\n", msk, pm->status);
-	if (pm->status & BIT(MPTCP_PM_ADD_ADDR_SEND_ACK)) {
+	pr_debug("msk=%p status=%x\n", msk, status);
+	if (status & BIT(MPTCP_PM_ADD_ADDR_SEND_ACK)) {
+		spin_lock_bh(&pm->lock);
 		pm->status &= ~BIT(MPTCP_PM_ADD_ADDR_SEND_ACK);
 		mptcp_pm_addr_send_ack(msk);
+		spin_unlock_bh(&pm->lock);
 	}
-	if (pm->status & BIT(MPTCP_PM_RM_ADDR_RECEIVED)) {
+	if (status & BIT(MPTCP_PM_RM_ADDR_RECEIVED)) {
+		spin_lock_bh(&pm->lock);
 		pm->status &= ~BIT(MPTCP_PM_RM_ADDR_RECEIVED);
 		mptcp_pm_rm_addr_recv(msk);
+		spin_unlock_bh(&pm->lock);
 	}
+	spin_lock_bh(&pm->lock);
 	__mptcp_pm_kernel_worker(msk);
-
-	spin_unlock_bh(&msk->pm.lock);
+	spin_unlock_bh(&pm->lock);
 }
 
 static void mptcp_pm_ops_init(struct mptcp_sock *msk,
