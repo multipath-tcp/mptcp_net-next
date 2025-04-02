a23e3c0e3e8a65c5e38536e9c4ac5509742bf53a
Geliang Tang <tanggeliang@kylinos.cn>
2025-04-02 10:53:08 +0800

    mptcp: pm: userspace: drop delete_local_addr helper
    Address entries should not be removed from local_addr_list when a subflow
    is deleted by the userspace PM, should only be removed when sending a
    REMOVE_ADDR.
    
    So mptcp_userspace_pm_delete_local_addr() helper shouldn't be called in
    mptcp_pm_nl_subflow_create_doit() and mptcp_pm_nl_subflow_destroy_doit().
    
    Since this helper is open-coding in mptcp_pm_nl_remove_doit(), it can be
    dropped now.
    
    Closes: https://github.com/multipath-tcp/mptcp_net-next/issues/403
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-Id: <f03554fea72ef5e89f940767466f88f0af36f88c.1743562290.git.tanggeliang@kylinos.cn>

--- net/mptcp/pm_userspace.c
+++ net/mptcp/pm_userspace.c
@@ -90,30 +90,6 @@ append_err:
 	return ret;
 }
 
-/* If the subflow is closed from the other peer (not via a
- * subflow destroy command then), we want to keep the entry
- * not to assign the same ID to another address and to be
- * able to send RM_ADDR after the removal of the subflow.
- */
-static int mptcp_userspace_pm_delete_local_addr(struct mptcp_sock *msk,
-						struct mptcp_pm_addr_entry *addr)
-{
-	struct sock *sk = (struct sock *)msk;
-	struct mptcp_pm_addr_entry *entry;
-
-	entry = mptcp_userspace_pm_lookup_addr(msk, &addr->addr);
-	if (!entry)
-		return -EINVAL;
-
-	/* TODO: a refcount is needed because the entry can
-	 * be used multiple times (e.g. fullmesh mode).
-	 */
-	list_del_rcu(&entry->list);
-	sock_kfree_s(sk, entry, sizeof(*entry));
-	msk->pm.local_addr_used--;
-	return 0;
-}
-
 static struct mptcp_pm_addr_entry *
 mptcp_userspace_pm_lookup_addr_by_id(struct mptcp_sock *msk, unsigned int id)
 {
@@ -409,14 +385,13 @@ int mptcp_pm_nl_subflow_create_doit(struct sk_buff *skb, struct genl_info *info)
 	err = __mptcp_subflow_connect(sk, &local, &addr_r);
 	release_sock(sk);
 
-	if (err)
+	if (err) {
 		GENL_SET_ERR_MSG_FMT(info, "connect error: %d", err);
+		goto create_err;
+	}
 
 	spin_lock_bh(&msk->pm.lock);
-	if (err)
-		mptcp_userspace_pm_delete_local_addr(msk, &entry);
-	else
-		msk->pm.subflows++;
+	msk->pm.subflows++;
 	spin_unlock_bh(&msk->pm.lock);
 
  create_err:
@@ -535,9 +510,6 @@ int mptcp_pm_nl_subflow_destroy_doit(struct sk_buff *skb, struct genl_info *info
 		goto release_sock;
 	}
 
-	spin_lock_bh(&msk->pm.lock);
-	mptcp_userspace_pm_delete_local_addr(msk, &addr_l);
-	spin_unlock_bh(&msk->pm.lock);
 	mptcp_subflow_shutdown(sk, ssk, RCV_SHUTDOWN | SEND_SHUTDOWN);
 	mptcp_close_ssk(sk, ssk, mptcp_subflow_ctx(ssk));
 	MPTCP_INC_STATS(sock_net(sk), MPTCP_MIB_RMSUBFLOW);
