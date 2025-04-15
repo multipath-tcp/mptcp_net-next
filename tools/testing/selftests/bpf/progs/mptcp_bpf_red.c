8da424fa33f8125d7d6d668f2ab7bb3ae6fdc7ce
Geliang Tang <tanggeliang@kylinos.cn>
2025-04-15 05:55:12 +0000

    selftests/bpf: Add bpf_red scheduler & test
    This patch implements the redundant BPF MPTCP scheduler, named bpf_red,
    which sends all packets redundantly on all available subflows.
    
    Using MPTCP_SCHED_TEST macro to add a new test for this bpf_red
    scheduler, the arguments "1 1" means data has been sent on both
    net devices. Run this test by RUN_MPTCP_TEST macro.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Reviewed-by: Mat Martineau <martineau@kernel.org>
    Reviewed-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

---
+++ tools/testing/selftests/bpf/progs/mptcp_bpf_red.c
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (c) 2022, SUSE. */
+
+#include "mptcp_bpf.h"
+#include <bpf/bpf_tracing.h>
+
+char _license[] SEC("license") = "GPL";
+
+SEC("struct_ops")
+void BPF_PROG(mptcp_sched_red_init, struct mptcp_sock *msk)
+{
+}
+
+SEC("struct_ops")
+void BPF_PROG(mptcp_sched_red_release, struct mptcp_sock *msk)
+{
+}
+
+SEC("struct_ops")
+int BPF_PROG(bpf_red_get_send, struct mptcp_sock *msk)
+{
+	struct mptcp_subflow_context *subflow;
+
+	bpf_for_each(mptcp_subflow, subflow, (struct sock *)msk)
+		mptcp_subflow_set_scheduled(subflow, true);
+
+	return 0;
+}
+
+SEC(".struct_ops.link")
+struct mptcp_sched_ops red = {
+	.init		= (void *)mptcp_sched_red_init,
+	.release	= (void *)mptcp_sched_red_release,
+	.get_send	= (void *)bpf_red_get_send,
+	.name		= "bpf_red",
+};
