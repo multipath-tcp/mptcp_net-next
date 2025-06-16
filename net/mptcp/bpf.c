7fa52ccce0049b4bfb321506b8407386da23bd02
Geliang Tang <tanggeliang@kylinos.cn>
2025-06-16 05:55:52 +0000

    bpf: Add mptcp_subflow bpf_iter
    It's necessary to traverse all subflows on the conn_list of an MPTCP
    socket and then call kfunc to modify the fields of each subflow. In
    kernel space, mptcp_for_each_subflow() helper is used for this:
    
    	mptcp_for_each_subflow(msk, subflow)
    		kfunc(subflow);
    
    But in the MPTCP BPF program, this has not yet been implemented. As
    Martin suggested recently, this conn_list walking + modify-by-kfunc
    usage fits the bpf_iter use case.
    
    So this patch adds a new bpf_iter type named "mptcp_subflow" to do
    this and implements its helpers bpf_iter_mptcp_subflow_new()/_next()/
    _destroy(). And register these bpf_iter mptcp_subflow into mptcp
    common kfunc set. Then bpf_for_each() for mptcp_subflow can be used
    in BPF program like this:
    
    	bpf_for_each(mptcp_subflow, subflow, msk)
    		kfunc(subflow);
    
    v2: remove msk->pm.lock in _new() and _destroy() (Martin)
        drop DEFINE_BPF_ITER_FUNC, change opaque[3] to opaque[2] (Andrii)
    v3: drop bpf_iter__mptcp_subflow
    v4: if msk is NULL, initialize kit->msk to NULL in _new() and check it in
        _next() (Andrii)
    v5: use list_is_last() instead of list_entry_is_head()
        add KF_ITER_NEW/NEXT/DESTROY flags
        add msk_owned_by_me in _new()
    v6: add KF_TRUSTED_ARGS flag (Andrii, Martin)
    
    Suggested-by: Martin KaFai Lau <martin.lau@kernel.org>
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Reviewed-by: Mat Martineau <martineau@kernel.org>

--- net/mptcp/bpf.c
+++ net/mptcp/bpf.c
@@ -29,8 +29,82 @@ static const struct btf_kfunc_id_set bpf_mptcp_fmodret_set = {
 	.set   = &bpf_mptcp_fmodret_ids,
 };
 
+struct bpf_iter_mptcp_subflow {
+	__u64 __opaque[2];
+} __aligned(8);
+
+struct bpf_iter_mptcp_subflow_kern {
+	struct mptcp_sock *msk;
+	struct list_head *pos;
+} __aligned(8);
+
+__bpf_kfunc_start_defs();
+
+__bpf_kfunc static int
+bpf_iter_mptcp_subflow_new(struct bpf_iter_mptcp_subflow *it,
+			   struct sock *sk)
+{
+	struct bpf_iter_mptcp_subflow_kern *kit = (void *)it;
+	struct mptcp_sock *msk;
+
+	BUILD_BUG_ON(sizeof(struct bpf_iter_mptcp_subflow_kern) >
+		     sizeof(struct bpf_iter_mptcp_subflow));
+	BUILD_BUG_ON(__alignof__(struct bpf_iter_mptcp_subflow_kern) !=
+		     __alignof__(struct bpf_iter_mptcp_subflow));
+
+	if (unlikely(!sk || !sk_fullsock(sk)))
+		return -EINVAL;
+
+	if (sk->sk_protocol != IPPROTO_MPTCP)
+		return -EINVAL;
+
+	msk = mptcp_sk(sk);
+
+	msk_owned_by_me(msk);
+
+	kit->msk = msk;
+	kit->pos = &msk->conn_list;
+	return 0;
+}
+
+__bpf_kfunc static struct mptcp_subflow_context *
+bpf_iter_mptcp_subflow_next(struct bpf_iter_mptcp_subflow *it)
+{
+	struct bpf_iter_mptcp_subflow_kern *kit = (void *)it;
+
+	if (!kit->msk || list_is_last(kit->pos, &kit->msk->conn_list))
+		return NULL;
+
+	kit->pos = kit->pos->next;
+	return list_entry(kit->pos, struct mptcp_subflow_context, node);
+}
+
+__bpf_kfunc static void
+bpf_iter_mptcp_subflow_destroy(struct bpf_iter_mptcp_subflow *it)
+{
+}
+
+__bpf_kfunc_end_defs();
+
+BTF_KFUNCS_START(bpf_mptcp_iter_kfunc_ids)
+BTF_ID_FLAGS(func, bpf_iter_mptcp_subflow_new, KF_ITER_NEW | KF_TRUSTED_ARGS)
+BTF_ID_FLAGS(func, bpf_iter_mptcp_subflow_next, KF_ITER_NEXT | KF_RET_NULL)
+BTF_ID_FLAGS(func, bpf_iter_mptcp_subflow_destroy, KF_ITER_DESTROY)
+BTF_KFUNCS_END(bpf_mptcp_iter_kfunc_ids)
+
+static const struct btf_kfunc_id_set bpf_mptcp_iter_kfunc_set = {
+	.owner	= THIS_MODULE,
+	.set	= &bpf_mptcp_iter_kfunc_ids,
+};
+
 static int __init bpf_mptcp_kfunc_init(void)
 {
-	return register_btf_fmodret_id_set(&bpf_mptcp_fmodret_set);
+	int ret;
+
+	ret = register_btf_fmodret_id_set(&bpf_mptcp_fmodret_set);
+	ret = ret ?: register_btf_kfunc_id_set(BPF_PROG_TYPE_STRUCT_OPS,
+					       &bpf_mptcp_iter_kfunc_set);
+
+	return ret;
 }
 late_initcall(bpf_mptcp_kfunc_init);
