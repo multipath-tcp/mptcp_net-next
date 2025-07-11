e1069c4a53cc8472398b54ece09058da96154b9d
Geliang Tang <tanggeliang@kylinos.cn>
2025-07-11 09:36:05 +0800

    Squash to "mptcp: add recv_should_stop helper"
    tcp: add recv_should_stop helper
    
    Drop "shutdown" parameter of mptcp_recv_should_stop.
    Use mptcp_recv_should_stop for "if (copied)" case too.
    Rename this helper as tcp_recv_should_stop using for both TCP and MPTCP.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-ID: <782e65ce91f13786c11954cf5b69c481f193c44e.1752197546.git.tanggeliang@kylinos.cn>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -2116,40 +2116,12 @@ static unsigned int mptcp_inq_hint(const struct sock *sk)
 	return 0;
 }
 
-static int mptcp_recv_should_stop(struct sock *sk, long timeo, int *shutdown)
-{
-	if (sk->sk_err)
-		return sock_error(sk);
-
-	if (sk->sk_shutdown & RCV_SHUTDOWN) {
-		*shutdown = 1;
-		/* race breaker: the shutdown could be after the
-		 * previous receive queue check
-		 */
-		if (__mptcp_move_skbs(sk))
-			*shutdown = 0;
-		return 0;
-	}
-
-	if (sk->sk_state == TCP_CLOSE)
-		return -ENOTCONN;
-
-	if (!timeo)
-		return -EAGAIN;
-
-	if (signal_pending(current))
-		return sock_intr_errno(timeo);
-
-	return 0;
-}
-
 static int mptcp_recvmsg(struct sock *sk, struct msghdr *msg, size_t len,
 			 int flags, int *addr_len)
 {
 	struct mptcp_sock *msk = mptcp_sk(sk);
 	struct scm_timestamping_internal tss;
 	int copied = 0, cmsg_flags = 0;
-	int shutdown = -1;
 	int target;
 	long timeo;
 
@@ -2192,19 +2164,22 @@ static int mptcp_recvmsg(struct sock *sk, struct msghdr *msg, size_t len,
 		if (copied >= target)
 			break;
 
-		if (copied) {
-			if (sk->sk_err ||
-			    sk->sk_state == TCP_CLOSE ||
-			    (sk->sk_shutdown & RCV_SHUTDOWN) ||
-			    !timeo ||
-			    signal_pending(current))
+		err = tcp_recv_should_stop(sk, timeo);
+		if (err < 0) {
+			if (copied)
 				break;
-		} else {
-			copied = mptcp_recv_should_stop(sk, timeo, &shutdown);
-			if (copied < 0 || shutdown == 1)
+
+			if (err == -ESHUTDOWN) {
+				/* race breaker: the shutdown could be after the
+				 * previous receive queue check
+				 */
+				if (__mptcp_move_skbs(sk))
+					continue;
 				break;
-			if (shutdown == 0)
-				continue;
+			}
+
+			copied = err;
+			break;
 		}
 
 		pr_debug("block timeout %ld\n", timeo);
