465db19db788dadb775f09dbc34a643c94c28721
Geliang Tang <tanggeliang@kylinos.cn>
2025-07-19 00:12:27 +0200

    mptcp: pm: add get_priority() interface
    This patch adds get_priority() interface for struct mptcp_sched_ops,
    then mptcp_pm_is_backup() can directly invoke get_priority() interface
    through "ops" of "msk->pm". Instead of using mptcp_pm_is_userspace()
    to check which is_backup() helper to invoke.
    
    Also rename mptcp_pm_nl_is_backup() to mptcp_pm_kernel_get_priority()
    and mptcp_userspace_pm_is_backup() to mptcp_pm_userspace_get_priority(),
    and make them static.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Reviewed-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- net/mptcp/pm_userspace.c
+++ net/mptcp/pm_userspace.c
@@ -145,8 +145,8 @@ static int mptcp_pm_userspace_get_local_id(struct mptcp_sock *msk,
 	return mptcp_userspace_pm_append_new_local_addr(msk, skc, true);
 }
 
-bool mptcp_userspace_pm_is_backup(struct mptcp_sock *msk,
-				  struct mptcp_addr_info *skc)
+static bool mptcp_pm_userspace_get_priority(struct mptcp_sock *msk,
+					    struct mptcp_addr_info *skc)
 {
 	struct mptcp_pm_addr_entry *entry;
 	bool backup;
@@ -694,6 +694,7 @@ static void mptcp_pm_userspace_release(struct mptcp_sock *msk)
 
 static struct mptcp_pm_ops mptcp_pm_userspace = {
 	.get_local_id		= mptcp_pm_userspace_get_local_id,
+	.get_priority		= mptcp_pm_userspace_get_priority,
 	.release		= mptcp_pm_userspace_release,
 	.name			= "userspace",
 	.owner			= THIS_MODULE,
