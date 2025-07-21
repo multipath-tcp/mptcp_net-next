100f16e2d28d31e5f072028ccd7b7799b79ff9f6
Kuniyuki Iwashima <kuniyu@google.com>
2025-07-21 20:35:20 +0000

    mptcp: Fix up subflow's memcg when CONFIG_SOCK_CGROUP_DATA=n.
    When sk_alloc() allocates a socket, mem_cgroup_sk_alloc() sets
    sk->sk_memcg based on the current task.
    
    MPTCP subflow socket creation is triggered from userspace or
    an in-kernel worker.
    
    In the latter case, sk->sk_memcg is not what we want.  So, we fix
    it up from the parent socket's sk->sk_memcg in mptcp_attach_cgroup().
    
    Although the code is placed under #ifdef CONFIG_MEMCG, it is buried
    under #ifdef CONFIG_SOCK_CGROUP_DATA.
    
    The two configs are orthogonal.  If CONFIG_MEMCG is enabled without
    CONFIG_SOCK_CGROUP_DATA, the subflow's memory usage is not charged
    correctly.
    
    Let's move the code out of the wrong ifdef guard.
    
    Note that sk->sk_memcg is freed in sk_prot_free() and the parent
    sk holds the refcnt of memcg->css here, so we don't need to use
    css_tryget().
    
    Fixes: 3764b0c5651e3 ("mptcp: attach subflow socket to parent cgroup")
    Signed-off-by: Kuniyuki Iwashima <kuniyu@google.com>
    Message-ID: <20250721203624.3807041-2-kuniyu@google.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -1602,6 +1602,8 @@ extern struct static_key_false memcg_sockets_enabled_key;
 #define mem_cgroup_sockets_enabled static_branch_unlikely(&memcg_sockets_enabled_key)
 void mem_cgroup_sk_alloc(struct sock *sk);
 void mem_cgroup_sk_free(struct sock *sk);
+void mem_cgroup_sk_inherit(const struct sock *sk, struct sock *newsk);
+
 static inline bool mem_cgroup_under_socket_pressure(struct mem_cgroup *memcg)
 {
 #ifdef CONFIG_MEMCG_V1
@@ -1623,6 +1625,11 @@ void reparent_shrinker_deferred(struct mem_cgroup *memcg);
 #define mem_cgroup_sockets_enabled 0
 static inline void mem_cgroup_sk_alloc(struct sock *sk) { };
 static inline void mem_cgroup_sk_free(struct sock *sk) { };
+
+static inline void mem_cgroup_sk_inherit(const struct sock *sk, struct sock *newsk)
+{
+}
+
 static inline bool mem_cgroup_under_socket_pressure(struct mem_cgroup *memcg)
 {
 	return false;
