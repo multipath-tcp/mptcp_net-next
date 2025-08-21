7ffe35ff034a2196aca667f620f6752bb411a61f
Geliang Tang <tanggeliang@kylinos.cn>
2025-08-21 16:54:53 +0800

    selftests: mptcp: sockopt: add SO_BINDTOIFINDEX test
    Implement tests for the SO_BINDTOIFINDEX socket option:
    - Uses ioctl to get the loopback interface index
    - Sets the option to bind to "lo"
    - Validates the bound index via getsockopt
    - Supports both IPv4 and IPv6
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-ID: <91ec59691a0dae607baa6f6260643b1617483c6c.1755680616.git.tanggeliang@kylinos.cn>

--- tools/testing/selftests/net/mptcp/mptcp_sockopt.sh
+++ tools/testing/selftests/net/mptcp/mptcp_sockopt.sh
@@ -271,7 +271,7 @@ do_mptcp_sockopt_tests()
 
 	for opt in "SOL_MPTCP" \
 		   "SO_REUSEADDR" "SO_REUSEPORT" \
-		   "SO_BINDTODEVICE"; do
+		   "SO_BINDTODEVICE" "SO_BINDTOIFINDEX"; do
 		print_title "$opt sockopt v4"
 		mptcp_lib_pr_ok
 		mptcp_lib_result_pass "$opt sockopt v4"
@@ -290,7 +290,7 @@ do_mptcp_sockopt_tests()
 
 	for opt in "SOL_MPTCP" \
 		   "SO_REUSEADDR" "SO_REUSEPORT" \
-		   "SO_BINDTODEVICE"; do
+		   "SO_BINDTODEVICE" "SO_BINDTOIFINDEX"; do
 		print_title "$opt sockopt v6"
 		mptcp_lib_pr_ok
 		mptcp_lib_result_pass "$opt sockopt v6"
