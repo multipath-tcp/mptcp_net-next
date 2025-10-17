b3fcd37af116b9d47aff33250e7ad14da0387147
Geliang Tang <tanggeliang@kylinos.cn>
2025-10-17 05:54:58 +0000

    mptcp: pm: add get_priority() interface
    This patch adds get_priority() interface for struct mptcp_sched_ops,
    then mptcp_pm_is_backup() can directly invoke get_priority() interface
    through "ops" of "msk->pm". Instead of using mptcp_pm_is_userspace()
    to check which is_backup() helper to invoke.
    
    Also rename mptcp_pm_nl_is_backup() to mptcp_pm_kernel_get_priority()
    and mptcp_userspace_pm_is_backup() to mptcp_pm_userspace_get_priority(),
    and make them static.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Reviewed-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- net/mptcp/pm.c
+++ net/mptcp/pm.c
@@ -921,10 +921,7 @@ bool mptcp_pm_is_backup(struct mptcp_sock *msk, struct sock_common *skc)
 
 	mptcp_local_address((struct sock_common *)skc, &skc_local);
 
-	if (mptcp_pm_is_userspace(msk))
-		return mptcp_userspace_pm_is_backup(msk, &skc_local);
-
-	return mptcp_pm_nl_is_backup(msk, &skc_local);
+	return msk->pm.ops->get_priority(msk, &skc_local);
 }
 
 static void mptcp_pm_subflows_chk_stale(const struct mptcp_sock *msk, struct sock *ssk)
@@ -1090,7 +1087,7 @@ struct mptcp_pm_ops *mptcp_pm_find(const char *name)
 
 int mptcp_pm_validate(struct mptcp_pm_ops *pm_ops)
 {
-	if (!pm_ops->get_local_id) {
+	if (!pm_ops->get_local_id || !pm_ops->get_priority) {
 		pr_err("%s does not implement required ops\n", pm_ops->name);
 		return -EINVAL;
 	}
