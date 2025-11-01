f31a07afa409c6ad9082423577b374dd0c676d7e
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2025-11-01 10:14:45 +0100

    selftests: mptcp: join: userspace: longer transfer
    In rare cases, when the test environment is very slow, some userspace
    tests can fail because some expected events have not been seen.
    
    Because the tests are expecting a long on-going connection, and they are
    not waiting for the end of the transfer, it is fine to make the
    connection longer. This connection will be killed at the end, after the
    verifications, so making it longer doesn't change anything, apart from
    avoid it to end before the end of the verifications
    
    To play it safe, all userspace tests not waiting for the end of the
    transfer are now sharing a longer file (128KB) at slow speed.
    
    Fixes: 4369c198e599 ("selftests: mptcp: test userspace pm out of transfer")
    Fixes: b2e2248f365a ("selftests: mptcp: userspace pm create id 0 subflow")
    Fixes: e3b47e460b4b ("selftests: mptcp: userspace pm remove initial subflow")
    Fixes: b9fb176081fb ("selftests: mptcp: userspace pm send RM_ADDR for ID 0")
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Message-ID: <20251101-slft-join-inst-v1-4-10572512a0b2@kernel.org>

--- tools/testing/selftests/net/mptcp/mptcp_join.sh
+++ tools/testing/selftests/net/mptcp/mptcp_join.sh
@@ -3952,7 +3952,7 @@ userspace_tests()
 	   continue_if mptcp_lib_has_file '/proc/sys/net/mptcp/pm_type'; then
 		set_userspace_pm $ns1
 		pm_nl_set_limits $ns2 2 2
-		{ speed=5 \
+		{ test_linkfail=128 speed=5 \
 			run_tests $ns1 $ns2 10.0.1.1 & } 2>/dev/null
 		local tests_pid=$!
 		wait_mpj $ns1
@@ -3985,7 +3985,7 @@ userspace_tests()
 	   continue_if mptcp_lib_has_file '/proc/sys/net/mptcp/pm_type'; then
 		set_userspace_pm $ns2
 		pm_nl_set_limits $ns1 0 1
-		{ speed=5 \
+		{ test_linkfail=128 speed=5 \
 			run_tests $ns1 $ns2 10.0.1.1 & } 2>/dev/null
 		local tests_pid=$!
 		wait_mpj $ns2
@@ -4013,7 +4013,7 @@ userspace_tests()
 	   continue_if mptcp_lib_has_file '/proc/sys/net/mptcp/pm_type'; then
 		set_userspace_pm $ns2
 		pm_nl_set_limits $ns1 0 1
-		{ speed=5 \
+		{ test_linkfail=128 speed=5 \
 			run_tests $ns1 $ns2 10.0.1.1 & } 2>/dev/null
 		local tests_pid=$!
 		wait_mpj $ns2
@@ -4034,7 +4034,7 @@ userspace_tests()
 	   continue_if mptcp_lib_has_file '/proc/sys/net/mptcp/pm_type'; then
 		set_userspace_pm $ns2
 		pm_nl_set_limits $ns1 0 1
-		{ speed=5 \
+		{ test_linkfail=128 speed=5 \
 			run_tests $ns1 $ns2 10.0.1.1 & } 2>/dev/null
 		local tests_pid=$!
 		wait_mpj $ns2
@@ -4058,7 +4058,7 @@ userspace_tests()
 	   continue_if mptcp_lib_has_file '/proc/sys/net/mptcp/pm_type'; then
 		set_userspace_pm $ns1
 		pm_nl_set_limits $ns2 1 1
-		{ speed=5 \
+		{ test_linkfail=128 speed=5 \
 			run_tests $ns1 $ns2 10.0.1.1 & } 2>/dev/null
 		local tests_pid=$!
 		wait_mpj $ns1
