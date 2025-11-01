dccecb55467df474da7417ff74864d49e53d18a5
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2025-11-01 10:14:47 +0100

    selftests: mptcp: connect: trunc: read all recv data
    MPTCP Join "fastclose server" selftest is sometimes failing because the
    client output file doesn't have the expected size, e.g. 296B instead of
    1024B.
    
    When looking at a packet trace when this happens, the server sent the
    expected 1024B in two parts -- 100B, then 924B -- then the MP_FASTCLOSE.
    It is then strange to see the client only receiving 296B, which would
    mean it only got a part of the second packet. The problem is then not on
    the networking side, but rather on the data reception side.
    
    When mptcp_connect is launched with '-f -1', it means the connection
    might stop before having sent everything, because a reset has been
    received. When this happens, the program was directly stopped. But it is
    also possible there are still some data to read, simply because the
    previous 'read' step was done with a buffer smaller than the pending
    data, see do_rnd_read(). In this case, it is important to read what's
    left in the kernel buffers before stopping without error like before.
    
    Fixes: 6bf41020b72b ("selftests: mptcp: update and extend fastclose test-cases")
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Message-ID: <20251101-slft-join-inst-v1-6-10572512a0b2@kernel.org>

--- tools/testing/selftests/net/mptcp/mptcp_connect.c
+++ tools/testing/selftests/net/mptcp/mptcp_connect.c
@@ -710,8 +710,12 @@ static int copyfd_io_poll(int infd, int peerfd, int outfd,
 
 				bw = do_rnd_write(peerfd, winfo->buf + winfo->off, winfo->len);
 				if (bw < 0) {
-					if (cfg_rcv_trunc)
-						return 0;
+					/* expected reset, continue to read */
+					if (cfg_rcv_trunc && errno == ECONNRESET) {
+						fds.events &= ~POLLOUT;
+						continue;
+					}
+
 					perror("write");
 					return 111;
 				}
@@ -737,8 +741,10 @@ static int copyfd_io_poll(int infd, int peerfd, int outfd,
 		}
 
 		if (fds.revents & (POLLERR | POLLNVAL)) {
-			if (cfg_rcv_trunc)
-				return 0;
+			if (cfg_rcv_trunc) {
+				fds.events &= ~(POLLERR | POLLNVAL);
+				continue;
+			}
 			fprintf(stderr, "Unexpected revents: "
 				"POLLERR/POLLNVAL(%x)\n", fds.revents);
 			return 5;
