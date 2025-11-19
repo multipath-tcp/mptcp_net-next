66151bdfa634d7255a62181e91815a16cccd1a3a
Paolo Abeni <pabeni@redhat.com>
2025-11-19 22:54:21 +0100

    mptcp: fix receive space timestamp initialization.
    MPTCP initialize the receive buffer stamp in mptcp_rcv_space_init(),
    using the provided subflow stamp. Such helper is invoked in several
    places; for passive sockets, space init happened at clone time.
    
    In such scenario, MPTCP ends-up accesses the subflow stamp before
    its initialization, leading to quite randomic timing for the first
    receive buffer auto-tune event, as the timestamp for newly created
    subflow is not refreshed there.
    
    Fix the issue moving the stamp initialization out of the mentioned helper,
    at the data transfer start, and always using a fresh timestamp.
    
    Fixes: 013e3179dbd2 ("mptcp: fix rcv space initialization")
    Reviewed-by: Mat Martineau <martineau@kernel.org>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Message-ID: <811d8cc6f0b9bfa6080bd108805cabc021b3e9a5.1763588352.git.pabeni@redhat.com>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -2089,8 +2089,8 @@ static void mptcp_rcv_space_adjust(struct mptcp_sock *msk, int copied)
 
 	msk->rcvq_space.copied += copied;
 
-	mstamp = div_u64(tcp_clock_ns(), NSEC_PER_USEC);
-	time = tcp_stamp_us_delta(mstamp, msk->rcvq_space.time);
+	mstamp = mptcp_stamp();
+	time = tcp_stamp_us_delta(mstamp, READ_ONCE(msk->rcvq_space.time));
 
 	rtt_us = msk->rcvq_space.rtt_us;
 	if (rtt_us && time < (rtt_us >> 3))
@@ -3530,7 +3530,7 @@ struct sock *mptcp_sk_clone_init(const struct sock *sk,
 	mptcp_copy_inaddrs(nsk, ssk);
 	__mptcp_propagate_sndbuf(nsk, ssk);
 
-	mptcp_rcv_space_init(msk, ssk);
+	msk->rcvq_space.time = mptcp_stamp();
 
 	if (mp_opt->suboptions & OPTION_MPTCP_MPC_ACK)
 		__mptcp_subflow_fully_established(msk, subflow, mp_opt);
@@ -3547,8 +3547,6 @@ void mptcp_rcv_space_init(struct mptcp_sock *msk, const struct sock *ssk)
 	msk->rcvspace_init = 1;
 	msk->rcvq_space.copied = 0;
 	msk->rcvq_space.rtt_us = 0;
-
-	msk->rcvq_space.time = tp->tcp_mstamp;
 
 	/* initial rcv_space offering made to peer */
 	msk->rcvq_space.space = min_t(u32, tp->rcv_wnd,
@@ -3745,6 +3743,7 @@ void mptcp_finish_connect(struct sock *ssk)
 	 * accessing the field below
 	 */
 	WRITE_ONCE(msk->local_key, subflow->local_key);
+	WRITE_ONCE(msk->rcvq_space.time, mptcp_stamp());
 
 	mptcp_pm_new_connection(msk, ssk, 0);
 }
