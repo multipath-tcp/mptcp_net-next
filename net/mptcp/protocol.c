e6e537dee9602b54b1f38a825f4b684406c6526f
Gang Yan <yangang@kylinos.cn>
2025-11-27 15:04:17 +0800

    mptcp: sync the msk->sndbuf at accept() time
    After an MPTCP connection is established, the sk_sndbuf of client's msk
    can be updated through 'subflow_finish_connect'. However, the newly
    accepted msk on the server side has a small sk_sndbuf than
    msk->first->sk_sndbuf:
    
    '''
    MPTCP: msk:00000000e55b09db, msk->sndbuf:20480, msk->first->sndbuf:2626560
    '''
    
    This means that when the server immediately sends MSG_DONTWAIT data to
    the client after the connection is established, it is more likely to
    encounter EAGAIN.
    
    This patch synchronizes the sk_sndbuf by triggering its update during accept.
    
    Fixes: 8005184fd1ca ("mptcp: refactor sndbuf auto-tuning")
    Signed-off-by: Gang Yan <yangang@kylinos.cn>
    Message-ID: <20251127070417.53513-1-gang.yan@linux.dev>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -4213,6 +4213,7 @@ static int mptcp_stream_accept(struct socket *sock, struct socket *newsock,
 
 		mptcp_graft_subflows(newsk);
 		mptcp_rps_record_subflows(msk);
+		__mptcp_propagate_sndbuf(newsk, subflow->tcp_sock);
 
 		/* Do late cleanup for the first subflow as necessary. Also
 		 * deal with bad peers not doing a complete shutdown.
