a5cdb3d625cd1452fc680ad9de31afa282c8b77d
Geliang Tang <tanggeliang@kylinos.cn>
2026-01-04 17:35:11 +0800

    selftests: mptcp: join: add mptcp tls tests
    Two MPTCP TLS tests have been added to mptcp_join.sh. The command
    './mptcp_join.sh -c' can be used to run the tests.
    
    Co-developed-by: Gang Yan <yangang@kylinos.cn>
    Signed-off-by: Gang Yan <yangang@kylinos.cn>
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-ID: <6f4bd896b6ecd35613bd5c41b9f6d208bf403386.1767518836.git.tanggeliang@kylinos.cn>

--- tools/testing/selftests/net/mptcp/mptcp_join.sh
+++ tools/testing/selftests/net/mptcp/mptcp_join.sh
@@ -63,6 +63,7 @@ unset fastclose
 unset fullmesh
 unset speed
 unset bind_addr
+unset tls
 unset join_syn_rej
 unset join_csum_ns1
 unset join_csum_ns2
@@ -974,6 +975,7 @@ do_transfer()
 	local fastclose=${fastclose:-""}
 	local speed=${speed:-"fast"}
 	local bind_addr=${bind_addr:-"::"}
+	local tls=${tls:-"::"}
 	local listener_in="${sin}"
 	local connector_in="${cin}"
 	port=$(get_port)
@@ -993,6 +995,10 @@ do_transfer()
 		extra_args="-r 50"
 	elif [ $speed -gt 0 ]; then
 		extra_args="-r ${speed}"
+	fi
+
+	if [ -n "${tls}" ]; then
+		extra_args="$extra_args -o TLS"
 	fi
 
 	local extra_cl_args=""
@@ -4306,6 +4312,31 @@ endpoint_tests()
 	fi
 }
 
+tls_tests()
+{
+	# multiple subflows, tls tests
+	if reset "multiple subflows, tls tests"; then
+		pm_nl_set_limits $ns1 0 2
+		pm_nl_set_limits $ns2 0 2
+		pm_nl_add_endpoint $ns2 10.0.2.2 flags subflow
+		pm_nl_add_endpoint $ns2 10.0.3.2 flags subflow
+		tls=1 run_tests $ns1 $ns2 10.0.1.1
+		chk_join_nr 2 2 2
+	fi
+
+	# multiple subflows, signal, tls tests
+	if reset "multiple subflows, signal, tls tests"; then
+		pm_nl_set_limits $ns1 0 3
+		pm_nl_add_endpoint $ns1 10.0.2.1 dev ns1eth2 flags signal
+		pm_nl_set_limits $ns2 1 3
+		pm_nl_add_endpoint $ns2 10.0.3.2 dev ns2eth3 flags subflow
+		pm_nl_add_endpoint $ns2 10.0.4.2 dev ns2eth4 flags subflow
+		tls=1 run_tests $ns1 $ns2 10.0.1.1
+		chk_join_nr 3 3 3
+		chk_add_nr 1 1
+	fi
+}
+
 # [$1: error message]
 usage()
 {
@@ -4356,6 +4387,7 @@ all_tests_sorted=(
 	F@fail_tests
 	u@userspace_tests
 	I@endpoint_tests
+	c@tls_tests
 )
 
 all_tests_args=""
