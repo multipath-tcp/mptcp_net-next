22c625c9b85e88c892b9151b972b59d77a22499b
Gang Yan <yangang@kylinos.cn>
2026-01-13 17:09:13 +0800

    mptcp: update mptcp_check_readable
    This patch makes mptcp_check_readable() aligned with TCP, and renames it to
    mptcp_stream_is_readable(). It will be used in the case of KTLS, because
    'prot' will be modified, tls_sw_sock_is_readable() is expected to be called
    from prot->sock_is_readable().
    
    Signed-off-by: Gang Yan <yangang@kylinos.cn>
    Message-ID: <a2bef0a2891f3f4839d73518747fd2cb43c34368.1768294706.git.tanggeliang@kylinos.cn>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -3239,9 +3239,11 @@ void __mptcp_unaccepted_force_close(struct sock *sk)
 	__mptcp_destroy_sock(sk);
 }
 
-static __poll_t mptcp_check_readable(struct sock *sk)
+static bool mptcp_stream_is_readable(struct sock *sk)
 {
-	return mptcp_epollin_ready(sk) ? EPOLLIN | EPOLLRDNORM : 0;
+	if (mptcp_epollin_ready(sk))
+		return true;
+	return sk_is_readable(sk);
 }
 
 static void mptcp_check_listen_stop(struct sock *sk)
@@ -4303,7 +4305,8 @@ static __poll_t mptcp_poll(struct file *file, struct socket *sock,
 		mask |= EPOLLIN | EPOLLRDNORM | EPOLLRDHUP;
 
 	if (state != TCP_SYN_SENT && state != TCP_SYN_RECV) {
-		mask |= mptcp_check_readable(sk);
+		if (mptcp_stream_is_readable(sk))
+			mask |= EPOLLIN | EPOLLRDNORM;
 		if (shutdown & SEND_SHUTDOWN)
 			mask |= EPOLLOUT | EPOLLWRNORM;
 		else
