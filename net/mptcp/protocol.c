f9afe00cadcc14cde15d63225e1576f4f53dbf46
Geliang Tang <tanggeliang@kylinos.cn>
2026-02-02 17:21:50 +0800

    mptcp: implement .splice_eof
    This patch implements the .splice_eof interface for MPTCP, namely
    mptcp_splice_eof(), which sequentially calls do_tcp_splice_eof() for
    each subflow.
    
    Suggested-by: Matthieu Baerts <matttbe@kernel.org>
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-ID: <9e3dddfc65f24c1a11e5f0353b497f1d4f1c6f82.1770023932.git.tanggeliang@kylinos.cn>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -4018,6 +4018,20 @@ out:
 	return 0;
 }
 
+static void mptcp_splice_eof(struct socket *sock)
+{
+	struct mptcp_subflow_context *subflow;
+	struct sock *sk = sock->sk, *ssk;
+
+	lock_sock(sk);
+	mptcp_for_each_subflow(mptcp_sk(sk), subflow) {
+		ssk = mptcp_subflow_tcp_sock(subflow);
+
+		do_tcp_splice_eof(ssk);
+	}
+	release_sock(sk);
+}
+
 static struct proto mptcp_prot = {
 	.name		= "MPTCP",
 	.owner		= THIS_MODULE,
@@ -4049,6 +4063,7 @@ static struct proto mptcp_prot = {
 	.obj_size	= sizeof(struct mptcp_sock),
 	.slab_flags	= SLAB_TYPESAFE_BY_RCU,
 	.no_autobind	= true,
+	.splice_eof	= mptcp_splice_eof,
 };
 
 static int mptcp_bind(struct socket *sock, struct sockaddr_unsized *uaddr, int addr_len)
@@ -4540,6 +4555,7 @@ static const struct proto_ops mptcp_stream_ops = {
 	.set_rcvlowat	   = mptcp_set_rcvlowat,
 	.read_sock	   = mptcp_read_sock,
 	.splice_read	   = mptcp_splice_read,
+	.splice_eof	   = inet_splice_eof,
 };
 
 static struct inet_protosw mptcp_protosw = {
