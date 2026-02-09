2ba7d6661a15f3da1e608010a9c9703193aac59d
Geliang Tang <tanggeliang@kylinos.cn>
2026-02-09 17:04:19 +0800

    mptcp: add sock_set_nodelay
    This patch introduces an MPTCP-specific helper, mptcp_sock_set_nodelay,
    which sets the TCP_NODELAY option for every subflow socket within an
    MPTCP connection. It is utilized on both the target and host sides in
    the 'NVMe over MPTCP' implementation.
    
    Using tcp_sock_set_nodelay() with MPTCP will cause list corruption:
    
      nvmet: adding nsid 1 to subsystem nqn.2014-08.org.nvmexpress.mptcpdev
      nvmet_tcp: enabling port 1234 (127.0.0.1:4420)
       slab MPTCP start ffff8880108f0b80 pointer offset 2480 size 2816
      list_add corruption. prev->next should be next (ffff8880108f1530), but
      was ffff8885108f1530. (prev=ffff8880108f1530).
      ------------[ cut here ]------------
      kernel BUG at lib/list_debug.c:32!
      Oops: invalid opcode: 0000 [#1] SMP KASAN NOPTI
      CPU: 1 UID: 0 PID: 182 Comm: nvme Not tainted 6.16.0-rc3+ #1 PREEMPT(full)
      Hardware name: Bochs Bochs, BIOS Bochs 01/01/2011
    
    Co-developed-by: zhenwei pi <zhenwei.pi@linux.dev>
    Signed-off-by: zhenwei pi <zhenwei.pi@linux.dev>
    Co-developed-by: Hui Zhu <zhuhui@kylinos.cn>
    Signed-off-by: Hui Zhu <zhuhui@kylinos.cn>
    Co-developed-by: Gang Yan <yangang@kylinos.cn>
    Signed-off-by: Gang Yan <yangang@kylinos.cn>
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-ID: <a41c967c8157e54bf61c027e511893a5c5cedc50.1770627071.git.tanggeliang@kylinos.cn>

--- drivers/nvme/target/tcp.c
+++ drivers/nvme/target/tcp.c
@@ -2071,6 +2071,8 @@ static int nvmet_tcp_add_port(struct nvmet_port *nport)
 	port->data_ready = port->sock->sk->sk_data_ready;
 	port->sock->sk->sk_data_ready = nvmet_tcp_listen_data_ready;
 	sock_set_reuseaddr(port->sock->sk);
+	sk_is_msk(port->sock->sk) ?
+	mptcp_sock_set_nodelay(port->sock->sk) :
 	tcp_sock_set_nodelay(port->sock->sk);
 	if (so_priority > 0)
 		sock_set_priority(port->sock->sk, so_priority);
