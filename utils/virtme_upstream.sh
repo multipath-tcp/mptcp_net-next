51852b13d2b96e193218923342e638238eac416b
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2026-02-16 11:19:37 +0100

    utils: virtme ups: new DRY_RUN=1 mode
    Useful to check the script is OK, and to print variables.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- utils/virtme_upstream.sh
+++ utils/virtme_upstream.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+: "${DRY_RUN:=0}"
+
 is_stable() {
    [ "${INPUT_STABLE}" = 1 ] ||
       [ -s ".git/BISECT_LOG" ] ||
@@ -10,10 +12,18 @@ is_net() {
    [ "$(b4 prep --show-info prefixes 2>/dev/null)" = "net" ]
 }
 
+if [ "${DRY_RUN}" = 1 ]; then
+   EXEC="echo"
+   set -x
+else
+   EXEC=""
+fi
+
 SYZK=0
 trap_exit() {
    if [ "${SYZK}" = 1 ]; then
-      systemctl --user start syzkaller || true
+      echo "Restarting syzkaller"
+      ${EXEC} systemctl --user start syzkaller || true
    fi
 }
 trap trap_exit EXIT
@@ -22,7 +32,8 @@ CLEAN=0
 
 if systemctl --user is-enabled syzkaller &>/dev/null && systemctl --user is-active syzkaller &>/dev/null; then
    SYZK=1
-   systemctl --user stop syzkaller
+   echo "Stopping syzkaller..."
+   ${EXEC} systemctl --user stop syzkaller
 fi
 
 if is_stable || is_net; then
@@ -34,11 +45,13 @@ else
    PACKETDRILL_STABLE=0
 fi
 
+[ "${DRY_RUN}" = 1 ] && exit
+
 INPUT_BUILD_SKIP_PERF=1 \
    INPUT_VIRTME_EXEC_RUN="${INPUT_VIRTME_EXEC_RUN:-/dev/null}" \
    VIRTME_PACKETDRILL_STABLE=${PACKETDRILL_STABLE} \
    INPUT_SELFTESTS_MPTCP_LIB_OVERRIDE_FLAKY=0 \
-   INPUT_CLEAN=${INPUT_CLEAN:-${CLEAN}} \
-   INPUT_BUILD_SUFFIX=${SUFFIX} \
+   INPUT_CLEAN="${INPUT_CLEAN:-${CLEAN}}" \
+   INPUT_BUILD_SUFFIX="${SUFFIX}" \
    INPUT_EXPECT_TIMEOUT=7200 \
    "./${VIRTME_SH:-.virtme.sh}" "${@:-auto-all}"
