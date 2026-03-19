2e6e6160b2711b0cc3244875bdf381cc6ff36fef
Geliang Tang <tanggeliang@kylinos.cn>
2026-03-19 18:08:48 +0800

    selftests: mptcp: cover mptcp tls tests
    The mptcp tests for tls.c is available now, this patch adds mptcp_tls.sh
    to test it in the MPTCP CI by default.
    
    Co-developed-by: Gang Yan <yangang@kylinos.cn>
    Signed-off-by: Gang Yan <yangang@kylinos.cn>
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-ID: <7dfdc9c184b8f96cb4a808e1022ef5a6af15099e.1773911536.git.tanggeliang@kylinos.cn>

---
+++ tools/testing/selftests/net/mptcp/mptcp_tls.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+
+. "$(dirname "${0}")/mptcp_lib.sh"
+
+ret=0
+ns1=""
+
+cleanup()
+{
+	if [ -n "$pid" ] && kill -0 "$pid" 2>/dev/null; then
+		kill "$pid" 2>/dev/null
+		wait "$pid" 2>/dev/null
+	fi
+
+        mptcp_lib_ns_exit "$ns1"
+}
+
+init()
+{
+        mptcp_lib_ns_init ns1
+
+        mptcp_lib_pm_nl_set_limits "$ns1" 8 8
+
+        local i
+        for i in $(seq 1 4); do
+                mptcp_lib_pm_nl_add_endpoint "$ns1" \
+			"127.0.0.1" flags signal port 1000"$i"
+        done
+}
+
+init
+trap cleanup EXIT
+
+ip netns exec "$ns1" ./tls -v 12_aes_gcm_mptcp \
+			   -v 13_aes_gcm_mptcp \
+			   -v 12_chacha_mptcp \
+			   -v 13_chacha_mptcp \
+			   -v 13_sm4_gcm_mptcp \
+			   -v 13_sm4_ccm_mptcp \
+			   -v 12_aes_ccm_mptcp \
+			   -v 13_aes_ccm_mptcp \
+			   -v 12_aes_gcm_256_mptcp \
+			   -v 13_aes_gcm_256_mptcp \
+			   -v 13_nopad_mptcp \
+			   -v 12_aria_gcm_mptcp \
+			   -v 12_aria_gcm_256_mptcp &
+pid=$!
+wait $pid
+ret=$?
+
+mptcp_lib_result_print_all_tap
+exit $ret
