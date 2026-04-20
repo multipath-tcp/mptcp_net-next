7779576242b00bb916b1f69c9aa685c551fdc5f9
Paolo Abeni <pabeni@redhat.com>
2026-04-20 12:29:25 +0200

    mptcp: move checks vs rcvbuf size earlier in the RX path
    Currently the enforcement of the rcvbuf constraint is implemented
    when moving the skbs into the msk receive or OoO queue.
    
    Under significant memory pressure the above can cause permanent data
    transfer stalls. Move the checks early on, before landing even in
    the subflow queues.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Message-ID: <7bd58b4078ae99c6514d356792baa75e0c4f1b9a.1776680489.git.pabeni@redhat.com>

--- net/mptcp/options.c
+++ net/mptcp/options.c
@@ -1158,8 +1158,19 @@ static bool add_addr_hmac_valid(struct mptcp_sock *msk,
 	return hmac == mp_opt->ahmac;
 }
 
-/* Return false in case of error (or subflow has been reset),
- * else return true.
+static bool mptcp_over_limit(const struct sock *sk, struct sk_buff *skb)
+{
+	int limit;
+
+	if (!skb->len)
+		return false;
+
+	limit = READ_ONCE(sk->sk_rcvbuf) << 1;
+	return sk_rmem_alloc_get(sk) > limit;
+}
+
+/* Return false when the caller must to drop the packet, i.e. in case of error,
+ * subflow has been reset, or over memory limits.
  */
 bool mptcp_incoming_options(struct sock *sk, struct sk_buff *skb)
 {
@@ -1185,6 +1196,9 @@ bool mptcp_incoming_options(struct sock *sk, struct sk_buff *skb)
 
 		__mptcp_data_acked(subflow->conn);
 		mptcp_data_unlock(subflow->conn);
+
+		if (mptcp_over_limit(subflow->conn, skb))
+			return false;
 		return true;
 	}
 
@@ -1262,6 +1276,9 @@ bool mptcp_incoming_options(struct sock *sk, struct sk_buff *skb)
 
 		return true;
 	}
+
+	if (mptcp_over_limit(subflow->conn, skb))
+		return false;
 
 	mpext = skb_ext_add(skb, SKB_EXT_MPTCP);
 	if (!mpext)
