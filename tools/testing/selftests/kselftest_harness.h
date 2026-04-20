08cec27d8824843e0e9d45cbe240c8c4249f58e8
Geliang Tang <tanggeliang@kylinos.cn>
2026-04-20 15:04:57 +0800

    selftests: harness: fix pidfd leak in __wait_for_test
    Fix the pidfd leak in kselftest_harness.h's __wait_for_test() where
    childfd = syscall(__NR_pidfd_open, t->pid, 0) is never closed.
    
    Fixes: 73a3cde97677 ("selftests: harness: Implement test timeouts through pidfd")
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-ID: <f6a4cf56c8c361d3e7373d83fd3930bd28600b4c.1776668161.git.tanggeliang@kylinos.cn>

--- tools/testing/selftests/kselftest_harness.h
+++ tools/testing/selftests/kselftest_harness.h
@@ -1001,7 +1001,7 @@ static void __wait_for_test(struct __test_metadata *t)
 		fprintf(TH_LOG_STREAM,
 			"# %s: unable to wait on child pidfd\n",
 			t->name);
-		return;
+		goto out;
 	} else if (ret == 0) {
 		timed_out = true;
 		/* signal process group */
@@ -1013,7 +1013,7 @@ static void __wait_for_test(struct __test_metadata *t)
 		fprintf(TH_LOG_STREAM,
 			"# %s: Failed to wait for PID %d (errno: %d)\n",
 			t->name, t->pid, errno);
-		return;
+		goto out;
 	}
 
 	if (timed_out) {
@@ -1066,6 +1066,8 @@ static void __wait_for_test(struct __test_metadata *t)
 			t->name,
 			status);
 	}
+out:
+	close(childfd);
 }
 
 static void test_harness_list_tests(void)
