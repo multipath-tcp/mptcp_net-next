288205491780acf0cf4b56594b52bb51cad2b929
Geliang Tang <tanggeliang@kylinos.cn>
2026-05-15 17:46:20 +0800

    nvme-tcp: implement host mptcp proto
    An MPTCP-specific version of struct nvme_tcp_proto is implemented,
    and it is assigned to ctrl->proto when the transport string is "mptcp".
    
    The socket option setting logic is similar to the target side, except that
    mptcp_sock_set_syncnt is newly defined for the host side.
    
    It sets the value on the first subflow socket of an MPTCP connection.
    The value is then synchronized to other newly created subflows in
    sync_socket_options().
    
    A separate nvme_mptcp_ctrl_ops structure with .name = "mptcp" is defined
    and used for MPTCP controllers.
    
    Cc: Hannes Reinecke <hare@suse.de>
    Co-developed-by: zhenwei pi <zhenwei.pi@linux.dev>
    Signed-off-by: zhenwei pi <zhenwei.pi@linux.dev>
    Co-developed-by: Hui Zhu <zhuhui@kylinos.cn>
    Signed-off-by: Hui Zhu <zhuhui@kylinos.cn>
    Co-developed-by: Gang Yan <yangang@kylinos.cn>
    Signed-off-by: Gang Yan <yangang@kylinos.cn>
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-ID: <fca251d4ef7849c305c98d63dfe33be1b7b981d6.1778837549.git.tanggeliang@kylinos.cn>

--- net/mptcp/sockopt.c
+++ net/mptcp/sockopt.c
@@ -1602,6 +1602,8 @@ static void sync_socket_options(struct mptcp_sock *msk, struct sock *ssk)
 	priority = READ_ONCE(sk->sk_priority);
 	if (priority > 0)
 		sock_set_priority(ssk, priority);
+	if (msk->icsk_syn_retries > 0)
+		tcp_sock_set_syncnt(ssk, msk->icsk_syn_retries);
 }
 
 void mptcp_sockopt_sync_locked(struct mptcp_sock *msk, struct sock *ssk)
@@ -1761,3 +1763,22 @@ void mptcp_sock_set_tos(struct sock *sk, int val)
 	release_sock(sk);
 }
 EXPORT_SYMBOL(mptcp_sock_set_tos);
+
+int mptcp_sock_set_syncnt(struct sock *sk, int val)
+{
+	struct mptcp_sock *msk = mptcp_sk(sk);
+	struct sock *ssk;
+
+	if (val < 1 || val > MAX_TCP_SYNCNT)
+		return -EINVAL;
+
+	lock_sock(sk);
+	sockopt_seq_inc(msk);
+	msk->icsk_syn_retries = val;
+	ssk = __mptcp_nmpc_sk(msk);
+	if (!IS_ERR(ssk))
+		tcp_sock_set_syncnt(ssk, val);
+	release_sock(sk);
+	return 0;
+}
+EXPORT_SYMBOL(mptcp_sock_set_syncnt);
