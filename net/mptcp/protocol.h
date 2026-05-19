71030762bebf3da73c40cb32f54ef63d80aeded4
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2026-05-19 15:51:12 +1000

    mptcp: pm: avoid computing rm_addr size twice
    mptcp_rm_addr_len helper was called twice: in mptcp_pm_rm_addr_signal,
    then just after in mptcp_established_options_rm_addr. Both to check the
    remaining space.
    
    The second call is not needed: if there is not enough space,
    mptcp_pm_rm_addr_signal will return false, and the caller,
    mptcp_established_options_rm_addr, will do the same without re-checking
    the size again. Instead, mptcp_pm_rm_addr_signal can directly set the
    size.
    
    While at it, move mptcp_rm_addr_len to pm.c, as it is now only used
    there, once.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Message-ID: <20260519-mptcp-add-addr6-port-ts-v2-3-3b36a51e6a86@kernel.org>

--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -1224,20 +1224,12 @@ static inline unsigned int mptcp_add_addr_len(int family, bool echo, bool port)
 	return len;
 }
 
-static inline int mptcp_rm_addr_len(const struct mptcp_rm_list *rm_list)
-{
-	if (rm_list->nr == 0 || rm_list->nr > MPTCP_RM_IDS_MAX)
-		return -EINVAL;
-
-	return TCPOLEN_MPTCP_RM_ADDR_BASE + roundup(rm_list->nr - 1, 4) + 1;
-}
-
 bool mptcp_pm_add_addr_signal(struct mptcp_sock *msk, const struct sk_buff *skb,
 			      unsigned int opt_size, unsigned int remaining,
 			      struct mptcp_addr_info *addr, bool *echo,
 			      bool *drop_other_suboptions);
 bool mptcp_pm_rm_addr_signal(struct mptcp_sock *msk, unsigned int remaining,
-			     struct mptcp_rm_list *rm_list);
+			     struct mptcp_rm_list *rm_list, unsigned int *len);
 int mptcp_pm_get_local_id(struct mptcp_sock *msk, struct sock_common *skc);
 bool mptcp_pm_is_backup(struct mptcp_sock *msk, struct sock_common *skc);
 int mptcp_pm_nl_dump_addr(struct sk_buff *msg,
