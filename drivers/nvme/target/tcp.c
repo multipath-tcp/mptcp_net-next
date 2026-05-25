2d23b64a9263b242e57f7584daabda062bebdce9
Geliang Tang <tanggeliang@kylinos.cn>
2026-05-25 17:33:08 +0800

    nvmet-tcp: fix page fragment cache leak in error path
    In nvmet_tcp_alloc_queue(), when a connection is closed during the
    allocation process (e.g., nvmet_tcp_set_queue_sock() returns -ENOTCONN),
    the error handling jumps to out_free_queue without draining the page
    fragment cache.
    
    Although nvmet_tcp_free_cmd() is called in some error paths to release
    individual page fragments, the underlying page cache reference held by
    queue->pf_cache is never released. This results in a page leak each time
    a connection fails during allocation, which could lead to memory
    exhaustion over time if connections are repeatedly opened and closed.
    
    Fix this by calling page_frag_cache_drain() before freeing the queue
    structure in the out_free_queue label.
    
    Cc: Hannes Reinecke <hare@suse.de>
    Cc: zhenwei pi <zhenwei.pi@linux.dev>
    Cc: Hui Zhu <zhuhui@kylinos.cn>
    Cc: Gang Yan <yangang@kylinos.cn>
    Fixes: 872d26a391da ("nvmet-tcp: add NVMe over TCP target driver")
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-ID: <f1c10a800fb0367d0fbbc86fbf4a6ae3fd31ebc0.1779701391.git.tanggeliang@kylinos.cn>

--- drivers/nvme/target/tcp.c
+++ drivers/nvme/target/tcp.c
@@ -2047,6 +2047,7 @@ out_ida_remove:
 out_sock:
 	fput(queue->sock->file);
 out_free_queue:
+	page_frag_cache_drain(&queue->pf_cache);
 	kfree(queue);
 out_release:
 	pr_err("failed to allocate queue, error %d\n", ret);
