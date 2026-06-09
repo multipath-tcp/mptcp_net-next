331b3a4180dc18788b65e4972e07239b6dd83d50
Geliang Tang <tanggeliang@kylinos.cn>
2026-06-09 10:18:28 +0800

    tls: use sendmsg_locked from the underlying socket
    TLS offload (device and sw) may call tcp_sendmsg_locked() directly
    when pushing TLS records. This assumes the underlying socket is always
    a TCP socket. With MPTCP, the socket can be an MPTCP socket, which
    does not directly expose a sendmsg_locked method via its proto_ops.
    
    Replace the hard-coded tcp_sendmsg_locked() call with
    sk->sk_socket->ops->sendmsg_locked(). This enables TLS to work
    transparently over any socket that implements .sendmsg_locked,
    including MPTCP after the previous commit.
    
    The change is safe because both TCP and MPTCP now provide a conformant
    .sendmsg_locked implementation.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-ID: <ffdba5765d2cd4696bbb32e756329be6ae95dd88.1780970284.git.tanggeliang@kylinos.cn>

--- net/tls/tls_main.c
+++ net/tls/tls_main.c
@@ -194,7 +194,7 @@ retry:
 		bvec_set_page(&bvec, p, size, offset);
 		iov_iter_bvec(&msg.msg_iter, ITER_SOURCE, &bvec, 1, size);
 
-		ret = tcp_sendmsg_locked(sk, &msg, size);
+		ret = sk->sk_socket->ops->sendmsg_locked(sk, &msg, size);
 
 		if (ret != size) {
 			if (ret > 0) {
