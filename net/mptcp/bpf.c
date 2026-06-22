0f4ed15e3a6220d84970e116046d7bd84acab2ce
Geliang Tang <tanggeliang@kylinos.cn>
2026-06-22 17:39:01 +0800

    Squash to "bpf: Export mptcp packet scheduler helpers" 2
    Remove __ign suffix from bpf_mptcp_subflow_ctx() and add sk_type ==
    SOCK_STREAM check to prevent raw socket misuse.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Link: https://patch.msgid.link/9da8028711cbb18a1a7ddddb893c15a9bbe5f4ce.1782120674.git.tanggeliang@kylinos.cn
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- net/mptcp/bpf.c
+++ net/mptcp/bpf.c
@@ -220,11 +220,9 @@ struct bpf_iter_mptcp_subflow_kern {
 __bpf_kfunc_start_defs();
 
 __bpf_kfunc static struct mptcp_subflow_context *
-bpf_mptcp_subflow_ctx(const struct sock *sk__ign)
+bpf_mptcp_subflow_ctx(const struct sock *sk)
 {
-	const struct sock *sk = sk__ign;
-
-	if (sk && sk_fullsock(sk) &&
+	if (sk && sk_fullsock(sk) && sk->sk_type == SOCK_STREAM &&
 	    sk->sk_protocol == IPPROTO_TCP && sk_is_mptcp(sk))
 		return mptcp_subflow_ctx(sk);
 
