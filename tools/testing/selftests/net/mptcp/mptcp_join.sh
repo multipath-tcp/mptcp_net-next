f1c99ec653fac65aeaf2cbcd38382dc7b0e8de71
Geliang Tang <tanggeliang@kylinos.cn>
2026-06-27 06:45:54 +0800

    selftests: mptcp: join: explicitly remove userspace pm addresses
    Now address entries only be removed from local_addr_list when sending a
    REMOVE_ADDR by the userspace PM, they're no longer removed when a subflow
    is deleted.
    
    To make userspace PM selftests in mptcp_join.sh pass, this patch calls
    explicit userspace_pm_rm_addr() to clean up the address IDs, and adjust
    the chk_rm_nr counter accordingly to match the total number of RM_ADDRs
    sent.
    
    Reviewed-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-ID: <176327823995be8f450b6e654306b87e8ff23593.1782513313.git.tanggeliang@kylinos.cn>

--- tools/testing/selftests/net/mptcp/mptcp_join.sh
+++ tools/testing/selftests/net/mptcp/mptcp_join.sh
@@ -4088,11 +4088,12 @@ userspace_tests()
 		userspace_pm_chk_get_addr "${ns1}" "10" "id 10 flags signal 10.0.2.1"
 		userspace_pm_chk_get_addr "${ns1}" "20" "id 20 flags signal 10.0.3.1"
 		userspace_pm_rm_sf $ns1 "::ffff:10.0.2.1" $MPTCP_LIB_EVENT_SUB_ESTABLISHED
+		userspace_pm_rm_addr $ns1 10
 		userspace_pm_chk_dump_addr "${ns1}" \
 			"id 20 flags signal 10.0.3.1" "after rm_sf 10"
 		userspace_pm_rm_addr $ns1 20
 		userspace_pm_chk_dump_addr "${ns1}" "" "after rm_addr 20"
-		chk_rm_nr 1 1 invert
+		chk_rm_nr 2 1 invert
 		chk_mptcp_info subflows 0 subflows 0
 		chk_subflows_total 1 1
 		kill_events_pids
@@ -4118,6 +4119,7 @@ userspace_tests()
 			"subflow"
 		userspace_pm_chk_get_addr "${ns2}" "20" "id 20 flags subflow 10.0.3.2"
 		userspace_pm_rm_sf $ns2 10.0.3.2 $MPTCP_LIB_EVENT_SUB_ESTABLISHED
+		userspace_pm_rm_addr $ns2 20
 		userspace_pm_chk_dump_addr "${ns2}" \
 			"" \
 			"after rm_sf 20"
