dc10025f64695385cb6db41c947dee3e0cc74cce
Geliang Tang <tanggeliang@kylinos.cn>
2026-07-09 15:06:12 +0800

    selftests: mptcp: sockopt: set TCP_INQ on accepted sockets
    Add helper functions to conditionally set TCP_INQ sockopt on accepted
    sockets when running in inq mode. This ensures proper testing of TCP_CM_INQ
    functionality by enabling the required socket option on both ends of the
    connection.
    
    These codes are from mptcp_inq.c.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-ID: <4365a32b22b7bb1364b3f5b5c0c038adaf99c722.1783579976.git.tanggeliang@kylinos.cn>

--- tools/testing/selftests/net/mptcp/mptcp_sockopt.c
+++ tools/testing/selftests/net/mptcp/mptcp_sockopt.c
@@ -718,12 +718,23 @@ static void process_one_client(int fd, int pipefd)
 	close(fd);
 }
 
+static void do_setsockopt_inq(int fd)
+{
+	int on = 1;
+
+	if (setsockopt(fd, IPPROTO_TCP, TCP_INQ, &on, sizeof(on)))
+		die_perror("setsockopt(TCP_INQ)");
+}
+
 static int xaccept(int s)
 {
 	int fd = accept(s, NULL, 0);
 
 	if (fd < 0)
 		die_perror("accept");
+
+	if (inq)
+		do_setsockopt_inq(fd);
 
 	return fd;
 }
