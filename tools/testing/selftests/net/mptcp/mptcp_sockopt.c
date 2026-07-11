55a0b74ee9f8c00e905db87f39009a5326d2953f
Geliang Tang <tanggeliang@kylinos.cn>
2026-07-11 11:43:39 +0800

    selftests: mptcp: sockopt: wait ack for huge transfer
    Add wait_for_ack() helper to synchronize test steps by waiting until the
    send queue is drained (all data ACKed). Used in server_huge_transfer()
    to ensure reliable TCP_INQ validation.
    
    These codes are from mptcp_inq.c.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-ID: <d2c3f6a810173d3e4faf761fbf309e2ee4f0c70a.1783740443.git.tanggeliang@kylinos.cn>

--- tools/testing/selftests/net/mptcp/mptcp_sockopt.c
+++ tools/testing/selftests/net/mptcp/mptcp_sockopt.c
@@ -26,6 +26,7 @@
 #include <netinet/in.h>
 
 #include <linux/tcp.h>
+#include <linux/sockios.h>
 #include <linux/compiler.h>
 
 static int pf = AF_INET;
@@ -598,10 +599,45 @@ static void do_getsockopts(struct so_state *s, int fd, size_t r, size_t w)
 		do_getsockopt_mptcp_full_info(s, fd);
 }
 
+/* wait up to timeout milliseconds */
+static void wait_for_ack(int fd, int timeout, size_t total)
+{
+	int i;
+
+	for (i = 0; i < timeout; i++) {
+		int nsd, ret, queued = -1;
+		struct timespec req;
+
+		ret = ioctl(fd, TIOCOUTQ, &queued);
+		if (ret < 0)
+			die_perror("TIOCOUTQ");
+
+		ret = ioctl(fd, SIOCOUTQNSD, &nsd);
+		if (ret < 0)
+			die_perror("SIOCOUTQNSD");
+
+		if ((size_t)queued > total)
+			xerror("TIOCOUTQ %u, but only %zu expected\n",
+			       queued, total);
+		assert(nsd <= queued);
+
+		if (queued == 0)
+			return;
+
+		/* wait for peer to ack rx of all data */
+		req.tv_sec = 0;
+		req.tv_nsec = 1 * 1000 * 1000ul; /* 1ms */
+		nanosleep(&req, NULL);
+	}
+
+	xerror("still tx data queued after %u ms\n", timeout);
+}
+
 static void server_huge_transfer(int fd, int unixfd, size_t len)
 {
 	char buf[4096], buf2[4];
 	size_t i, total;
+	size_t sent;
 	ssize_t ret;
 
 	for (i = 0; i < sizeof(buf) - 1; i++) {
@@ -617,9 +653,12 @@ static void server_huge_transfer(int fd, int unixfd, size_t len)
 
 	total = rand() % (16 * 1024 * 1024);
 	total += (1 * 1024 * 1024);
+	sent = total;
 
 	ret = write(unixfd, &total, sizeof(total));
 	assert(ret == (ssize_t)sizeof(total));
+
+	wait_for_ack(fd, 5000, len);
 
 	while (total > 0) {
 		if (total > sizeof(buf))
@@ -640,6 +679,8 @@ static void server_huge_transfer(int fd, int unixfd, size_t len)
 	ret = read(unixfd, buf2, 4);
 	assert(ret == 4);
 	assert(strncmp(buf2, "shut", 4) == 0);
+
+	wait_for_ack(fd, 5000, sent);
 
 	ret = write(fd, buf, 1);
 	assert(ret == 1);
