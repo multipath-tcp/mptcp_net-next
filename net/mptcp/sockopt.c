e55a3f27473807cd89e2d375345eaf8e118f21ad
Geliang Tang <tanggeliang@kylinos.cn>
2026-07-25 21:13:41 +0800

    mptcp: handle TCP_MAXSEG getsockopt in common case
    The TCP_MAXSEG getsockopt() case was a tail-clause returning
    mptcp_getsockopt_first_sf_only() while every other "first subflow
    only" option (TCP_FASTOPEN_*) was already grouped in the common
    case. Move it next to its peers so the dispatch reads uniformly.
    
    No behavioural change; the lookup still returns the cached MSS
    on the first subflow.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-ID: <ebade3eaaf58fafb9e54e644e5eb8f29d2af533f.1784985085.git.tanggeliang@kylinos.cn>

--- net/mptcp/sockopt.c
+++ net/mptcp/sockopt.c
@@ -1424,6 +1424,7 @@ static int mptcp_getsockopt_sol_tcp(struct mptcp_sock *msk, int optname,
 	case TCP_FASTOPEN_CONNECT:
 	case TCP_FASTOPEN_KEY:
 	case TCP_FASTOPEN_NO_COOKIE:
+	case TCP_MAXSEG:
 		return mptcp_getsockopt_first_sf_only(msk, SOL_TCP, optname,
 						      optval, optlen);
 	case TCP_INQ:
@@ -1448,9 +1449,6 @@ static int mptcp_getsockopt_sol_tcp(struct mptcp_sock *msk, int optname,
 		return mptcp_put_int_option(msk, optval, optlen, msk->notsent_lowat);
 	case TCP_IS_MPTCP:
 		return mptcp_put_int_option(msk, optval, optlen, 1);
-	case TCP_MAXSEG:
-		return mptcp_getsockopt_first_sf_only(msk, SOL_TCP, optname,
-						      optval, optlen);
 	}
 	return -EOPNOTSUPP;
 }
