f9e73e608f4678a4a75a624dc976bd9e09806389
Geliang Tang <tanggeliang@kylinos.cn>
2026-07-28 19:39:35 +0800

    mptcp: handle TCP_MAXSEG getsockopt in common case
    Move TCP_MAXSEG from a tail-clause into the common case alongside other
    "first subflow only" options (TCP_FASTOPEN_*), making the dispatch logic
    more uniform.
    
    No behavioural change.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Message-ID: <7de71aa6c2a057b05a5d3e7f30dc73177af0592a.1785238723.git.tanggeliang@kylinos.cn>

--- net/mptcp/sockopt.c
+++ net/mptcp/sockopt.c
@@ -1425,6 +1425,7 @@ static int mptcp_getsockopt_sol_tcp(struct mptcp_sock *msk, int optname,
 	case TCP_FASTOPEN_CONNECT:
 	case TCP_FASTOPEN_KEY:
 	case TCP_FASTOPEN_NO_COOKIE:
+	case TCP_MAXSEG:
 		return mptcp_getsockopt_first_sf_only(msk, SOL_TCP, optname,
 						      optval, optlen);
 	case TCP_INQ:
@@ -1449,9 +1450,6 @@ static int mptcp_getsockopt_sol_tcp(struct mptcp_sock *msk, int optname,
 		return mptcp_put_int_option(msk, optval, optlen, msk->notsent_lowat);
 	case TCP_IS_MPTCP:
 		return mptcp_put_int_option(msk, optval, optlen, 1);
-	case TCP_MAXSEG:
-		return mptcp_getsockopt_first_sf_only(msk, SOL_TCP, optname,
-						      optval, optlen);
 	}
 	return -EOPNOTSUPP;
 }
