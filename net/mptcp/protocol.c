bce912e922bdaae05b802b23ff8fb3dc395b37d8
Paolo Abeni <pabeni@redhat.com>
2026-08-28 12:17:14 +0200

    Squash-to: "mptcp: do not reschedule the RTX timer for fallback sockets"
    Sashiko noted that the 'RTX disabled' status is carried over
    across connect() failures, potentially to subsequent successful connect()
    or listen().
    
    Explicitly control the RTX enabling status across the whole msk life-cycle.
    To make the code more straight forward switch the newly introduced flag
    semantic.
    
    To be appended to the squash-to commit message:
    
    """
    The RTX enable bit is clear at close time and set before the msk could
    start retransmitting, with a couple of caveats:
    - passive sockets inherit the bit from the listener msk; set the bit on
      such socket to avoid flipping it in the fast-path, even if the listener
      will obviously never retransmit.
    - while fastopening, mptcp_sendmsg_fastopen still ends-up calling
      mptcp_connect via tcp_sendmsg_fastopen ->
      __inet_stream_connect(ssk->sk_socket), and the first subflow sk_socket
      points to the msk one.
    """
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Message-ID: <18efc48d4288baa509ab8305e24c1c8b10d78b16.1787911017.git.pabeni@redhat.com>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -96,7 +96,7 @@ bool __mptcp_try_fallback(struct mptcp_sock *msk, int fb_mib)
 
 	msk->allow_subflows = false;
 	set_bit(MPTCP_FALLBACK_DONE, &msk->flags);
-	set_bit(MPTCP_RTX_DISABLED, &msk->flags);
+	clear_bit(MPTCP_RTX_ENABLED, &msk->flags);
 	__MPTCP_INC_STATS(net, fb_mib);
 	spin_unlock_bh(&msk->fallback_lock);
 	return true;
@@ -1126,7 +1126,7 @@ static void mptcp_reset_rtx_timer(struct sock *sk)
 	unsigned long tout;
 
 	/* Prevent rescheduling on close and in case of fallback. */
-	if (test_bit(MPTCP_RTX_DISABLED, &msk->flags))
+	if (!test_bit(MPTCP_RTX_ENABLED, &msk->flags))
 		return;
 
 	tout = msk->timer_ival;
@@ -3363,7 +3363,7 @@ void mptcp_set_state(struct sock *sk, int state)
 		 */
 		break;
 	case TCP_CLOSE:
-		set_bit(MPTCP_RTX_DISABLED, &mptcp_sk(sk)->flags);
+		clear_bit(MPTCP_RTX_ENABLED, &mptcp_sk(sk)->flags);
 		fallthrough;
 	default:
 		if (oldstate == TCP_ESTABLISHED || oldstate == TCP_CLOSE_WAIT)
@@ -4189,6 +4189,7 @@ static int mptcp_connect(struct sock *sk, struct sockaddr_unsized *uaddr,
 	if (IS_ERR(ssk))
 		return PTR_ERR(ssk);
 
+	set_bit(MPTCP_RTX_ENABLED, &msk->flags);
 	mptcp_set_state(sk, TCP_SYN_SENT);
 	subflow = mptcp_subflow_ctx(ssk);
 #ifdef CONFIG_TCP_MD5SIG
@@ -4336,6 +4337,7 @@ static int mptcp_listen(struct socket *sock, int backlog)
 		goto unlock;
 	}
 
+	set_bit(MPTCP_RTX_ENABLED, &msk->flags);
 	mptcp_set_state(sk, TCP_LISTEN);
 	sock_set_flag(sk, SOCK_RCU_FREE);
 
