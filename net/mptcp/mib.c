d2c0346ead3aa1cafef06047434edb4a414258a8
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2026-09-04 11:08:40 +0200

    mptcp: options: track invalid suboptions
    Any unexpected suboptions combinations or use of invalid sizes are
    suspicious: either it is coming from a buggy host, or an attacker.
    
    Better to track that with a new MIB counter.
    
    Note that receiving unsupported versions or extensions can happen, so
    this part shouldn't be considered as invalid.
    
    Co-developed-by: Gang Yan <yangang@kylinos.cn>
    Signed-off-by: Gang Yan <yangang@kylinos.cn>
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Message-ID: <20260904-mptcp-mib-inval-opt-v2-2-a72003dc4425@kernel.org>

--- net/mptcp/mib.c
+++ net/mptcp/mib.c
@@ -95,6 +95,7 @@ static const struct snmp_mib mptcp_snmp_list[] = {
 	SNMP_MIB_ITEM("BacklogDrop", MPTCP_MIB_BACKLOGDROP),
 	SNMP_MIB_ITEM("RcvPruned", MPTCP_MIB_RCVPRUNED),
 	SNMP_MIB_ITEM("OFOPruned", MPTCP_MIB_OFOPRUNED),
+	SNMP_MIB_ITEM("OptInvalid", MPTCP_MIB_OPTINVALID),
 };
 
 /* mptcp_mib_alloc - allocate percpu mib counters
